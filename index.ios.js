47665272ee33b86234fe6b0868f72a7637f4e1a2
Drew Volz <volz@stolaf.edu>
2015-12-21 23:53:13 -0700

    Removed router temporarily

--- index.ios.js
+++ index.ios.js
@@ -148,26 +148,6 @@ var AllAboutOlaf = React.createClass({
     return (
         <View>
 
-            {/* Router */}
-            <Navigator
-                initialRoute={{name: 'My First Scene', index: 0}}
-                renderScene={(route, navigator) =>
-                  <MySceneComponent
-                    name={route.name}
-                    onForward={() => {
-                      var nextIndex = route.index + 1;
-                      navigator.push({
-                        name: 'Scene ' + nextIndex,
-                        index: nextIndex,
-                      });
-                    }}
-                    onBack={() => {
-                      if (route.index > 0) {
-                        navigator.pop();
-                      }
-                    }}/>
-                }/>
-
             {/* Top navigation bar */}
             <NavigationBar
                 title={titleConfig}
