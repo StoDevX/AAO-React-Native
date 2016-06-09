7311a4218b09ddd31a9f25590585952809c4cc99
Hawken Rives <hawkrives@gmail.com>
2016-06-09 16:51:36 -0500

    Add initial News views (#3)
    * begin work on News section
    
    * add a NewsView for android

---
+++ views/news/index.android.js
@@ -0,0 +1,55 @@
+/**
+ * All About Olaf
+ * iOS News page
+ */
+
+import React from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native'
+
+import {TabLayout, Tab} from 'react-native-android-tablayout'
+import NavigatorScreen from '../components/navigator-screen'
+import tabs from './tabs'
+
+export default class NewsView extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      selectedTab: 0
+    }
+  }
+
+  render() {
+    return <NavigatorScreen
+      {...this.props}
+      title="News"
+      renderScene={this.renderScene.bind(this)}
+    />
+  }
+
+  // Render a given scene
+  renderScene() {
+    let TabContents = tabs[this.state.selectedTab].content
+    return (
+      <View style={styles.container}>
+        <TabLayout
+          selectedTabIndicatorColor="darkslateblue"
+          selectedTab={this.state.selectedTab}
+          onTabSelected={e => {this.setState({selectedTab: e.nativeEvent.position})}}
+        >
+          {tabs.map(tab => <Tab key={tab.id} name={tab.title} />)}
+        </TabLayout>
+        {<TabContents />}
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
---
+++ views/news/politicole.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+} from 'react-native'
+
+const styles = StyleSheet.create({
+
+})
+
+export default class PoliticoleView extends React.Component {
+  render() {
+    return (
+      <ScrollView>
+        <View>
+          <Text>PoliticOle</Text>
+        </View>
+      </ScrollView>
+    )
+  }
+}
