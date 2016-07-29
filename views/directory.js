fd455b9a7866b455580437ea75cccb36b88de2c8
Elijah Verdoorn <elijahverdoorn@gmail.com>
2016-07-28 23:06:16 -0500

    Add webview for Directories

--- views/directory.js
+++ views/directory.js
@@ -1,18 +1,54 @@
 /**
  * All About Olaf
- * iOS Directory page
+ * Directory page
  */
 
 import React from 'react'
 import {
   StyleSheet,
   View,
-  Text,
+  NetInfo,
+  WebView,
 } from 'react-native'
+
 import NavigatorScreen from './components/navigator-screen'
-import queryStalkernet from '../lib/stalkernet'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webView: {
+    flex: 1,
+  }
+})
+
+const URL = 'https://www.stolaf.edu/personal/'
 
 export default class DirectoryView extends React.Component {
+  constructor() {
+    super()
+    this.scalesPageToFit = true
+    this.url = URL
+    this.state = {
+      isConnected: true
+    }
+  }
+
+  componentWillMount() {
+    NetInfo.isConnected.fetch().then(isConnected => {
+      this._onConnectivityChange(isConnected)
+    })
+    NetInfo.isConnected.addEventListener('change', this._onConnectivityChange.bind(this))
+  }
+
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener('change', this._onConnectivityChange.bind(this))
+  }
+
+  _onConnectivityChange(isConnected) {
+    this.setState({isConnected})
+  }
+
   render() {
     return <NavigatorScreen
       {...this.props}
@@ -21,18 +57,30 @@ export default class DirectoryView extends React.Component {
     />
   }
 
-  // Render a given scene
   renderScene() {
+    let webViewSource = {uri: this.url}
     return (
       <View style={styles.container}>
-        <Text>Directory</Text>
+        <WebView
+          automaticallyAdjustContentInsets={false}
+          style={styles.webView}
+          source={webViewSource}
+          javaScriptEnabled={true}
+          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+          onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest.bind(this)}
+          startInLoadingState={true}
+          scalesPageToFit={this.scalesPageToFit}
+        />
       </View>
     )
   }
-}
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  onShouldStartLoadWithRequest() {
+    return true
   }
-})
+
+  onNavigationStateChange(navState) {
+    this.url = navState.url
+    this.scalesPageToFit = true
+  }
+}
