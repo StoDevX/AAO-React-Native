dd71e4af25344f33a76dfa753ace79b6a9771247
Elijah Verdoorn <elijahverdoorn@gmail.com>
2016-08-09 00:50:11 -0500

    Android build now works.

--- android/app/src/main/java/com/allaboutolaf/MainApplication.java
+++ android/app/src/main/java/com/allaboutolaf/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.keychain.KeychainPackage;
+import com.xebia.reactnative.TabLayoutPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,7 +26,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+        new MainReactPackage(),
+        new VectorIconsPackage(),
+        new KeychainPackage(),
+        new TabLayoutPackage()
       );
     }
   };
