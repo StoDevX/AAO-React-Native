e0f24df2eeee4d7871ca307da9fc6a2f6abb153f
Elijah Verdoorn <elijahverdoorn@gmail.com>
2016-08-09 23:39:42 -0500

    Push notifications on android
    Push notifications are being recieved on android. Note that this required
    a clean build of the android build-generated-files.

--- android/app/src/main/java/com/allaboutolaf/MainApplication.java
+++ android/app/src/main/java/com/allaboutolaf/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,7 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
         new MainReactPackage(),
         new VectorIconsPackage(),
         new KeychainPackage(),
-        new TabLayoutPackage()
+        new TabLayoutPackage(),
+        new ReactNativeOneSignalPackage()
       );
     }
   };
