02c0bf19e747831f331dfde389aba9600f7397a6
Hawken Rives <hawkrives@gmail.com>
2016-08-15 18:54:19 +0900

    remove a pointless pop() from home

--- views/home.js
+++ views/home.js
@@ -118,7 +118,7 @@ export default function HomePage({navigator}: ScenePropsType) {
     rightButton={(route, navigator) =>
       <TouchableOpacity
         style={styles.navButton}
-        onPress={() => navigator.parentNavigator.pop()}
+        onPress={() => {}}
       >
         <Text style={styles.navigationButtonText}>Edit</Text>
       </TouchableOpacity>
