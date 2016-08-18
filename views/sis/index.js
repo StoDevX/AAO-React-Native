17e48ce458ba71f406b020a016e8a91072bff495
Drew Volz <volz@stolaf.edu>
2016-08-17 23:20:26 -0400

    create the SIS views

--- views/sis/index.js
+++ views/sis/index.js
@@ -5,35 +5,11 @@
  */
 
 import React from 'react'
-import {
-  StyleSheet,
-  View,
-} from 'react-native'
+import {StyleSheet, Navigator} from 'react-native'
+
 import NavigatorScreen from '../components/navigator-screen'
-
-// let financials = 'https://www.stolaf.edu/sis/st-financials.cfm'
-// let sis = 'https://www.stolaf.edu/sis/login.cfm'
-// let olecard = 'https://www.stolaf.edu/apps/olecard/checkbalance/authenticate.cfm'
-
-// import Frisbee from 'frisbee'
-// const api = new Frisbee({
-//   baseURI: 'https://www.stolaf.edu',
-// })
-
-// import buildFormData from './formdata'
-
-// import {saveLoginCredentials, loadLoginCredentials, clearLoginCredentials} from './loginstuff'
-
-// async function weeklyMealsRemaining() {
-//   let {username, password} = await loadLoginCredentials()
-//   let form = buildFormData({
-//     username: username,
-//     password: password,
-//   })
-//   let page = await api.post('/apps/olecard/checkbalance/authenticate.cfm', {body: form})
-//   console.log(page)
-//   return page
-// }
+import TabbedView from '../components/tabbed-view'
+import tabs from './tabs'
 
 const styles = StyleSheet.create({
   container: {
@@ -41,25 +17,14 @@ const styles = StyleSheet.create({
   },
 })
 
-export default class SISView extends React.Component {
-  componentWillMount() {
+export default function SISView({navigator}: {navigator: typeof Navigator}) {
+  return <NavigatorScreen
+    navigator={navigator}
+    title='SIS'
+    renderScene={() => <TabbedView style={styles.container} tabs={tabs} />}
+  />
+}
 
-  }
-
-  // Render a given scene
-  renderScene() {
-    return (
-      <View style={styles.container}>
-        {/*<SISLoginSection />*/}
-      </View>
-    )
-  }
-
-  render() {
-    return <NavigatorScreen
-      {...this.props}
-      title='SIS'
-      renderScene={this.renderScene.bind(this)}
-    />
-  }
+SISView.propTypes = {
+  navigator: React.PropTypes.instanceOf(Navigator).isRequired,
 }
