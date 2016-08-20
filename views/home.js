d9822ed77c5ca2b9a4b41128512136a38ca26dfc
Elijah Verdoorn <elijahverdoorn@gmail.com>
2016-08-20 02:03:45 -0500

    Change oleville icon

--- views/home.js
+++ views/home.js
@@ -35,7 +35,7 @@ const views: ViewType[] = [
   {usable: true, view: 'ContactsView', title: 'Important Contacts', icon: 'phone'},
   {usable: false, view: 'TransportationView', title: 'Transportation', icon: 'address'},
   {usable: true, view: 'DictionaryView', title: 'Campus Dictionary', icon: 'open-book'},
-  {usable: true, view: 'OlevilleView', title: 'Oleville', icon: 'youtube'},
+  {usable: true, view: 'OlevilleView', title: 'Oleville', icon: 'mouse-pointer'},
 ]
 
 const buttonStyles = StyleSheet.create({
