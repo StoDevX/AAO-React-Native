a78d155e18fb8006a01af4d9f4ab1590c656d64f
Elijah Verdoorn <elijahverdoorn@gmail.com>
2016-08-24 02:07:07 -0500

    Add iOS notifications
    Using Cocoapods to add the code driving the OneSignal service for the notificaitons. The backend is registering the simulator that I'm using for iOS testing, but since simulators cannot get notificaitons I haven't seen the end-to-end process work yet. I'll be able to do that within the day and will make an update if it's not working as expected.

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,33 @@
 import {AppRegistry} from 'react-native'
 import App from './app'
+import OneSignal from 'react-native-onesignal'
 AppRegistry.registerComponent('AllAboutOlaf', () => App)
+
+let pendingNotifications = [];
+// var _navigator; // If applicable, declare a variable for accessing your navigator object to handle payload.
+// function handleNotification (notification) { // If you want to handle the notifiaction with a payload.
+    // _navigator.to('main.post', notification.data.title, {
+    //  article: {
+    //    title: notification.data.title,
+    //    link: notification.data.url,
+    //    action: notification.data.actionSelected
+    //  }
+    //});
+// }
+
+OneSignal.configure({
+    onIdsAvailable: function(device) {
+        console.log('UserId = ', device.userId);
+        console.log('PushToken = ', device.pushToken);
+    },
+  onNotificationOpened: function(message, data, isActive) {
+      var notification = {message: message, data: data, isActive: isActive};
+      console.log('NOTIFICATION OPENED: ', notification);
+      //if (!_navigator) { // Check if there is a navigator object. If not, waiting with the notification.
+      //    console.log('Navigator is null, adding notification to pending list...');
+          pendingNotifications.push(notification);
+      //    return;
+      // }
+      handleNotification(notification);
+  }
+})
---
+++ ios/Pods/Target Support Files/Pods-AllAboutOlafTests/Pods-AllAboutOlafTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AllAboutOlafTests : NSObject
+@end
+@implementation PodsDummy_Pods_AllAboutOlafTests
+@end
