1e82d2ab407fb1d2a6d7795c1276e1074a236aa6
Elijah Verdoorn <elijahverdoorn@gmail.com>
2016-08-25 23:23:54 -0500

    Add error handling for a user not having credentials entered for SIS view
    Add a button that redirects them to settings, with an error message.

--- views/sis/index.js
+++ views/sis/index.js
@@ -5,20 +5,66 @@
  */
 
 import React from 'react'
-import {StyleSheet} from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Navigator,
+} from 'react-native'
 
 import TabbedView from '../components/tabbed-view'
+import Button from 'react-native-button'
 import tabs from './tabs'
-
+import {loadLoginCredentials} from '../../lib/login'
+import * as c from '../components/colors'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  error: {
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  errorButton: {
+    backgroundColor: c.denim,
+    width: 200,
+    color: c.white,
+    alignSelf: 'center',
+    height: 30,
+    paddingTop: 3,
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius: 6,
+    overflow: 'hidden',
+  },
 })
 
-export default function SISView() {
-  return <TabbedView style={styles.container} tabs={tabs} />
+export default function SISView({navigator, route}: {navigator: typeof Navigator, route: Object}) {
+  // Check if the user has a valid username and password
+  let {username, password} = loadLoginCredentials()
+  if (username && password) {
+    return <TabbedView style={styles.container} tabs={tabs} />
+  } else {
+    return (
+      <View style={styles.error}>
+        <Text>Sorry, we couldn't find your login credentials. Did you set them up in the settings?</Text>
+        <Button
+          onPress={() =>
+            navigator.push({
+              id: 'SettingsView',
+              index: route.index + 1,
+              title: 'Settings',
+            })
+          }
+          style={styles.errorButton}
+        >Open Settings</Button>
+      </View>
+    )
+  }
 }
 
 SISView.propTypes = {
+  navigator: React.PropTypes.instanceOf(Navigator).isRequired,
+  route: React.PropTypes.object.isRequired,
 }
