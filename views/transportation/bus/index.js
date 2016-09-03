47a44c5c48b5c69bfc0d495eb9e2d4d1091263d4
Hawken Rives <hawkrives@gmail.com>
2016-09-03 19:46:13 +0900

    refresh the bus view every second, so the times reflect reality

--- views/transportation/bus/index.js
+++ views/transportation/bus/index.js
@@ -23,15 +23,35 @@ let styles = StyleSheet.create({
   },
 })
 
-export default function BusView() {
-  return (
-    <ScrollView contentInset={{bottom: 49}} automaticallyAdjustContentInsets={false}>
-      {busInfo.map((busLine: BusLineType) =>
-        <View key={busLine.line}>
-          <Text style={styles.busLine}>{busLine.line}</Text>
-          <BusLineView schedule={busLine.schedule} />
-        </View>
-      )}
-    </ScrollView>
-  )
+export default class BusView extends React.Component {
+  state = {
+    now: Date.now(),
+  }
+
+  componentWillMount() {
+    // This updates the screen every second, so that the "next bus" times are
+    // updated without needing to leave and come back.
+    this.setState({intervalId: setInterval(this.updateTime, 1000)})
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.state.intervalId)
+  }
+
+  updateTime = () => {
+    this.setState({now: Date.now()})
+  }
+
+  render() {
+    return (
+      <ScrollView contentInset={{bottom: 49}} automaticallyAdjustContentInsets={false}>
+        {busInfo.map((busLine: BusLineType) =>
+          <View key={busLine.line}>
+            <Text style={styles.busLine}>{busLine.line}</Text>
+            <BusLineView schedule={busLine.schedule} />
+          </View>
+        )}
+      </ScrollView>
+    )
+  }
 }
