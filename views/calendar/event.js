eecbb201036941b7c97eea740aad78d0e306d9ea
Hawken Rives <hawkrives@gmail.com>
2016-09-04 20:53:48 +0900

    group events by day

--- views/calendar/event.js
+++ views/calendar/event.js
@@ -5,52 +5,50 @@ import {
   Text,
 } from 'react-native'
 
-import moment from 'moment-timezone'
 import * as c from '../components/colors'
 import {getText, parseHtml} from '../../lib/html'
 
 let styles = StyleSheet.create({
+  container: {
+    paddingTop: 10,
+    paddingBottom: 6,
+    paddingLeft: 10,
+  },
   itemTitle: {
     color: c.black,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingTop: 10,
     paddingBottom: 3,
     fontSize: 16,
-    textAlign: 'left',
+    fontWeight: '500',
   },
   itemPreview: {
     color: c.iosText,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 6,
     fontSize: 13,
-    textAlign: 'left',
   },
 })
 
 // PROPS: eventTitle, location, startTime, endTime
-export default function EventView(props: {eventTitle: string, location: string, startTime?: string, endTime?: string, style?: any}) {
+export default function EventView(props: {eventTitle: string, location: string, startTime?: Object, endTime?: Object, style?: any}) {
   let title = getText(parseHtml(props.eventTitle))
-
-  let stString = moment(props.startTime).format('M/D h:mma')
-  let etString = moment(props.endTime).format('M/D h:mma')
 
   let showTimes = props.startTime && props.endTime
   let showLocation = Boolean(props.location)
 
   return (
-    <View style={props.style}>
+    <View style={[styles.container, props.style]}>
       <Text style={styles.itemTitle}>{title}</Text>
-      { showTimes ? <Text style={styles.itemPreview}>{stString} - {etString}</Text> : null }
-      { showLocation ? <Text style={styles.itemPreview}>{props.location}</Text> : null }
+      { showTimes ? <Text style={styles.itemPreview}>
+        {props.startTime.format('M/D h:mma')} — {props.endTime.format('M/D h:mma')}
+      </Text> : null }
+      { showLocation ? <Text style={styles.itemPreview}>
+        {props.location}
+      </Text> : null }
     </View>
   )
 }
 EventView.propTypes = {
-  endTime: React.PropTypes.string,
+  endTime: React.PropTypes.object,
   eventTitle: React.PropTypes.string.isRequired,
   location: React.PropTypes.string,
-  startTime: React.PropTypes.string,
+  startTime: React.PropTypes.object,
   style: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.object]),
 }
