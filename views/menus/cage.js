b6041147afd3909bdd88750a71d1948c32244d4d
Matt Kilens <kilens@stolaf.edu>
2016-10-27 10:05:35 -0500

    Added native menu view for Stav and The Cage. FoodItems include Bon App's dietary labels.
    The Stav tab shows lunch until the lunch closing time and will show dinner after.

--- views/menus/cage.js
+++ views/menus/cage.js
@@ -1,48 +1,190 @@
 // @flow
+
 import React from 'react'
 import {
   StyleSheet,
   View,
   ListView,
+  Text,
+  ActivityIndicator,
 } from 'react-native'
-import MenuSection from './menuSection'
 
-import menu from '../../data/cage-menu.json'
+import FoodItem from './foodItem'
 
-import type {MenuSectionType} from './types'
+import DietaryFilters from './dietaryFilters'
+const dietaryFilters = DietaryFilters()
 
-let styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'stretch',
-    flexDirection: 'row',
-  },
-})
 
 export default class CageMenuView extends React.Component {
-  state = {
-    dataSource: new ListView.DataSource({
-      rowHasChanged: this._rowHasChanged,
-    }).cloneWithRows(menu),
+  constructor(props){
+    super(props)
+    let getSectionData = (dataBlob, sectionID) => {
+      return dataBlob[sectionID]
+    }
+
+    let getRowData = (dataBlob, sectionID, rowID) => {
+      return dataBlob[rowID]
+    }
+
+    this.state = {
+      loaded: false,
+      dataSource: new ListView.DataSource({
+        getSectionData: getSectionData,
+        getRowData: getRowData,
+        rowHasChanged: (row1, row2) => row1 !== row2,
+        sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+      }),
+    }
   }
 
-  _rowHasChanged(r1: MenuSectionType, r2: MenuSectionType) {
-    return r1.name !== r2.name
+  componentDidMount(){
+    this.fetchData()
   }
 
-  _renderRow(data: MenuSectionType) {
+  filterItems(data, currentStation){
+    let stationItems = []
+    for (let key in data){
+      if (data.hasOwnProperty(key)){
+        let item = data[key]
+        if (item.station.includes(currentStation) && item.special == 1){
+          let temp = {
+            name: item.label,
+            dietary: item.cor_icon,
+          }
+
+          stationItems.push(temp)
+        }
+      }
+    }
+    return stationItems
+  }
+
+  buildStationList(items){
+    let stationList = []
+    for (let item in items){
+      if (items.hasOwnProperty(item)){
+        let station = items[item].station
+        station = station.substring(station.indexOf('@')+1, station.lastIndexOf('<'))
+        if (stationList.indexOf(station) < 0){
+          stationList.push(station)
+        }
+      }
+    }
+    return stationList
+  }
+
+  fetchData () {
+    fetch('http://legacy.cafebonappetit.com/api/2/menus?cafe=262').then(response => response.json()).then(responseData => {
+      let items = responseData.items
+
+      let stations = this.buildStationList(items)
+
+      const dataBlob = {}
+      const sectionIDs = []
+      const rowIDs = []
+
+
+      for (let sectionId = 0; sectionId < stations.length; sectionId++) {
+        let currentStation = stations[sectionId]
+        let stationItems = this.filterItems(items, currentStation)
+
+        if (stationItems.length > 0) {
+          sectionIDs.push(sectionId)
+
+          dataBlob[sectionId] = { station: currentStation }
+
+          rowIDs.push([])
+
+          for (let i = 0; i < stationItems.length; i++) {
+
+            const rowId = `${sectionId}:${i}`
+
+            rowIDs[rowIDs.length - 1].push(rowId)
+
+            dataBlob[rowId] = stationItems[i]
+          }
+        }
+      }
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlob, sectionIDs, rowIDs),
+        loaded: true,
+      })
+
+    }).done()
+  }
+
+
+
+  renderLoadingView() {
     return (
-      <MenuSection header={data.name} content={data.items} subText={data.subtext} />
-    )
+      <View style={styles.header}>
+      <View style={styles.container}>
+      <ActivityIndicator
+        animating={!this.state.loaded}
+        style={[styles.activityIndicator, {height: 80}]}
+        size='large'
+      />
+      </View>
+      </View>
+      )
+  }
+
+
+  renderSectionHeader(sectionData){
+    return (
+      <View style={styles.sectionHeader}>
+      <Text style={styles.sectionHeaderText}>{sectionData.station}</Text>
+      </View>
+      )
+  }
+
+  renderFoodItem(rowData){
+    return (
+      <FoodItem data={rowData} filters={dietaryFilters} />
+      )
+  }
+
+  renderListView() {
+    return (
+      <View style={styles.container}>
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={this.renderFoodItem}
+        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+        renderSectionHeader={this.renderSectionHeader}
+      />
+      </View>
+      )
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={this._renderRow.bind(this)} />
-      </View>
-    )
+    if (!this.state.loaded) {
+      return this.renderLoadingView()
+    }
+
+    return this.renderListView()
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 50,
+  },
+  sectionHeader: {
+    flex: 1,
+    padding: 8,
+    justifyContent: 'center',
+    backgroundColor: '#EAEAEA',
+  },
+  sectionHeaderText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#8E8E8E',
+  },
+})
--- views/menus/stav.js
+++ views/menus/stav.js
@@ -4,67 +4,194 @@ import React from 'react'
 import {
   StyleSheet,
   View,
-  NetInfo,
-  WebView,
+  ListView,
+  Text,
+  ActivityIndicator,
 } from 'react-native'
+
+import FoodItem from './foodItem'
+
+import moment from 'moment-timezone'
+const CENTRAL_TZ = 'America/Winnipeg'
+
+import DietaryFilters from './dietaryFilters'
+const dietaryFilters = DietaryFilters()
+
+import buildingHours from '../../data/building-hours.json'
+
+
+export default class StavMenuView extends React.Component {
+  constructor(props){
+    super(props)
+    let getSectionData = (dataBlob, sectionID) => {
+      return dataBlob[sectionID]
+    }
+
+    let getRowData = (dataBlob, sectionID, rowID) => {
+      return dataBlob[rowID]
+    }
+
+    this.state = {
+      loaded: false,
+      dataSource: new ListView.DataSource({
+        getSectionData: getSectionData,
+        getRowData: getRowData,
+        rowHasChanged: (row1, row2) => row1 !== row2,
+        sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+      }),
+    }
+  }
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+  getTodaysLunchHours(currentDay){
+    for (let i = 0; i < buildingHours.length; i++){
+      if (buildingHours[i].name=='Stav Hall Lunch'){
+        return buildingHours[i].times.hours[currentDay][1]
+      }
+    }
+  }
+
+  whichMeal(){
+    let current = moment().tz(CENTRAL_TZ)
+    let currentTime = current.format('H:mm')
+    let lunchClose = this.getTodaysLunchHours(current.format('ddd'))
+
+    if (currentTime < lunchClose){
+      return 1
+    } else {
+      return 2
+    }
+  }
+
+  buildStation(station, items){
+    let stationItems = []
+    for (let i = 0; i < station.items.length; i++){
+      let food = items[station.items[i]]
+      if (food.special == 1){
+        let temp = {
+          name: food.label,
+          dietary: food.cor_icon,
+        }
+
+        stationItems.push(temp)
+      }
+    }
+    return stationItems
+  }
+
+
+  fetchData () {
+    fetch('http://legacy.cafebonappetit.com/api/2/menus?cafe=261').then(response => response.json()).then(responseData => {
+      let items = responseData.items   
+
+      const dataBlob = {}
+      const sectionIDs = []
+      const rowIDs = []
+
+      let meal = responseData.days[0].cafes[261].dayparts[0][this.whichMeal()]
+
+      let stations = meal.stations
+
+      for (let sectionId = 0; sectionId < stations.length; sectionId++){
+        let stationItems = this.buildStation(stations[sectionId], items)
+
+        if (stationItems.length > 0) {
+          sectionIDs.push(sectionId)
+
+
+          dataBlob[sectionId] = { station: stations[sectionId].label }
+
+          rowIDs.push([])
+
+          for (let j = 0; j < stationItems.length; j++) {
+
+            const rowId = `${sectionId}:${j}`
+
+            rowIDs[rowIDs.length - 1].push(rowId)
+
+            dataBlob[rowId] = stationItems[j]
+          }
+        }
+      }
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlob, sectionIDs, rowIDs),
+        loaded: true,
+      })
+
+    }).done()
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.header}>
+      <View style={styles.container}>
+      <ActivityIndicator
+        animating={!this.state.loaded}
+        style={[styles.activityIndicator, {height: 80}]}
+        size='large'
+      />
+      </View>
+      </View>
+      )
+  }
+
+  renderSectionHeader(sectionData){
+    return (
+      <View style={styles.sectionHeader}>
+      <Text style={styles.sectionHeaderText}>{sectionData.station}</Text>
+      </View>
+      )
+  }
+
+  renderFoodItem(rowData){
+    return (
+      <FoodItem data={rowData} filters={dietaryFilters} />
+      )
+  }
+  renderListView() {
+    return (
+      <View style={styles.container}>
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={this.renderFoodItem}
+        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+        renderSectionHeader={this.renderSectionHeader}
+      />
+      </View>
+      )
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView()
+    }
+
+    return this.renderListView()
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingBottom: 50,
   },
-  webView: {
+  sectionHeader: {
     flex: 1,
+    padding: 8,
+    justifyContent: 'center',
+    backgroundColor: '#EAEAEA',
+  },
+  sectionHeaderText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#8E8E8E',
   },
 })
-
-const URL = 'http://stolaf.cafebonappetit.com/cafe/stav-hall#Lunch'
-// const DEFAULT_URL = 'http://legacy.cafebonappetit.com/print-menu/cafe/261/menu/112732/days/today/pgbrks/0/'
-
-export default class StavMenuView extends React.Component {
-  state = {
-    isConnected: true,
-    url: URL,
-    scalesPageToFit: true,
-  }
-
-  componentWillMount() {
-    NetInfo.isConnected.fetch().then(isConnected => {
-      this._onConnectivityChange(isConnected)
-    })
-    NetInfo.isConnected.addEventListener('change', this._onConnectivityChange.bind(this))
-  }
-
-  componentWillUnmount() {
-    NetInfo.isConnected.removeEventListener('change', this._onConnectivityChange.bind(this))
-  }
-
-  _onConnectivityChange(isConnected: boolean) {
-    this.setState({isConnected})
-  }
-
-  onShouldStartLoadWithRequest() {
-    return true
-  }
-
-  onNavigationStateChange(navState: {url: string}) {
-    this.setState({url: navState.url})
-  }
-
-  render() {
-    let webViewSource = {uri: this.state.url}
-    return (
-      <View style={styles.container}>
-        <WebView
-          automaticallyAdjustContentInsets={false}
-          style={styles.webView}
-          source={webViewSource}
-          javaScriptEnabled={true}
-          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
-          onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest.bind(this)}
-          startInLoadingState={true}
-          scalesPageToFit={true}
-        />
-      </View>
-    )
-  }
-}
