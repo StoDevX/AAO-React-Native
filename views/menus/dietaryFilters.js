cd7d5b0e383c51b77e29021b20a935d15ed7f649
mattk410 <mattk410@users.noreply.github.com>
2016-11-05 23:17:45 -0500

    Revamp menu (#296)
    * Added native menu view for Stav and The Cage. FoodItems include Bon App's dietary labels.
    
    The Stav tab shows lunch until the lunch closing time and will show dinner after.
    
    * Logic fixes
    
    * Clean up code
    
    * Fix travis errors

---
+++ views/menus/dietaryFilters.js
@@ -0,0 +1,34 @@
+module.exports = {
+  1: {
+    label: 'vegetarian',
+    icon: require('../../data/images/menu-icons/v.png'),
+  },
+  3: {
+    label: 'seafood watch',
+    icon: require('../../data/images/menu-icons/sw.png'),
+  },
+  4: {
+    label: 'vegan',
+    icon: require('../../data/images/menu-icons/vg.png'),
+  },
+  5: {
+    label: 'for your well-being',
+    icon: require('../../data/images/menu-icons/wb.png'),
+  },
+  6: {
+    label: 'farm to fork',
+    icon: require('../../data/images/menu-icons/ff.png'),
+  },
+  7: {
+    label: 'in balance',
+    icon: require('../../data/images/menu-icons/ib.png'),
+  },
+  9: {
+    label: 'made without gluten-containing ingredients',
+    icon: require('../../data/images/menu-icons/mwgci.png'),
+  },
+  11: {
+    label: 'kosher',
+    icon: require('../../data/images/menu-icons/k.png'),
+  },
+}
---
+++ views/menus/foodItem.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+} from 'react-native'
+
+export default class FoodItem extends React.Component {
+  getDietaryTags(){
+    let tags = []
+    for (let key in this.props.filters){
+      if (this.props.data.dietary.hasOwnProperty(key)){
+        tags.push(
+          <Image
+            key={key}
+            source={this.props.filters[key].icon}
+            style={styles.icons}
+          />
+        )
+      }
+    }
+    return tags
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.name}>
+          <Text style={styles.text}>
+            {this.props.data.name}
+          </Text>
+        </View>
+        <View style={styles.iconContainer}>
+          {this.getDietaryTags()}
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 14,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    marginLeft: 12,
+    fontSize: 13,
+    flexWrap: 'wrap',
+  },
+  name: {
+    flex: 2,
+    alignItems: 'stretch',
+  },
+  iconContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+  },
+  icons: {
+    marginLeft: 7,
+    width: 15,
+    height: 15,
+  },
+})
+
+FoodItem.propTypes = {
+  data: React.PropTypes.object.isRequired,
+  filters: React.PropTypes.object.isRequired,
+}
