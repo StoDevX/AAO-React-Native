e1f41240bde264b32138d2b2ca20adad11f77fad
Hawken Rives <hawkrives@gmail.com>
2016-11-09 15:09:41 -0600

    remove blank lines from calendar

--- views/calendar/index.js
+++ views/calendar/index.js
@@ -10,11 +10,9 @@ import {StyleSheet} from 'react-native'
 import TabbedView from '../components/tabbed-view'
 import tabs from './tabs'
 
-
 export default function CalendarPage() {
   return <TabbedView style={styles.container} tabs={tabs} />
 }
-
 
 let styles = StyleSheet.create({
   container: {
