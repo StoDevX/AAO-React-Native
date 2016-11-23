7b4465b92f3217a545e196e69d28e7b7951499d8
Hawken Rives <hawkrives@gmail.com>
2016-11-23 10:24:22 -0600

    disable some disabled tests so flow passes

--- views/building-hours/__tests__/moment.helper.js
+++ views/building-hours/__tests__/moment.helper.js
@@ -2,5 +2,5 @@
 import moment from 'moment-timezone'
 export {moment}
 
-export const dayMoment = time => moment.tz(time, 'ddd H:mm', false, 'America/Winnipeg')
-export const hourMoment = time => moment.tz(time, 'H:mm', false, 'America/Winnipeg')
+export const dayMoment = (time: string) => moment.tz(time, 'ddd H:mm', false, 'America/Winnipeg')
+export const hourMoment = (time: string) => moment.tz(time, 'H:mm', false, 'America/Winnipeg')
