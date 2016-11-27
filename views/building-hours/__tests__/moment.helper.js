96a8721d916aba3054456ed205dc8bab7e4c898e
Hawken Rives <hawkrives@gmail.com>
2016-11-27 16:32:13 -0600

    update moment test helper

--- views/building-hours/__tests__/moment.helper.js
+++ views/building-hours/__tests__/moment.helper.js
@@ -2,5 +2,5 @@
 import moment from 'moment-timezone'
 export {moment}
 
-export const dayMoment = (time: string) => moment.tz(time, 'ddd H:mm', false, 'America/Winnipeg')
-export const hourMoment = (time: string) => moment.tz(time, 'H:mm', false, 'America/Winnipeg')
+export const dayMoment = (time: string) => moment.tz(time, 'ddd h:mma', false, 'America/Winnipeg')
+export const hourMoment = (time: string) => moment.tz(time, 'h:mma', false, 'America/Winnipeg')
