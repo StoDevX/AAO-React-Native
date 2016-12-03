590de43d8ba173895825e9b41ba7f84ba6602a8d
Hawken Rives <hawkrives@gmail.com>
2016-12-03 08:49:25 -0600

    fix hockeysdk installation script

--- scripts/hockeyapp.sh
+++ scripts/hockeyapp.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -ev
 
+# We're adding the HockeySDK pod to the Podfile.
+
+# From http://stackoverflow.com/a/32513340/2347774:
+# -l strips newlines and adds them back in, eliminating the need for "\n"
+# -p loops over the input file, printing every line
+# -e executes the code in single quotes
+# $. is the line number
+
+# yay perl
+
 cd ios || exit 1
-pod install HockeyApp
+perl -lpe "print \"  pod 'HockeySDK'\" if $. == 10" Podfile
+pod install
 cd ..
