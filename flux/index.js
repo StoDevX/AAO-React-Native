9155ff7f1f5e5c8701c978b60bfb7e138eaf52c3
Drew Volz <volz.drew@gmail.com>
2016-12-12 17:04:36 -0500

    Fix blank ordering on init of the Edit Home view (#426)
    * Fixes blank ordering on init of the Edit Home view.
    
    * The order passed in would remain forever blank if we did not initialize it first.
    * This was changed by accident during the redux/flux additions.
    * Adding back a default order in place of a blank array solves this.
    
    * Simplify default view order logic
    
    @hawkrives had a nice suggestion to simplify the mapping logic
    
    * Fixup according to review
    
    Make the boolean comparison a lambda function

--- flux/index.js
+++ flux/index.js
@@ -3,11 +3,13 @@ import createLogger from 'redux-logger'
 import reduxPromise from 'redux-promise'
 import reduxThunk from 'redux-thunk'
 import {AsyncStorage} from 'react-native'
+import {allViews} from '../views/views'
 
 export const SAVE_HOMESCREEN_ORDER = 'SAVE_HOMESCREEN_ORDER'
+let defaultViewOrder = allViews.map(v => v.view)
 
 export const loadHomescreenOrder = async () => {
-  let savedOrder = JSON.parse(await AsyncStorage.getItem('homescreen:view-order')) || []
+  let savedOrder = JSON.parse(await AsyncStorage.getItem('homescreen:view-order')) || defaultViewOrder
   return saveHomescreenOrder(savedOrder)
 }
 
