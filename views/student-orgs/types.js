60fa2c2afe60ea2ad43f4848e5343efd1f6f1004
Hawken Rives <hawkrives@gmail.com>
2016-12-20 17:12:27 -0600

    finished adding all Org types to types.js

--- views/student-orgs/types.js
+++ views/student-orgs/types.js
@@ -1,27 +1,41 @@
 // @flow
-import {Navigator} from 'react-native'
 
 export type StudentOrgAbridgedType = {
-  name: string,
+  campusName: string,
   categories: string[],
+  hasCoverImage: boolean,
+  hasUpcomingEvents: boolean,
+  memberCount: number,
+  name: string,
   newOrg: boolean,
+  orgMember: boolean,
+  photoType?: string,
+  photoUri?: string,
+  photoVersion: string,
+  subdomain: string,
+  uri: string,
 }
 
 export type StudentOrgInfoType = {
-  name: string,
+  campusName: string,
   categories: string[],
-  regularMeetingTime: string,
-  regularMeetingLocation: string,
   contactName: string,
-  description: string,
-}
-
-export type StudentOrgPropsType = {
-  navigator: typeof Navigator,
-  route: Object,
-  url: string,
-}
-
-export type StudentOrgDetailPropsType = {
-  data: Object,
+  description?: string,
+  documentCount: number,
+  facebook: string,
+  forms: any[],
+  hasCoverImage: boolean,
+  hasEmailAddress: boolean,
+  isMember: boolean,
+  isOfficer: boolean,
+  memberCount: number,
+  name: string,
+  photoType?: string,
+  photoUri?: string,
+  photoVersion: string,
+  regularMeetingLocation: string,
+  regularMeetingTime: string,
+  subdomain: string,
+  twitter: string,
+  uri: string,
 }
