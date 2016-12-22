afb2a69d01ae1d5fca5a2182fcbd5ce1cb420aa9
Hawken Rives <hawkrives@gmail.com>
2016-12-22 12:25:13 -0600

    extract LoadingView into NoticeView

--- views/components/loading.js
+++ views/components/loading.js
@@ -1,27 +1,9 @@
 // @flow
 import React from 'react'
-
-import {
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native'
+import {NoticeView} from './notice'
 
 export default function LoadingView({text='Loading…'}: {text?: string}) {
   return (
-    <View style={styles.container}>
-      <Text>
-        {text}
-      </Text>
-    </View>
+    <NoticeView text={text} />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-})
