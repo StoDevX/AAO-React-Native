9b53c972a538eaaf2709bfc5fad0646492e96440
Hawken Rives <hawkrives@gmail.com>
2016-12-22 17:27:01 -0600

    update tests for getSelectedValuesFromListFilter

--- views/components/filter/__tests__/get-values.test.js
+++ views/components/filter/__tests__/get-values.test.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 import {getSelectedValuesFromListFilter as getValues} from '../get-values'
 
-const makeListFilter = ({options=[], value=[]}={}) => ({
+const makeListFilter = (options=[], value=[]) => ({
   type: 'list',
   multiple: true,
   key: 'key',
@@ -26,4 +26,14 @@ it('should return an empty array if no filter is passed', () => {
 it('should return an array for a List filter', () => {
   let filter = makeListFilter()
   expect(getValues(filter)).toBeInstanceOf(Array)
+})
+
+it('should remove selected items from the returned list', () => {
+  let filter = makeListFilter(['a', 'b', 'c'], ['b'])
+  expect(getValues(filter)).toEqual(['a', 'c'])
+})
+
+it("should not return selected items that aren't valid options", () => {
+  let filter = makeListFilter(['a', 'b', 'c'], ['d'])
+  expect(getValues(filter)).toEqual(['a', 'b', 'c'])
 })
