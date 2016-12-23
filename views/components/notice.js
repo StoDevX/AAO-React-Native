1406666ba7bd083d262cd9b2c9764e0cd9b96157
Hawken Rives <hawkrives@gmail.com>
2016-12-22 23:15:45 -0600

    add NoticeView
    NoticeView automatically expands to fill the allotted space, and
    displays a message in the center of that space.
    
    This branch also reimplements LoadingView on top of NoticeView.

---
+++ views/components/notice.js
@@ -0,0 +1,23 @@
+// @flow
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    backgroundColor: '#ffffff',
+  },
+})
+
+export function NoticeView({text, style}: {text: string, style?: any}) {
+  return (
+    <View style={[styles.container, style]}>
+      <Text>
+        {text}
+      </Text>
+    </View>
+  )
+}
