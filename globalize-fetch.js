StoDevX/AAO-React-Native

commit c9edd9515570e403b376ba83997ce785deb3d778

Add global `fetch` helpers (#446)
* add a handy-dandy fetchJson function

* move fetch helpers into the global namespace

globalize-fetch.js
@@ -1,0 +1,24 @@
+// @flow
+
+global.rawFetch = global.fetch
+
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    let error = new Error(response.statusText)
+    // attach the original response to the thrown error
+    ;(error: any).response = response
+    throw error
+  }
+}
+
+function json(response) {
+  return response.json()
+}
+
+// make fetch() calls throw if the server returns a non-200 status code
+global.fetch = (...args: any[]) => global.fetch(...args).then(status)
+
+// add a global fetchJson wrapper
+global.fetchJson = (...args: any[]) => fetch(...args).then(json)

.eslintrc.yaml
@@ -17,6 +17,8 @@ plugins: [babel, react, flowtype]
 globals:
   fetch: true
   FormData: true
+  fetchJson: true
+  rawFetch: true
 
 rules:
   array-callback-return: error

app.js
@@ -3,6 +3,9 @@
  * Index view
  */
 
+// Tweak the global fetch
+import './globalize-fetch'
+
 import React from 'react'
 import {
   Navigator,

flow-typed/aao/fetch.js
@@ -1,0 +1,2 @@
+declare function rawFetch(): any;
+declare function fetchJson(): any;

lib/fetch.js
@@ -1,17 +1,0 @@
-export function status(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response
-  } else {
-    let error = new Error(response.statusText)
-    error.response = response
-    throw error
-  }
-}
-
-export function json(response) {
-  return response.json()
-}
-
-export function text(response) {
-  return response.text()
-}

lib/stalkernet.js
@@ -1,5 +1,4 @@
 import qs from 'querystring'
-import {status, text} from './fetch'
 import uniqBy from 'lodash/uniqBy'
 import xml2js from 'xml2js'
 import toPairs from 'lodash/toPairs'
@@ -75,8 +74,7 @@ function parseStalkernetResults(data) {
 export function query(query) {
   let queryString = buildStalkernetQueryString(query)
   return fetch(`${STALKERNET_URL}?${queryString}`)
-    .then(status)
-    .then(text)
+    .then(r => r.text())
     .then(parseStalkernetResults)
     .catch(err => {
       console.error(err)

views/oleville/index.js
@@ -26,8 +26,6 @@ import zipWith from 'lodash/zipWith'
 const URL = 'http://oleville.com/wp-json/wp/v2/posts?per_page=5'
 const defaultOlevilleImageUrl = 'http://oleville.com/wp-content/uploads/2015/12/Oleville-Logo.png'
 
-const fetchJson = url => fetch(url).then(r => r.json())
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,

commit 59cc4deebe1e9240ea6cbef6247faf7fc2e0bb90

fix infinite recursion error introduced with #446

globalize-fetch.js
@@ -18,7 +18,7 @@ function json(response) {
 }
 
 // make fetch() calls throw if the server returns a non-200 status code
-global.fetch = (...args: any[]) => global.fetch(...args).then(status)
+global.fetch = (...args: any[]) => global.rawFetch(...args).then(status)
 
 // add a global fetchJson wrapper
 global.fetchJson = (...args: any[]) => fetch(...args).then(json)