995717f94f012aac7226caf203295c2a4a7ea0e7
Hawken Rives <hawkrives@gmail.com>
2017-01-02 18:53:20 -0600

    update bundle-data to run automatically on a folder

--- scripts/bundle-data.js
+++ scripts/bundle-data.js
@@ -1,35 +1,40 @@
 #!/usr/bin/env node
+const fs = require('fs')
+const junk = require('junk')
+const path = require('path')
 const bundleDataDir = require('./bundle-data-dir')
 const convertDataFile = require('./convert-data-file')
 
+const isDir = pth => fs.statSync(pth).isDirectory()
+const isFile = pth => fs.statSync(pth).isFile()
+const readDir = pth => fs.readdirSync(pth).filter(junk.not).filter(entry => !entry.startsWith('_'))
+const findDirsIn = pth => readDir(pth).filter(entry => isDir(path.join(pth, entry)))
+const findFilesIn = pth => readDir(pth).filter(entry => isFile(path.join(pth, entry)))
+
+const args = process.argv.slice(2)
+const fromDir = args[0]
+const toDir = args[1]
+if (!fromDir || !toDir || fromDir === '-h' || fromDir === '--help') {
+  console.error('usage: node bundle-data.js <from-dir> <to-dir>')
+  process.exit(1)
+}
+
 // Bundle each directory of yaml files into one big json file
-let dirs = [
-  'building-hours',
-  'bus-times',
-  'contact-info',
-  'dictionary',
-  'map-coordinates',
-  'transportation',
-  'webcams',
-]
+const dirs = findDirsIn(fromDir)
 dirs.forEach(dirname => {
-  console.log(`bundle-data-dir data/${dirname} docs/${dirname}.json`)
-  bundleDataDir({fromDir: `data/${dirname}`, toFile: `docs/${dirname}.json`})
+  const input = path.join(fromDir, dirname)
+  const output = path.join(toDir, dirname) + '.json'
+  console.time(`bundle-data-dir ${input} ${output}`)
+  bundleDataDir({fromDir: input, toFile: output})
+  console.timeEnd(`bundle-data-dir ${input} ${output}`)
 })
 
 // Convert these files into JSON equivalents
-let files = [
-  'breaks.yaml',
-  'chapel.yaml',
-  'credits.yaml',
-  'legal.md',
-  'faqs.md',
-  'map.yaml',
-  'pause-menu.yaml',
-  'privacy.md',
-]
+const files = findFilesIn(fromDir)
 files.forEach(file => {
-  let out = file.replace(/\.(md|yaml)$/, '.json')
-  console.log(`convert-data-file data/${file} docs/${out}`)
-  convertDataFile({fromFile: `data/${file}`, toFile: `docs/${out}`})
+  const input = path.join(fromDir, file)
+  const output = path.join(toDir, file).replace(/\.(.*)$/, '.json')
+  console.time(`convert-data-file ${input} ${output}`)
+  convertDataFile({fromFile: input, toFile: output})
+  console.timeEnd(`convert-data-file ${input} ${output}`)
 })
