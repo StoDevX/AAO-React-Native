a888fc144f3aa13b895e99b9aaa154a0477ef178
Drew Volz <volz.drew@gmail.com>
2017-01-04 12:35:10 -0500

    Switch position of northfield events

--- views/calendar/tabs.js
+++ views/calendar/tabs.js
@@ -10,17 +10,17 @@ export default [
     props: {calendarId: 'le6tdd9i38vgb7fcmha0hu66u9gjus2e%40import.calendar.google.com'},
   },
   {
-    id: 'northfield',
-    title: 'Northfield',
-    rnVectorIcon: {iconName: 'pin'},
-    component: CalendarView,
-    props: {calendarId: 'thisisnorthfield%40gmail.com'},
-  },
-  {
     id: 'oleville',
     title: 'Oleville',
     rnVectorIcon: {iconName: 'happy'},
     component: CalendarView,
     props: {calendarId: 'stolaf.edu_fvulqo4larnslel75740vglvko@group.calendar.google.com'},
+  },
+  {
+    id: 'northfield',
+    title: 'Northfield',
+    rnVectorIcon: {iconName: 'pin'},
+    component: CalendarView,
+    props: {calendarId: 'thisisnorthfield%40gmail.com'},
   },
 ]
