c9702360e4f9316606da0c6bfada06ccf590fe6f
Hawken Rives <hawkrives@gmail.com>
2017-01-08 00:59:48 -0600

    add ListView&co

---
+++ views/components/list/list-separator.js
@@ -0,0 +1,33 @@
+// @flow
+import React from 'react'
+import {Platform, StyleSheet} from 'react-native'
+import {Separator} from '../separator'
+
+const styles = StyleSheet.create({
+  separator: {
+    marginLeft: 15,
+  },
+})
+
+type PropsType = {
+  styles?: any,
+  fullWidth?: boolean,
+  spacing?: {left?: number, right?: number}
+};
+export function ListSeparator(props: PropsType) {
+  if (Platform.OS === 'android') {
+    return null
+  }
+
+  const {
+    fullWidth,
+    spacing: {left: leftSpacing = 15, right: rightSpacing} = {},
+  } = props
+
+  let spacing = {marginLeft: leftSpacing, marginRight: rightSpacing}
+  if (fullWidth) {
+    spacing = {marginLeft: 0, marginRight: 0}
+  }
+
+  return <Separator style={[styles.separator, spacing, props.styles]} />
+}
