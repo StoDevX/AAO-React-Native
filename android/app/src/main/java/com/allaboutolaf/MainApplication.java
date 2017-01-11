62b4d8093e6f4fd2910a133c51f747c8c18ec075
Hawken Rives <hawkrives@gmail.com>
2017-01-11 11:30:49 -0600

    apply internal upgrades for rn@0.40

--- android/app/src/main/java/com/allaboutolaf/MainApplication.java
+++ android/app/src/main/java/com/allaboutolaf/MainApplication.java
@@ -4,16 +4,16 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.psykar.cookiemanager.CookieManagerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.oblador.keychain.KeychainPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage;
 import com.facebook.soloader.SoLoader;
+import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.oblador.keychain.KeychainPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.psykar.cookiemanager.CookieManagerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,11 +30,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-        new RNDeviceInfo(),
         new CookieManagerPackage(),
-        new VectorIconsPackage(),
         new KeychainPackage(),
-        new ReactNativeOneSignalPackage()
+        new ReactNativeOneSignalPackage(),
+        new RNDeviceInfo(),
+        new VectorIconsPackage()
       );
     }
   };
