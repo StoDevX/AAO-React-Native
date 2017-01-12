af9a6853018c3ad76d53907aba00d3f64596885f
Hawken Rives <hawkrives@gmail.com>
2017-01-12 15:49:45 -0600

    Chapter 1, or: the Creation of ListRow&co (#539)
    * add ListView&co
    
    * add iosHeaderTopBorder and iosHeaderBottomBorder colors
    
    slowly but surely we're getting rid of the magic color constants…
    
    * expand type for Separator's style prop

--- views/components/separator.js
+++ views/components/separator.js
@@ -9,6 +9,6 @@ const styles = StyleSheet.create({
   },
 })
 
-export function Separator({style}: {style?: Number|Object|Array<Number|Object>}) {
+export function Separator({style}: {style?: mixed}) {
   return <View style={[styles.separator, style]} />
 }
