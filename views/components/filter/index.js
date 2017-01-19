02275c10b076777bccd07f89a1ff309e8ea4a0d0
Hawken Rives <hawkrives@gmail.com>
2017-01-19 16:52:22 -0600

    shrink the amount of data we send for analytics
    because those menu filters are huge

--- views/components/filter/index.js
+++ views/components/filter/index.js
@@ -2,3 +2,4 @@
 export {FilterView} from './filter-view'
 export type {FilterType} from './types'
 export {applyFiltersToItem} from './apply-filters'
+export {stringifyFilters} from './stringify-filters'
---
+++ views/components/filter/stringify-filters.js
@@ -0,0 +1,30 @@
+/**
+ * @flow
+ * Stringify a set of filters for transfer to GA.
+ * (because we really don't need to ship those descriptions over the network)
+ */
+import type {FilterType, ListType} from './types'
+
+export function stringifyFilters(filters: FilterType[]): string {
+  return filters.map(f => stringifyFilter(f)).join('; ')
+}
+
+function stringifyFilter(filter: FilterType): string {
+  let spec = 'n/a'
+  if (filter.type === 'list') {
+    spec = stringifyListFilter(filter)
+  }
+
+  return JSON.stringify({
+    key: filter.key,
+    type: filter.type,
+    enabled: filter.enabled,
+    spec: spec,
+  })
+}
+
+function stringifyListFilter(filter: ListType): string {
+  // Extract the list of "selected" items
+  const filterValue = filter.spec.selected
+  return filterValue.map(f => f.title).join(', ')
+}
