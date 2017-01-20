4c10f3d074c9505042dd88599c69fc47de717a0e
Hawken Rives <hawkrives@gmail.com>
2017-01-20 10:27:33 -0500

    fix custom dimension tracking in analytics (#606)
    * fix custom dimension tracking in analytics
    
    closes #601
    
    * shrink the amount of data we send for analytics
    
    because those menu filters are huge

--- analytics.js
+++ analytics.js
@@ -4,6 +4,7 @@ import {
   GoogleAnalyticsTracker,
   GoogleAnalyticsSettings,
 } from 'react-native-google-analytics-bridge'
+import {stringifyFilters} from './views/components/filter'
 
 const trackerId = __DEV__ ? 'UA-90234209-1' : 'UA-90234209-2'
 export const tracker = new GoogleAnalyticsTracker(trackerId)
@@ -18,10 +19,10 @@ disableIfOptedOut()
 
 // Google requires that custom dimensions be tracked by index, and we only get
 // 20 custom dimensions, so I decided to centralize them here.
-export function trackMenuFilters(menuName: string, filters: mixed) {
-  tracker.trackEvent('menus', 'filter', {label: menuName}, {'1': JSON.stringify(filters)})
+export function trackMenuFilters(menuName: string, filters: any) {
+  tracker.trackEventWithCustomDimensionValues('menus', 'filter', {label: menuName}, {'1': stringifyFilters(filters)})
 }
 
 export function trackHomescreenOrder(order: string[]) {
-  tracker.trackEvent('homescreen', 'reorder', {}, {'2': order.join(', ')})
+  tracker.trackEventWithCustomDimensionValues('homescreen', 'reorder', {}, {'2': order.join(', ')})
 }
