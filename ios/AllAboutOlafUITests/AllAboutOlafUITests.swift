3dbb9541ed110f5efa2707fc0c5771c29dbf69cd
Hawken Rives <hawkrives@gmail.com>
2017-01-26 16:54:09 -0600

    remove xcode's trailing whitespace

--- ios/AllAboutOlafUITests/AllAboutOlafUITests.swift
+++ ios/AllAboutOlafUITests/AllAboutOlafUITests.swift
@@ -9,12 +9,12 @@
 import XCTest
 
 class All_About_Olaf_UI_Tests: XCTestCase {
-        
+
     override func setUp() {
         super.setUp()
-        
+
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -26,7 +26,7 @@ class All_About_Olaf_UI_Tests: XCTestCase {
 
         XCUIDevice.shared().orientation = .portrait
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
