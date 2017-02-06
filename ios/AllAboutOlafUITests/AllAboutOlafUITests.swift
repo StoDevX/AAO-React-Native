a6423dbf78e873f6b2174f8516c9b25bd329fcc8
Hawken Rives <hawkrives@gmail.com>
2017-02-06 12:15:22 -0600

    remove some spaces from UITests.swift

--- ios/AllAboutOlafUITests/AllAboutOlafUITests.swift
+++ ios/AllAboutOlafUITests/AllAboutOlafUITests.swift
@@ -8,22 +8,22 @@
 
 import XCTest
 
-class All_About_Olaf_UI_Tests: XCTestCase {
-
+class AllAboutOlafUITests: XCTestCase {
     override func setUp() {
         super.setUp()
-
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        // In UI tests it is usually best to stop immediately when a failure
+        // occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+
+        // UI tests must launch the application that they test. Doing this in
+        // setup will make sure it happens for each test method.
         let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-
+        // In UI tests it’s important to set the initial state - such as
+        // interface orientation - required for your tests before they run.
+        // The setUp method is a good place to do this.
         XCUIDevice.shared().orientation = .portrait
     }
 
