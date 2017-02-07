b65a04709a06cdeeff205e24d12d9e1d70ac7017
Hawken Rives <hawkrives@gmail.com>
2017-02-07 13:46:23 -0600

    add things to dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,11 +1,12 @@
 import { danger, fail, warn } from "danger"
-import fs from "fs"
+import { readFileSync } from "fs"
+import execa from "execa"
 
 const jsFiles = danger.git.created_files.filter(path => path.endsWith("js"))
 
 // new js files should have `@flow` at the top
 const unFlowedFiles = jsFiles.filter(filepath => {
-  const content = fs.readFileSync(filepath)
+  const content = readFileSync(filepath, 'utf-8')
   return !content.includes("@flow")
 })
 
@@ -13,10 +14,34 @@ if (unFlowedFiles.length > 0) {
   warn(`These new JS files do not have Flow enabled: ${unFlowedFiles.join(", ")}`)
 }
 
-const packageChanged = danger.git.modified_files.includes('package.json')
-const lockfileChanged = danger.git.modified_files.includes('yarn.lock')
-if (packageChanged && !lockfileChanged) {
-  const message = 'Changes were made to package.json, but not to yarn.lock'
-  const idea = 'Perhaps you need to run `yarn install`?'
-  warn(`${message} - <i>${idea}</i>`)
+// revisit this when we move to yarn
+// const packageChanged = danger.git.modified_files.includes('package.json')
+// const lockfileChanged = danger.git.modified_files.includes('yarn.lock')
+// if (packageChanged && !lockfileChanged) {
+//   const message = 'Changes were made to package.json, but not to yarn.lock'
+//   const idea = 'Perhaps you need to run <code>yarn install</code>?'
+//   warn(`${message} - <i>${idea}</i>`)
+// }
+
+// Be careful of leaving testing shortcuts in the codebase
+const jsTests = jsFiles.filter(filepath => filepath.endsWith('test.js'))
+jsTests.forEach(file => {
+  const content = readFileSync(file, 'utf-8')
+  if (content.includes('it.only') || content.includes('describe.only')) {
+    fail(`An <code>only</code> was left in ${file} – that prevents any other tests from running.`)
+  }
+})
+
+// Warn when PR size is large (mainly for hawken)
+const bigPRThreshold = 400
+const thisPRSize = github.pr.additions + github.pr.deletions
+if (thisPRSize > bigPRThreshold) {
+  warn(':exclamation: Big PR!')
+  markdown(`> The Pull Request size is a bit big. We like to try and keep PRs under ${bigPRThreshold} lines per PR, and this one was ${thisPRSize} lines. If the PR contains multiple logical changes, splitting each into separate PRs will allow a faster, easier, and more thorough review.`)
 }
+
+// wishlist:
+// - `flow check` output
+// - `eslint` output
+// - `validate-data` output
+// - results of `bundle-data`
