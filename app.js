65d1ecd25175e20df22e277cbf930142474fadfc
Hawken Rives <hawkrives@gmail.com>
2017-02-11 12:34:46 -0600

    fix grammar in app.js onesignal stuff

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ class App extends React.Component {
   }
 
   onRegistered(notifData) {
-    console.log('Device had been registered for push notifications!', notifData)
+    console.log('Device is now registered for push notifications!', notifData)
   }
 
   onIds(device) {
