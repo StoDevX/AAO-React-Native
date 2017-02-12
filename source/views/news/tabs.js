94bcecd2192c5302dfeffde1539095fc401eaf1f
Hawken Rives <hawkrives@gmail.com>
2017-02-11 18:38:05 -0600

    add KstoNewsView

--- source/views/news/tabs.js
+++ source/views/news/tabs.js
@@ -48,4 +48,16 @@ export default [
       name: 'PoliticOle',
     },
   },
+  {
+    id: 'KstoNewsView',
+    title: 'KSTO',
+    rnVectorIcon: {iconName: 'radio'},
+    component: NewsContainer,
+    props: {
+      mode: 'wp-json',
+      url: 'https://pages.stolaf.edu/ksto/wp-json/wp/v2/posts/',
+      query: {'per_page': 10, _embed: true},
+      name: 'KSTO',
+    },
+  },
 ]
