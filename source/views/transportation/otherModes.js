e5cd40ae030ff483cffaa26bba77ebc8cd3865c1
Hawken Rives <hawkrives@gmail.com>
2017-02-21 17:58:37 -0600

    port OtherModes to SimpleListView

--- source/views/transportation/otherModes.js
+++ source/views/transportation/otherModes.js
@@ -4,13 +4,13 @@ import {
   Text,
   StyleSheet,
   View,
-  ListView,
   Linking,
 } from 'react-native'
 import type {OtherModeType} from './types'
 import {data as modes} from '../../../docs/transportation.json'
 import * as c from '../components/colors'
-import Button from 'react-native-button' // the button
+import {Button} from '../components/button'
+import SimpleListView from '../components/listview'
 import {tracker} from '../../analytics'
 
 let styles = StyleSheet.create({
@@ -51,41 +51,26 @@ let styles = StyleSheet.create({
   },
 })
 
-export default class OtherModesView extends React.Component {
-  state = {
-    dataSource: new ListView.DataSource({
-      rowHasChanged: this._rowHasChanged,
-    }).cloneWithRows(modes),
-  }
-
-  _rowHasChanged(r1: OtherModeType, r2: OtherModeType) {
-    return r1.name !== r2.name
-  }
-
-  _renderRow(data: OtherModeType) {
-    return (
-      <View style={styles.mode}>
-        <Text style={styles.title}>{data.name}</Text>
-        <Text style={styles.content}>{data.description}</Text>
-        <Button
-          onPress={() => Linking.openURL(data.url).catch(err => {
-            tracker.trackException(err.message)
-            console.error('An error occurred', err)
-          })}
-          style={styles.button}>
-          More info
-        </Button>
-      </View>
-    )
-  }
-
-  render() {
-    return (
-      <ListView
-        contentContainerStyle={styles.container}
-        dataSource={this.state.dataSource}
-        renderRow={this._renderRow.bind(this)}
-      />
-    )
-  }
+export default function OtherModesView() {
+  return (
+    <SimpleListView
+      contentContainerStyle={styles.container}
+      forceBottomInset={true}
+      data={modes}
+    >
+      {(data: OtherModeType) =>
+        <View style={styles.mode}>
+          <Text style={styles.title}>{data.name}</Text>
+          <Text style={styles.content}>{data.description}</Text>
+          <Button
+            title='More info'
+            onPress={() => Linking.openURL(data.url).catch(err => {
+              tracker.trackException(err.message)
+              console.error('An error occurred', err)
+            })}
+            style={styles.button}
+          />
+        </View>}
+    </SimpleListView>
+  )
 }
