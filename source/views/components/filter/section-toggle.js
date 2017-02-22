a14c64b7a64ad9e50f12f46354fe3883a423b375
Hawken Rives <hawkrives@gmail.com>
2017-02-22 09:26:41 -0600

    replace SectionToggle's CustomCell with Cell

--- source/views/components/filter/section-toggle.js
+++ source/views/components/filter/section-toggle.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import type {ToggleType} from './types'
 import {Text, Switch} from 'react-native'
-import {Section, CustomCell} from 'react-native-tableview-simple'
+import {Section, Cell} from 'react-native-tableview-simple'
 
 type PropsType = {
   filter: ToggleType,
@@ -14,10 +14,10 @@ export function SingleToggleSection({filter, onChange}: PropsType) {
   const {title='', caption, label} = spec
   return (
     <Section header={title.toUpperCase()} footer={caption}>
-      <CustomCell>
-        <Text style={{flex: 1, fontSize: 16}}>{label}</Text>
-        <Switch value={enabled} onValueChange={val => onChange({...filter, enabled: val})} />
-      </CustomCell>
+      <Cell
+        cellContentView={<Text style={{flex: 1, fontSize: 16}}>{label}</Text>}
+        cellAccessoryView={<Switch value={enabled} onValueChange={val => onChange({...filter, enabled: val})} />}
+      />
     </Section>
   )
 }
