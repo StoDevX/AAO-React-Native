ccdba60c3913c360d91e482a10abbbe375e91e28
Hawken Rives <hawkrives@gmail.com>
2017-02-22 18:18:36 -0600

    s/trim/strip/

--- dangerfile.js
+++ dangerfile.js
@@ -47,13 +47,13 @@ if (thisPRSize > bigPRThreshold) {
 const codeBlock = (contents, lang=null) => `\`\`\`${lang || ''}\n${contents}\n\`\`\``
 
 // Eslint
-const eslintLog = readFileSync('logs/eslint').trim()
-const dataValidationLog = readFileSync('logs/validate-data').trim()
-const dataBundlingStatusLog = readFileSync('logs/bundle-data').trim()
-const flowLog = readFileSync('logs/flow').trim()
-const iosJsBundleLog = readFileSync('logs/bundle-ios').trim()
-const androidJsBundleLog = readFileSync('logs/bundle-android').trim()
-const jestLog = readFileSync('logs/jest').trim()
+const eslintLog = readFileSync('logs/eslint').strip()
+const dataValidationLog = readFileSync('logs/validate-data').strip()
+const dataBundlingStatusLog = readFileSync('logs/bundle-data').strip()
+const flowLog = readFileSync('logs/flow').strip()
+const iosJsBundleLog = readFileSync('logs/bundle-ios').strip()
+const androidJsBundleLog = readFileSync('logs/bundle-android').strip()
+const jestLog = readFileSync('logs/jest').strip()
 
 if (eslintLog) {
   warn('Eslint had a thing to say!')
