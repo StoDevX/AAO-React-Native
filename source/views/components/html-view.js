c8277aff11124175e8596b7088636d032d0d0e65
Hawken Rives <hawkrives@gmail.com>
2017-02-24 08:37:15 -0600

    Extract openUrl from NewsItem (#757)
    * extract opener functions from NewsItem
    
    * extract HtmlView into components/
    
    * * Change Directory and Maps to use SFViewController/Custom Tabs
    * Use native browsers in Transportation's buttons
    
    * nix unused GA import (again)
    
    * add support for URLs to home screen buttons (#758)
    
    * use openUrl/SFViewController/CustomTabs instead of WebView in Transportation (#759)
    
    * Remove "this"es
    
    * Update views.js

---
+++ source/views/components/html-view.js
@@ -0,0 +1,35 @@
+// @flow
+
+import React from 'react'
+import {WebView} from 'react-native'
+import openUrl, {canOpenUrl} from '../components/open-url'
+
+export class HtmlView extends React.Component {
+  props: {
+    html: string,
+  }
+  _webview: WebView;
+
+  onNavigationStateChange = ({url}: {url: string}) => {
+    // iOS navigates to about:blank when you provide raw HTML to a webview.
+    // Android navigates to data:text/html;$stuff (that is, the document you passed) instead.
+    if (!canOpenUrl(url)) {
+      return
+    }
+
+    this._webview.stopLoading()
+    this._webview.goBack()
+
+    return openUrl(url)
+  }
+
+  render() {
+    return (
+      <WebView
+        ref={ref => this._webview = ref}
+        source={{html: this.props.html}}
+        onNavigationStateChange={this.onNavigationStateChange}
+      />
+    )
+  }
+}
