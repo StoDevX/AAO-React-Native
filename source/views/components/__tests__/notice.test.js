ab1d7d80196b2b4f660ea97ef72a00a7032a33aa
Hawken Rives <hawkrives@gmail.com>
2017-02-25 11:41:12 -0600

    add test for NoticeView

---
+++ source/views/components/__tests__/notice.test.js
@@ -0,0 +1,55 @@
+// @flow
+
+import React from 'react'
+import {Text} from 'react-native'
+import {shallow} from 'enzyme'
+
+import {NoticeView} from '../notice'
+import noop from 'lodash/noop'
+
+test('renders', () => {
+  const tree = shallow(
+    <NoticeView
+      text='A Message'
+    />
+  )
+
+  expect(tree).toMatchSnapshot()
+})
+
+test('renders the given text', () => {
+  const label = 'A Label I Am'
+
+  const tree = shallow(
+    <NoticeView
+      text={label}
+    />
+  )
+
+  expect(tree.find('Text').prop('children')).toBe(label)
+})
+
+test('renders the given text', () => {
+  const button = 'Button'
+
+  const tree = shallow(
+    <NoticeView
+      text='Label'
+      buttonText={button}
+    />
+  )
+
+  expect(tree.find('Button').length).toBe(1)
+  expect(tree.find('Button').prop('title')).toBe(button)
+})
+
+test('shows an ActivityIndicator if given [spinner]', () => {
+  const tree = shallow(
+    <NoticeView
+      text='Label'
+      spinner={true}
+    />
+  )
+
+  expect(tree.find('ActivityIndicator').length).toBe(1)
+})
