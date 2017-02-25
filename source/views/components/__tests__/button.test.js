e09921d5fc91e54a793405d073553099a5d0eb10
Hawken Rives <hawkrives@gmail.com>
2017-02-25 11:41:00 -0600

    add test for Button

---
+++ source/views/components/__tests__/button.test.js
@@ -0,0 +1,28 @@
+// @flow
+
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import {Button} from '../button'
+
+test('renders', () => {
+  const tree = shallow(<Button />)
+  expect(tree).toMatchSnapshot()
+})
+
+test('can change the title', () => {
+  const tree = shallow(<Button title='Title' />)
+  expect(tree).toMatchSnapshot()
+})
+
+test('calls the callback', () => {
+  const cb = jest.fn()
+
+  const tree = shallow(<Button onPress={cb} />)
+  expect(tree).toMatchSnapshot()
+
+  tree.simulate('press')
+
+  expect(cb).toHaveBeenCalled()
+  expect(tree).toMatchSnapshot()
+})
