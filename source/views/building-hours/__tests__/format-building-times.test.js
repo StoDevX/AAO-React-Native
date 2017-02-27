3c120d3628be1fa09169810a434e9cc5a22711bf
Hawken Rives <hawkrives@gmail.com>
2017-02-27 13:03:53 -0600

    Revert "add a failing test"
    2b8d8537ceaae325e8b95badec5532ae8f09a670

--- source/views/building-hours/__tests__/format-building-times.test.js
+++ source/views/building-hours/__tests__/format-building-times.test.js
@@ -13,5 +13,5 @@ it('changes 12:00am into "Midnight"', () => {
   let m = dayMoment('Fri 3:00pm')
   let schedule = {days: ['Fr'], from: '10:30am', to: '12:00am'}
 
-  expect(formatBuildingTimes(schedule, m)).toBe('10:30 — Midnight')
+  expect(formatBuildingTimes(schedule, m)).toBe('10:30am — Midnight')
 })
