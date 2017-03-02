9df0dec599b3bc766709d592c7ef274c0d016577
Hawken Rives <hawkrives@gmail.com>
2017-03-02 15:47:50 -0600

    move platform-generic lanes into platforms/generic

--- fastlane/lanes/register.rb
+++
@@ -1,7 +0,0 @@
-desc "Adds any unregistered devices to the provisioning profile"
-lane :register do
-  id = CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)
-  new_devices = get_unprovisioned_devices_from_hockey(app_bundle_id: id)
-  register_devices(devices: new_devices)
-  match(force: true)
-end
--- fastlane/platforms/generic.rb
+++ fastlane/platforms/generic.rb
@@ -15,3 +15,22 @@ lane :bump do |options|
   # update package.json version
   package_set_data(data: {:version => new_version})
 end
+
+desc "Adds any unregistered devices to the provisioning profile"
+lane :register do
+  id = CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)
+  new_devices = get_unprovisioned_devices_from_hockey(app_bundle_id: id)
+  register_devices(devices: new_devices)
+  match(force: true)
+end
+
+desc "Build the release notes: branch, commit hash, changelog"
+lane :release_notes do |options|
+  <<~END
+  branch: #{git_branch}
+  git commit: #{last_git_commit[:commit_hash]}
+
+  ## Changelog
+  #{changelog}
+  END
+end
