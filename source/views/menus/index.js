79875c5c6b2e794d55c79e53195a543f3ae540cc
Hawken Rives <hawkrives@gmail.com>
2017-03-06 09:46:19 -0600

    run prettier on menus/

--- source/views/menus/index.js
+++ source/views/menus/index.js
@@ -13,7 +13,14 @@ import TabbedView from '../components/tabbed-view'
 import tabs from './tabs'
 
 export function MenusView({navigator, route}: TopLevelViewPropsType) {
-  return <TabbedView style={styles.container} tabs={tabs} navigator={navigator} route={route} />
+  return (
+    <TabbedView
+      style={styles.container}
+      tabs={tabs}
+      navigator={navigator}
+      route={route}
+    />
+  )
 }
 MenusView.propTypes = TopLevelViewPropTypes
 
