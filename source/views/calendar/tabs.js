eb3101b5b4bdf77717feb5b4718c494187f7b1e7
Hawken Rives <hawkrives@gmail.com>
2017-03-06 09:39:54 -0600

    run prettier on calendar

--- source/views/calendar/tabs.js
+++ source/views/calendar/tabs.js
@@ -8,21 +8,27 @@ export default [
     title: 'St. Olaf',
     rnVectorIcon: {iconName: 'school'},
     component: GoogleCalendarView,
-    props: {calendarId: 'le6tdd9i38vgb7fcmha0hu66u9gjus2e%40import.calendar.google.com'},
+    props: {
+      calendarId: 'le6tdd9i38vgb7fcmha0hu66u9gjus2e%40import.calendar.google.com',
+    },
   },
   {
     id: 'OlevilleCalendarView',
     title: 'Oleville',
     rnVectorIcon: {iconName: 'happy'},
     component: GoogleCalendarView,
-    props: {calendarId: 'stolaf.edu_fvulqo4larnslel75740vglvko@group.calendar.google.com'},
+    props: {
+      calendarId: 'stolaf.edu_fvulqo4larnslel75740vglvko@group.calendar.google.com',
+    },
   },
   {
     id: 'PauseCalendarView',
     title: 'The Pause',
     rnVectorIcon: {iconName: 'paw'},
     component: GoogleCalendarView,
-    props: {calendarId: 'stolaf.edu_qkrej5rm8c8582dlnc28nreboc@group.calendar.google.com'},
+    props: {
+      calendarId: 'stolaf.edu_qkrej5rm8c8582dlnc28nreboc@group.calendar.google.com',
+    },
   },
   {
     id: 'StudentOrgsCalendarView',
