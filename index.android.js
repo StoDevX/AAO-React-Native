801909e6cb623c48e2fb16fd58988aa8deaf83c7
Hawken Rives <hawkrives@gmail.com>
2017-03-09 14:48:22 -0600

    deduplicate index entry points (#837)

--- index.android.js
+++ index.android.js
@@ -1,12 +1,1 @@
-import {AppRegistry} from 'react-native'
-import App from './source/app'
-AppRegistry.registerComponent('AllAboutOlaf', () => App)
-import codePush from 'react-native-code-push'
-
-const codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  installMode: codePush.InstallMode.ON_NEXT_RESUME,
-}
-
-AppRegistry.registerComponent('AllAboutOlaf', () =>
-  codePush(codePushOptions)(App))
+import './source/root.js'
