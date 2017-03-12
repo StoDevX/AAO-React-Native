4d5969ce8e9ee3fe398ad8f8ee71d94a4cf213ac
Hawken Rives <hawkrives@gmail.com>
2017-03-12 08:27:02 -0500

    move Menus to <Tab/>

--- source/views/menus/index.js
+++ source/views/menus/index.js
@@ -8,21 +8,68 @@ import React from 'react'
 import {StyleSheet} from 'react-native'
 
 import type {TopLevelViewPropsType} from '../types'
-import {TopLevelViewPropTypes} from '../types'
-import TabbedView from '../components/tabbed-view'
-import tabs from './tabs'
+import {TabbedView, Tab} from '../components/tabbed-view'
+import {BonAppHostedMenu} from './menu-bonapp'
+import {GitHubHostedMenu} from './menu-github'
+import {CarletonMenuPicker} from './carleton-list'
+import {BonAppPickerView} from './dev-bonapp-picker'
 
 export function MenusView({navigator, route}: TopLevelViewPropsType) {
   return (
-    <TabbedView
-      style={styles.container}
-      tabs={tabs}
-      navigator={navigator}
-      route={route}
-    />
+    <TabbedView style={styles.container}>
+      <Tab id='StavHallMenuView' title='Stav Hall' icon='nutrition'>
+        {() =>
+          <BonAppHostedMenu
+            route={route}
+            navigator={navigator}
+            name='stav'
+            cafeId='261'
+            loadingMessage={[
+              'Hunting Ferndale Turkey…',
+              'Tracking wild vegan burgers…',
+              '"Cooking" some lutefisk…',
+            ]}
+          />}
+      </Tab>
+
+      <Tab id='TheCageMenuView' title='The Cage' icon='cafe'>
+        {() =>
+          <BonAppHostedMenu
+            route={route}
+            navigator={navigator}
+            name='cage'
+            cafeId='262'
+            ignoreProvidedMenus={true}
+            loadingMessage={[
+              'Checking for vegan cookies…',
+              'Serving up some shakes…',
+            ]}
+          />}
+      </Tab>
+
+      <Tab id='ThePauseMenuView' title='The Pause' icon='paw'>
+        {() =>
+          <GitHubHostedMenu
+            route={route}
+            navigator={navigator}
+            name='pause'
+            loadingMessage={[
+              'Mixing up a shake…',
+              'Spinning up pizzas…',
+            ]}
+          />}
+      </Tab>
+
+      <Tab id='CarletonMenuList' title='Carleton' icon='pin'>
+        {() => <CarletonMenuPicker route={route} navigator={navigator} />}
+      </Tab>
+
+      {__DEV__ ? <Tab id='BonAppMenuPicker' title='BonApp' icon='ionic'>
+        {() => <BonAppPickerView route={route} navigator={navigator} />}
+      </Tab> : null}
+    </TabbedView>
   )
 }
-MenusView.propTypes = TopLevelViewPropTypes
 
 let styles = StyleSheet.create({
   container: {
