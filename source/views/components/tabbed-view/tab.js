5214ef49ce8c6af88a1d344c302cdece280a8446
Hawken Rives <hawkrives@gmail.com>
2017-03-12 09:32:08 -0500

    move TabPropsType to tabbed-view/types.js

--- source/views/components/tabbed-view/tab.js
+++ source/views/components/tabbed-view/tab.js
@@ -1,11 +1,5 @@
 // @flow
-
-type TabPropsType = {
-  id: string,
-  icon: string,
-  title: string,
-  render: () => ReactElement<*>,
-};
+import type {TabPropsType} from './types'
 
 export const Tab = (props: TabPropsType) =>
   props.children ? props.children : null
--- source/views/components/tabbed-view/types.js
+++ source/views/components/tabbed-view/types.js
@@ -1,3 +1,12 @@
+// @flow
+
+export type TabPropsType = {
+  id: string,
+  icon: string,
+  title: string,
+  render: () => ReactElement<*>,
+};
+
 export type TabbedViewPropsType = {
   style?: Object | number | Array<Object | number>,
 };
