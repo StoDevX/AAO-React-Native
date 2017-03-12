5151f0cf94f6b170206af0aaadca2eda3d051517
Hawken Rives <hawkrives@gmail.com>
2017-03-12 08:27:58 -0500

    move News to <Tab/>

--- source/views/news/index.js
+++ source/views/news/index.js
@@ -8,21 +8,73 @@ import React from 'react'
 import {StyleSheet} from 'react-native'
 
 import type {TopLevelViewPropsType} from '../types'
-import {TopLevelViewPropTypes} from '../types'
-import TabbedView from '../components/tabbed-view'
-import tabs from './tabs'
+import {TabbedView, Tab} from '../components/tabbed-view'
+import NewsContainer from './news-container'
 
 export default function NewsPage({navigator, route}: TopLevelViewPropsType) {
   return (
-    <TabbedView
-      style={styles.container}
-      tabs={tabs}
-      navigator={navigator}
-      route={route}
-    />
+    <TabbedView style={styles.container}>
+      <Tab id='StOlafNewsView' title='St. Olaf' icon='school'>
+        {() =>
+          <NewsContainer
+            route={route}
+            navigator={navigator}
+            mode='wp-json'
+            url='https://wp.stolaf.edu/wp-json/wp/v2/posts'
+            query={{'per_page': 10, _embed: true}}
+            name='St. Olaf'
+          />}
+      </Tab>
+
+      <Tab id='OlevilleNewsView' title='Oleville' icon='happy'>
+        {() =>
+          <NewsContainer
+            route={route}
+            navigator={navigator}
+            mode='wp-json'
+            url='http://oleville.com/wp-json/wp/v2/posts/'
+            query={{per_page: 10, _embed: true}}
+            embedFeaturedImage={true}
+            name='Oleville'
+          />}
+      </Tab>
+
+      <Tab id='TheMessNewsView' title='The Mess' icon='paper'>
+        {() =>
+          <NewsContainer
+            route={route}
+            navigator={navigator}
+            mode='rss'
+            url='http://manitoumessenger.com/feed/'
+            name='The Mess'
+          />}
+      </Tab>
+
+      <Tab id='PoliticOleNewsView' title='PoliticOle' icon='megaphone'>
+        {() =>
+          <NewsContainer
+            route={route}
+            navigator={navigator}
+            mode='rss'
+            url='http://oleville.com/politicole/feed/'
+            name='PoliticOle'
+          />}
+      </Tab>
+
+      <Tab id='KstoNewsView' title='KSTO' icon='radio'>
+        {() =>
+          <NewsContainer
+            route={route}
+            navigator={navigator}
+            mode='wp-json'
+            url='https://pages.stolaf.edu/ksto/wp-json/wp/v2/posts/'
+            query={{per_page: 10, _embed: true}}
+            name='KSTO'
+          />}
+      </Tab>
+    </TabbedView>
   )
 }
-NewsPage.propTypes = TopLevelViewPropTypes
 
 let styles = StyleSheet.create({
   container: {
