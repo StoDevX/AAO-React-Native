38f7840fe5f030844c96034bfc27da3dfb876705
Hawken Rives <hawkrives@gmail.com>
2017-03-12 08:28:25 -0500

    port SIS to <Tab/>
    fixup sis

--- source/views/sis/index.js
+++ source/views/sis/index.js
@@ -7,25 +7,33 @@
 import React from 'react'
 import {StyleSheet} from 'react-native'
 
-import TabbedView from '../components/tabbed-view'
-import tabs from './tabs'
+import {TabbedView, Tab} from '../components/tabbed-view'
+import type {TopLevelViewPropsType} from '../types'
+
+import BalancesView from './balances'
+import CoursesView from './courses'
+// import SearchView from './search'
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 })
 
-export default function SISView(props: {navigator: any, route: any}) {
+export default function SISView({navigator, route}: TopLevelViewPropsType) {
   return (
-    <TabbedView
-      navigator={props.navigator}
-      route={props.route}
-      style={styles.container}
-      tabs={tabs}
-    />
+    <TabbedView style={styles.container}>
+      <Tab id='BalancesView' title='Balances' icon='card'>
+        {() => <BalancesView route={route} navigator={navigator} />}
+      </Tab>
+
+      <Tab id='CoursesView' title='Courses' icon='archive'>
+        {() => <CoursesView route={route} navigator={navigator} />}
+      </Tab>
+
+      {/*<Tab id='CourseSearchView' title='Search' icon='search'>
+        {() => <SearchView route={route} navigator={navigator} />}
+      </Tab>*/}
+    </TabbedView>
   )
-}
-SISView.propTypes = {
-  navigator: React.PropTypes.object.isRequired,
-  route: React.PropTypes.object.isRequired,
 }
