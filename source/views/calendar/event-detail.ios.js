a9abb015db1616e4f2b324575aaf5d9e5aa11d19
Hawken Rives <hawkrives@gmail.com>
2017-03-14 11:01:16 -0500

    use EventRow in EventDetail

--- source/views/calendar/event-detail.ios.js
+++ source/views/calendar/event-detail.ios.js
@@ -1,19 +1,20 @@
 // @flow
 import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import type {EventType} from './types'
 import EventRow from './event-row'
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginHorizontal: 10,
+    marginVertical: 10,
   },
 })
 
 export function EventDetail({event}: {event: EventType}) {
   return (
     <View style={styles.container}>
-      <Text>{event.summary}</Text>
+      <EventRow onPress={() => {}} event={event} />
     </View>
   )
 }
