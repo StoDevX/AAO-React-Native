fbd5784f180afc836e0d2db09d60ebee37af854c
Hawken Rives <hawkrives@gmail.com>
2017-04-18 21:56:39 -0500

    fix ENV references
    use strings not symbols for this

--- fastlane/lib/before_all.rb
+++ fastlane/lib/before_all.rb
@@ -17,12 +17,12 @@ before_all do
 
   lane_context[:GRADLE_FILE] = "#{ENV['FL_GRADLE_PROJECT_DIR']}/app/build.gradle"
 
-  UI.message "FL_HOCKEY_COMMIT_SHA is #{ENV[:FL_HOCKEY_COMMIT_SHA]}"
-  UI.message "GYM_PROJECT is #{ENV[:GYM_PROJECT]}"
-  UI.message "GYM_SCHEME is #{ENV[:GYM_SCHEME]}"
-  UI.message "GYM_OUTPUT_DIRECTORY is #{ENV[:GYM_OUTPUT_DIRECTORY]}"
-  UI.message "GYM_OUTPUT_NAME is #{ENV[:GYM_OUTPUT_NAME]}"
-  UI.message "FL_GRADLE_PROJECT_DIR is #{ENV[:FL_GRADLE_PROJECT_DIR]}"
+  UI.message "FL_HOCKEY_COMMIT_SHA is #{ENV['FL_HOCKEY_COMMIT_SHA']}"
+  UI.message "GYM_PROJECT is #{ENV['GYM_PROJECT']}"
+  UI.message "GYM_SCHEME is #{ENV['GYM_SCHEME']}"
+  UI.message "GYM_OUTPUT_DIRECTORY is #{ENV['GYM_OUTPUT_DIRECTORY']}"
+  UI.message "GYM_OUTPUT_NAME is #{ENV['GYM_OUTPUT_NAME']}"
+  UI.message "FL_GRADLE_PROJECT_DIR is #{ENV['FL_GRADLE_PROJECT_DIR']}"
   UI.message "PRETTY_APP_NAME is #{lane_context[:PRETTY_APP_NAME]}"
   UI.message "GRADLE_FILE is #{lane_context[:GRADLE_FILE]}"
   UI.message "PLATFORM_NAME is #{lane_context[:PLATFORM_NAME]}"
