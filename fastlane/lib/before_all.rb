3f0fca4ecf23ffabd1dff16a555ca0d88462944d
Hawken Rives <hawkrives@gmail.com>
2017-04-18 21:52:22 -0500

    fix hockey token name

--- fastlane/lib/before_all.rb
+++ fastlane/lib/before_all.rb
@@ -1,5 +1,5 @@
 before_all do
   # too lazy to change the name in travis, so we jut copy it here
-  ENV['FL_HOCKEY_API_TOKEN'] = ENV['HOCKEY_TOKEN']
+  ENV['FL_HOCKEY_API_TOKEN'] = ENV['HOCKEYAPP_TOKEN']
   ENV['FL_HOCKEY_COMMIT_SHA'] = ENV['TRAVIS_COMMIT']
 end
