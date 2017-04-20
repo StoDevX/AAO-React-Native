67bee6ee294c1136b0a2f0f240214ebf7e935133
Hawken Rives <hawkrives@gmail.com>
2017-04-19 22:29:01 -0500

    install the code-push cli

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -4,6 +4,11 @@ set -e -v -x
 # install packages
 npm install
 
+# install code-push
+if [[ $JS ]]; then
+  npm install -g code-push-cli@latest
+fi
+
 # install fastlane
 if [[ $ANDROID || $IOS ]]; then
   bundle install --deployment
