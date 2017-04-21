1f0c761f7baf6b705ff8db0e760f7cc377e62cf2
Hawken Rives <hawkrives@gmail.com>
2017-04-21 00:50:31 -0500

    add initial wifi reporting

---
+++ source/views/help/index.js
@@ -0,0 +1,18 @@
+// @flow
+
+import React from 'react'
+import {View} from 'react-native'
+import {Card} from '../components/card'
+import {Button} from '../components/button'
+
+import {ReportWifiProblemView} from './wifi'
+
+export default class HelpView extends React.Component {
+    render() {
+        return (
+            <View>
+                <ReportWifiProblemView />
+            </View>
+        )
+    }
+}
