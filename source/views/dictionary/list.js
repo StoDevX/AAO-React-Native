7966d9beed8e3a29bb65cb1aea29e190ea702dd8
Drew Volz <volz.drew@gmail.com>
2017-04-23 23:13:00 -0400

    Searchable dictionary for iOS (#959)
    * integrate search into dictionary
    * adds searchbar wrapper component

--- source/views/dictionary/list.js
+++ source/views/dictionary/list.js
@@ -4,7 +4,7 @@
  * Dictionary page
  */
 import React from 'react'
-import {StyleSheet, Platform} from 'react-native'
+import {StyleSheet, View, Platform} from 'react-native'
 import {StyledAlphabetListView} from '../components/alphabet-listview'
 import {Column} from '../components/layout'
 import {
@@ -14,16 +14,25 @@ import {
   ListSectionHeader,
   ListSeparator,
 } from '../components/list'
+import LoadingView from '../components/loading'
 import type {WordType} from './types'
 import {tracker} from '../../analytics'
 import groupBy from 'lodash/groupBy'
 import head from 'lodash/head'
+import uniq from 'lodash/uniq'
+import words from 'lodash/words'
+import deburr from 'lodash/deburr'
+import isString from 'lodash/isString'
 import {data as terms} from '../../../docs/dictionary.json'
+import {SearchBar} from '../components/searchbar'
 
 const rowHeight = Platform.OS === 'ios' ? 76 : 89
 const headerHeight = Platform.OS === 'ios' ? 33 : 41
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
   row: {
     height: rowHeight,
   },
@@ -39,6 +48,12 @@ export class DictionaryView extends React.Component {
   static propTypes = {
     navigator: React.PropTypes.object.isRequired,
     route: React.PropTypes.object.isRequired,
+  }
+
+  state: {
+    results: {[key: string]: Array<WordType>},
+  } = {
+    results: terms,
   }
 
   onPressRow = (data: WordType) => {
@@ -77,21 +92,59 @@ export class DictionaryView extends React.Component {
     return <ListSeparator key={`${sectionId}-${rowId}`} />
   }
 
+  splitToArray = (str: string) => {
+    return words(deburr(str.toLowerCase()))
+  }
+
+  termToArray = (term: WordType) => {
+    return uniq([
+      ...this.splitToArray(term.word),
+      ...this.splitToArray(term.definition),
+    ])
+  }
+
+  performSearch = (text: string) => {
+    // Android clear button returns an object
+    if (!isString(text)) {
+      this.setState({results: terms})
+      return
+    }
+
+    const query = text.toLowerCase()
+    this.setState(() => ({
+      results: terms.filter(term =>
+        this.termToArray(term).some(word => word.startsWith(query)),
+      ),
+    }))
+  }
+
+  searchBar: any
+
   render() {
+    if (!terms) {
+      return <LoadingView />
+    }
+
     return (
-      <StyledAlphabetListView
-        data={groupBy(terms, item => head(item.word))}
-        cell={this.renderRow}
-        // just setting cellHeight sends the wrong values on iOS.
-        cellHeight={
-          rowHeight +
-            (Platform.OS === 'ios' ? 11 / 12 * StyleSheet.hairlineWidth : 0)
-        }
-        sectionHeader={this.renderHeader}
-        sectionHeaderHeight={headerHeight}
-        showsVerticalScrollIndicator={false}
-        renderSeparator={this.renderSeparator}
-      />
+      <View style={styles.wrapper}>
+        <SearchBar
+          getRef={ref => (this.searchBar = ref)}
+          onChangeText={this.performSearch}
+        />
+        <StyledAlphabetListView
+          data={groupBy(this.state.results, item => head(item.word))}
+          cell={this.renderRow}
+          // just setting cellHeight sends the wrong values on iOS.
+          cellHeight={
+            rowHeight +
+              (Platform.OS === 'ios' ? 11 / 12 * StyleSheet.hairlineWidth : 0)
+          }
+          sectionHeader={this.renderHeader}
+          sectionHeaderHeight={headerHeight}
+          showsVerticalScrollIndicator={false}
+          renderSeparator={this.renderSeparator}
+        />
+      </View>
     )
   }
 }
