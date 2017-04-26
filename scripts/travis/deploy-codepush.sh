61bf9d67ba0ecc9052770c7f5e7c196f2e36db72
Hawken Rives <hawkrives@gmail.com>
2017-04-25 21:26:29 -0500

    change codepush target

--- scripts/travis/deploy-codepush.sh
+++ scripts/travis/deploy-codepush.sh
@@ -2,7 +2,10 @@
 set -ev
 set -o pipefail
 
-echo "releasing codepush for ios";
-code-push release-react AllAboutOlaf-iOS ios -d release --targetBinaryVersion '2.*';
-echo "releasing codepush for android";
-code-push release-react AllAboutOlaf-Android android -d release --targetBinaryVersion '2.*';
+target_version="~2.1"
+
+echo "releasing codepush for ios"
+code-push release-react AllAboutOlaf-iOS ios -d release --targetBinaryVersion "$target_version"
+
+echo "releasing codepush for android"
+code-push release-react AllAboutOlaf-Android android -d release --targetBinaryVersion "$target_version"
