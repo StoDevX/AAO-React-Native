1698691c530f74efdeeedb1f1b7548fd04294a7f
Hawken Rives <rives@stolaf.edu>
2017-05-03 18:16:49 -0500

    change things

--- source/views/help/wifi.js
+++ source/views/help/wifi.js
@@ -1,11 +1,12 @@
 // @flow
 
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, ScrollView, Text} from 'react-native'
 import {Card} from '../components/card'
 import {Button} from '../components/button'
 import deviceInfo from 'react-native-device-info'
 import networkInfo from 'react-native-network-info'
+import pkg from '../../../package.json'
 
 const getIpAddress = () =>
   new Promise(resolve => {
@@ -34,15 +35,23 @@ const collectData = async () => ({
   os: deviceInfo.getSystemName(),
   osVersion: deviceInfo.getSystemVersion(),
   appVersion: deviceInfo.getReadableVersion(),
+  jsVersion: pkg.version,
   ua: deviceInfo.getUserAgent(),
   ip: await getIpAddress(),
 })
 
 function reportToServer(data) {
-  // return fetch('https://www.stolaf.edu/apps/aao/wifi.cfm?fuseaction=Submit', {
-  //   method: 'POST',
-  //   body: JSON.stringify(data),
-  // })
+  return fetch('https://www.stolaf.edu/apps/all-about-olaf/index.cfm?fuseaction=Submit', {
+    method: 'POST',
+    body: JSON.stringify(data),
+  }).then(async r => {
+      let text = await r.text()
+      try {
+          return JSON.parse(text)
+      } catch (err) {
+          return text
+      }
+  })
 }
 
 export class ReportWifiProblemView extends React.Component {
@@ -53,16 +62,27 @@ export class ReportWifiProblemView extends React.Component {
 
   start = async () => {
     this.setState(() => ({status: 'Collecting data…'}))
-    const [pos, dev] = await Promise.all([getPosition(), collectData()])
+    const [position, device] = await Promise.all([getPosition(), collectData()])
     this.setState(() => ({status: 'Reporting data…'}))
-    await reportToServer({pos, dev})
-    this.setState(() => ({data: {...pos, ...dev}}))
-    this.setState(() => ({status: 'Thanks!'}))
+    try {
+        let data = {position, device, version: 1};
+        let resp = await reportToServer(data)
+        if (resp.status === 'success') {
+            this.setState(() => ({data}))
+            this.setState(() => ({status: 'Thanks!'}))
+        } else {
+            console.error(resp)
+            this.setState(() => ({status: 'Server error'}))
+        }
+    } catch (err) {
+        console.warn(err)
+        this.setState(() => ({status: err.message}))
+    }
   }
 
   render() {
     return (
-      <View>
+      <ScrollView>
         <Card header="Report a WiFi Problem!" footer={this.state.status}>
           <Button
             disabled={this.state.status !== ''}
@@ -73,7 +93,7 @@ export class ReportWifiProblemView extends React.Component {
             {this.state.data && JSON.stringify(this.state.data, null, 2)}
           </Text>
         </Card>
-      </View>
+      </ScrollView>
     )
   }
 }
