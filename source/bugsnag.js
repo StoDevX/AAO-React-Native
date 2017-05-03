8f09e2945d405a92063112c06c345e710dd9d240
Hawken Rives <hawkrives@gmail.com>
2017-05-02 22:56:21 -0500

    only enable bugsnag in dev mode

--- source/bugsnag.js
+++ source/bugsnag.js
@@ -1,6 +1,12 @@
 // @flow
 import {Client} from 'bugsnag-react-native'
+import noop from 'lodash/noop'
 
-const client = new Client()
+// We will need to add any other methods we eventually use in Bugsnag to this
+// faux-bugsnag object, I believe.
+let client = {notify: noop}
+if (process.env.NODE_ENV === 'production') {
+  client = new Client()
+}
 
 export default client
