80cda21bbfe85c46bb5d7af25c2a72f48eb2b396
Hawken Rives <rives@stolaf.edu>
2017-05-06 14:27:54 -0500

    change codepush to install on restart, not resume

--- source/root.js
+++ source/root.js
@@ -10,14 +10,14 @@ import './bugsnag'
 
 let codePushOptions = {
   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  installMode: codePush.InstallMode.ON_NEXT_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_RESTART,
 }
 
 if (process.env.NODE_ENV !== 'production') {
   // essentially disable codepush on dev mode
   codePushOptions = {
     checkFrequency: codePush.CheckFrequency.MANUAL,
-    installMode: codePush.InstallMode.ON_NEXT_RESUME,
+    installMode: codePush.InstallMode.ON_NEXT_RESTART,
   }
 }
 
