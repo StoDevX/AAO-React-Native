13545c5d88cdf06c7fb60794380f5270a7b52f6b
Hawken Rives <hawkrives@gmail.com>
2017-05-08 22:25:23 -0500

    switch prerelease specifier from -rc to -beta

--- fastlane/lib/util.rb
+++ fastlane/lib/util.rb
@@ -65,7 +65,7 @@ def auto_beta
   beta if last_commit != current_commit
 end
 
-def codepush_cli(app:, channel: 'nightly', install_target: '~2.2 || ~2.2.0-rc')
+def codepush_cli(app:, channel: 'nightly', install_target: '~2.2 || ~2.2.0-beta')
   target = "--targetBinaryVersion '#{install_target}'"
   # `fastlane x` runs in the ./fastlane folder, so we have to go up a level
   Dir.chdir("..") do
