f03b0ca9c7aede81ed0d041669132d2689619b5b
Hawken Rives <hawkrives@gmail.com>
2017-05-19 21:13:35 -0500

    add some internal fastlane functions for working with match repos

--- fastlane/platforms/agnostic.rb
+++ fastlane/platforms/agnostic.rb
@@ -47,3 +47,32 @@ desc 'run `npm run bundle-data`'
 lane :bundle_data do
   sh('npm run bundle-data')
 end
+
+desc 'clone the match repo'
+private_lane :clone_match do
+  git_url = 'https://github.com/hawkrives/aao-keys'
+  dir = Dir.mktmpdir
+  command = "git clone --depth 1 '#{git_url}' '#{dir}'"
+
+  # this block pulled from https://github.com/fastlane/fastlane/blob/master/match/lib/match/git_helper.rb
+  UI.message 'Cloning remote git repo...'
+  begin
+    # GIT_TERMINAL_PROMPT will fail the `git clone` command if user credentials are missing
+    FastlaneCore::CommandExecutor.execute(command: "GIT_TERMINAL_PROMPT=0 #{command}",
+                                          print_all: true,
+                                          print_command: true)
+  rescue
+    UI.error 'Error cloning the certificates repo. Please make sure you have read access to the repository you want to use'
+    UI.error "Run the following command manually to make sure you're properly authenticated:"
+    UI.command command
+    UI.user_error! 'Error cloning the certificates git repo. Please make sure you have access to the repository - see instructions above'
+  end
+
+  dir
+end
+
+desc 'remove the match cloned folder'
+private_lane :remove_match_clone do |options|
+  UI.command "removing #{options[:dir]}"
+  FileUtils.remove_entry_secure options[:dir]
+end
