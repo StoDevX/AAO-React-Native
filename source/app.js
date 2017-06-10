794e137adea6133f5e86bf278cbbac13ae3d8262
Hawken Rives <hawkrives@gmail.com>
2017-06-10 17:28:30 -0500

    convert all root route defs to react-nav

--- source/app.js
+++ source/app.js
@@ -6,54 +6,29 @@
 
 import './globalize-fetch'
 import './setup-moment'
-import {tracker} from './analytics'
 import OneSignal from 'react-native-onesignal'
 
 import React from 'react'
-import {Navigator, BackAndroid, StyleSheet, Platform} from 'react-native'
 import {Provider} from 'react-redux'
 import {store} from './flux'
-import * as c from './views/components/colors'
-import {
-  Title,
-  LeftButton,
-  RightButton,
-  configureScene,
-} from './views/components/navigation'
-import {renderScene} from './navigation'
+import {tracker} from './analytics'
+import {AppNavigator} from './navigation'
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: Platform.OS === 'ios' ? 64 : 56,
-    flex: 1,
-    backgroundColor: Platform.OS === 'ios'
-      ? c.iosLightBackground
-      : c.androidLightBackground,
-  },
-  navigationBar: {
-    backgroundColor: c.olevilleGold,
-    ...Platform.select({
-      ios: {
-        shadowOffset: {width: 0, height: StyleSheet.hairlineWidth},
-        shadowColor: 'rgb(100, 100, 100)',
-        shadowOpacity: 0.5,
-        shadowRadius: StyleSheet.hairlineWidth,
-      },
-      android: {
-        elevation: 4,
-      },
-    }),
-  },
-})
+// gets the current screen from navigation state
+function getCurrentRouteName(navigationState): ?string {
+  if (!navigationState) {
+    return null;
+  }
+  const route = navigationState.routes[navigationState.index];
+  // dive into nested navigators
+  if (route.routes) {
+    return getCurrentRouteName(route);
+  }
+  return route.routeName;
+}
 
 export default class App extends React.Component {
   componentDidMount() {
-    tracker.trackEvent('app', 'launch')
-    BackAndroid.addEventListener(
-      'hardwareBackPress',
-      this.registerAndroidBackButton,
-    )
-
     OneSignal.addEventListener('received', this.onReceived)
     OneSignal.addEventListener('opened', this.onOpened)
     OneSignal.addEventListener('registered', this.onRegistered)
@@ -61,11 +36,6 @@ export default class App extends React.Component {
   }
 
   componentWillUnmount() {
-    BackAndroid.removeEventListener(
-      'hardwareBackPress',
-      this.registerAndroidBackButton,
-    )
-
     OneSignal.removeEventListener('received', this.onReceived)
     OneSignal.removeEventListener('opened', this.onOpened)
     OneSignal.removeEventListener('registered', this.onRegistered)
@@ -91,40 +61,19 @@ export default class App extends React.Component {
     console.log('Device info:', device)
   }
 
-  _navigator: Navigator
+  trackScreenChanges(prevState: any, currentState: any) {
+    const currentScreen = getCurrentRouteName(currentState)
+    const prevScreen = getCurrentRouteName(prevState)
 
-  registerAndroidBackButton = () => {
-    if (this._navigator && this._navigator.getCurrentRoutes().length > 1) {
-      this._navigator.pop()
-      return true
+    if (currentScreen !== prevScreen) {
+      tracker.trackScreenView(currentScreen)
     }
-    return false
   }
 
   render() {
     return (
       <Provider store={store}>
-        <Navigator
-          ref={nav => (this._navigator = nav)}
-          navigationBar={
-            <Navigator.NavigationBar
-              style={styles.navigationBar}
-              routeMapper={{
-                LeftButton,
-                RightButton,
-                Title,
-              }}
-            />
-          }
-          initialRoute={{
-            id: 'HomeView',
-            title: 'All About Olaf',
-            index: 0,
-          }}
-          renderScene={renderScene}
-          sceneStyle={styles.container}
-          configureScene={configureScene}
-        />
+        <AppNavigator onNavigationStateChange={this.trackScreenChanges} />
       </Provider>
     )
   }
