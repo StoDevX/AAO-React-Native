4dd2176e331a047c1f43791fbeeb1a136eee2aab
Hawken Rives <hawkrives@gmail.com>
2017-06-10 18:46:29 -0500

    update streaming media for react-navigation

--- source/views/streaming/index.js
+++ source/views/streaming/index.js
@@ -6,35 +6,41 @@
 
 import React from 'react'
 
-import TabbedView from '../components/tabbed-view'
+import {TabNavigator} from 'react-navigation'
+import {TabBarIcon} from '../components/tabbar-icon'
+import * as c from '../components/colors'
 
 import KSTOView from './radio'
 // import WeeklyMovieView from './movie'
 import WebcamsView from './webcams'
 
-export default function MediaPage() {
-  return (
-    <TabbedView
-      tabs={[
-        {
-          id: 'KSTORadioView',
-          title: 'KSTO',
-          icon: 'radio',
-          component: () => <KSTOView />,
-        },
-        // {
-        //   id: 'WeeklyMovieView',
-        //   title: 'Weekly Movie',
-        //   icon: 'film',
-        //   component: () => <WeeklyMovieView />,
-        // },
-        {
-          id: 'LiveWebcamsView',
-          title: 'Webcams',
-          icon: 'videocam',
-          component: () => <WebcamsView />,
-        },
-      ]}
-    />
-  )
+const KstoTab = () => <KSTOView />
+KstoTab.navigationOptions = {
+  tabBarLabel: 'KSTO',
+  tabBarIcon: TabBarIcon('radio'),
 }
+
+// const WeeklyMovieTab = () => <WeeklyMovieView />
+// WeeklyMovieTab.navigationOptions = {
+//   tabBarLabel: 'film',
+//   tabBarIcon: TabBarIcon('bus'),
+// }
+
+const WebcamsTab = () => <WebcamsView />
+WebcamsTab.navigationOptions = {
+  tabBarLabel: 'Webcams',
+  tabBarIcon: TabBarIcon('videocam'),
+}
+
+export default TabNavigator({
+  KSTORadioView: {screen: KstoTab},
+  // WeeklyMovieView: {screen: WeeklyMovieTab},
+  LiveWebcamsView: {screen: WebcamsTab},
+}, {
+  navigationOptions: {
+    title: 'Streaming Media',
+  },
+  tabBarOptions: {
+    activeTintColor: c.mandarin,
+  }
+})
