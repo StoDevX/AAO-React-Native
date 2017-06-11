5a5b21204466dd3775f34857fe0bbe636ff7421a
Hawken Rives <hawkrives@gmail.com>
2017-06-10 21:46:33 -0500

    implement a custom TabNavigator wrapper

---
+++ source/views/components/tabbed-view/index.js
@@ -0,0 +1,27 @@
+// @flow
+
+import {
+  TabNavigator as TabNav,
+  NavigationScreenRouteConfig,
+} from 'react-navigation'
+import * as c from '../colors'
+
+type ComponentType = (
+  screens: {[key: string]: NavigationScreenRouteConfig},
+  options: any, // I don't know how to type this better…
+  // the package provides a bunch of types… but it doesn't even use some
+  // of them??? and none seem to be the combination of args to the second
+  // arg of TabNavigator.
+) => TabNav
+
+export const TabNavigator: ComponentType = (screens, options) => TabNav(
+  screens,
+  {
+    tabBarOptions: {
+      activeTintColor: c.mandarin,
+      backBehavior: 'none',
+      ...(options.tabBarOptions || {}),
+    },
+    ...options,
+  },
+)
