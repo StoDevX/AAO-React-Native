2b927ff6bff180b22d3946c13ae25fcd39d354ac
Hawken Rives <hawkrives@gmail.com>
2017-06-12 07:51:54 -0500

    add initial fully-featured building editor

--- source/views/building-hours/index.js
+++ source/views/building-hours/index.js
@@ -2,3 +2,7 @@
 
 export {BuildingHoursView} from './stateful-list'
 export {BuildingHoursDetailView} from './detail'
+export {
+  BuildingHoursProblemReportView,
+  BuildingHoursScheduleEditorView,
+} from './report'
