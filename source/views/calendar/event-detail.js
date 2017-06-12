00104e96e9d77b5a4222c53be77838d2df7c8bc1
Hawken Rives <hawkrives@gmail.com>
2017-06-12 07:22:09 -0500

    deduplicate the time gathering code in Calendar

--- source/views/calendar/event-detail.js
+++ source/views/calendar/event-detail.js
@@ -6,6 +6,7 @@ import {fastGetTrimmedText} from '../../lib/html'
 import {Cell, Section, TableView} from 'react-native-tableview-simple'
 import type {EventType} from './types'
 import {ShareButton} from '../components/nav-buttons'
+import {times} from './times'
 
 const styles = StyleSheet.create({
   chunk: {
@@ -36,31 +37,10 @@ function display(title: string, data: string) {
 }
 
 function displayTimes(title: string, event: EventType) {
-  const eventLength = moment
-    .duration(event.endTime.diff(event.startTime))
-    .asHours()
-
-  const allDay = eventLength === 24
-  const multiDay = event.startTime.dayOfYear() !== event.endTime.dayOfYear()
-  const sillyZeroLength = event.startTime.isSame(event.endTime, 'minute')
+  const {allDay, start, end} = times(event)
 
   if (allDay) {
     return display(title, 'All-Day')
-  }
-
-  let start, end
-  if (event.isOngoing) {
-    start = event.startTime.format('MMM. D')
-    end = event.endTime.format('MMM. D')
-  } else if (multiDay) {
-    start = event.startTime.format('h:mm A')
-    end = `${event.endTime.format('MMM. D h:mm A')}`
-  } else if (sillyZeroLength) {
-    start = event.startTime.format('h:mm A')
-    end = '???'
-  } else {
-    start = event.startTime.format('h:mm A')
-    end = event.endTime.format('h:mm A')
   }
 
   return display(title, start + ' — ' + end)
---
+++ source/views/calendar/times.js
@@ -0,0 +1,42 @@
+// @flow
+
+import moment from 'moment-timezone'
+
+export function times(event: EventType) {
+  const eventLength = moment
+    .duration(event.endTime.diff(event.startTime))
+    .asHours()
+
+  const allDay = eventLength === 24
+  const multiDay = event.startTime.dayOfYear() !== event.endTime.dayOfYear()
+  const sillyZeroLength = event.startTime.isSame(event.endTime, 'minute')
+
+  let startTimeFormatted = event.startTime.format('h:mm A')
+  let endTimeFormatted = event.endTime.format('h:mm A')
+  let midnightTime = '12:00 AM'
+
+  let start, end
+  if (event.isOngoing) {
+    start = event.startTime.format('MMM. D')
+    end = event.endTime.format('MMM. D')
+  } else if (multiDay) {
+    // 12:00 PM to Jun. 25 3:00pm
+    // Midnight to Jun. 25 <-- assuming the end time is also midnight
+    start = startTimeFormatted
+    const endFormat = endTimeFormatted === midnightTime
+      ? 'MMM. D'
+      : 'MMM. D h:mm A'
+    end = `to ${event.endTime.format(endFormat)}`
+  } else if (sillyZeroLength) {
+    start = startTimeFormatted
+    end = 'until ???'
+  } else {
+    start = startTimeFormatted
+    end = endTimeFormatted
+  }
+
+  start = start === midnightTime ? 'Midnight' : start
+  end = end === midnightTime ? 'Midnight' : end
+
+  return {start, end, allDay}
+}
