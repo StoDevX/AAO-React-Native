8a7f13fc8ea83f0284a3ae9c82be2336026ce6f5
Hawken Rives <hawkrives@gmail.com>
2017-06-21 21:50:54 -0500

    switch variables in parse-hours.test to const

--- source/views/building-hours/lib/__tests__/parse-hours.test.js
+++ source/views/building-hours/lib/__tests__/parse-hours.test.js
@@ -3,9 +3,9 @@ import {parseHours} from '../parse-hours'
 import {dayMoment, hourMoment, moment} from './moment.helper'
 
 it('returns an {open, close} tuple', () => {
-  let now = hourMoment('10:01am')
-  let input = {days: [], from: '10:00am', to: '4:00pm'}
-  let actual = parseHours(input, now)
+  const now = hourMoment('10:01am')
+  const input = {days: [], from: '10:00am', to: '4:00pm'}
+  const actual = parseHours(input, now)
 
   expect(actual).toBeDefined()
   expect(actual.open).toBeDefined()
@@ -13,23 +13,23 @@ it('returns an {open, close} tuple', () => {
 })
 
 it('returns a Moment for .open', () => {
-  let now = hourMoment('10:01am')
-  let input = {days: [], from: '10:00am', to: '4:00pm'}
-  let {open} = parseHours(input, now)
+  const now = hourMoment('10:01am')
+  const input = {days: [], from: '10:00am', to: '4:00pm'}
+  const {open} = parseHours(input, now)
   expect(moment.isMoment(open)).toBe(true)
 })
 
 it('returns a Moment for .close', () => {
-  let now = hourMoment('10:01am')
-  let input = {days: [], from: '10:00am', to: '4:00pm'}
-  let {close} = parseHours(input, now)
+  const now = hourMoment('10:01am')
+  const input = {days: [], from: '10:00am', to: '4:00pm'}
+  const {close} = parseHours(input, now)
   expect(moment.isMoment(close)).toBe(true)
 })
 
 it('will add a day to the close time with nextDay:true', () => {
-  let now = hourMoment('10:01am')
-  let input = {days: [], from: '10:00am', to: '2:00am'}
-  let {open, close} = parseHours(input, now)
+  const now = hourMoment('10:01am')
+  const input = {days: [], from: '10:00am', to: '2:00am'}
+  const {open, close} = parseHours(input, now)
 
   expect(close.isAfter(open)).toBe(true)
   expect(close.isAfter(now)).toBe(true)
@@ -37,17 +37,17 @@ it('will add a day to the close time with nextDay:true', () => {
 
 describe('handles wierd times', () => {
   it('handles Friday at 4:30pm', () => {
-    let now = dayMoment('Fri 4:30pm')
-    let input = {days: [], from: '10:00am', to: '2:00am'}
-    let {open, close} = parseHours(input, now)
+    const now = dayMoment('Fri 4:30pm')
+    const input = {days: [], from: '10:00am', to: '2:00am'}
+    const {open, close} = parseHours(input, now)
 
     expect(now.isBetween(open, close)).toBe(true)
   })
 
   it('handles Saturday at 1:30am', () => {
-    let now = dayMoment('Sat 1:30am')
-    let input = {days: [], from: '10:00am', to: '2:00am'}
-    let {open, close} = parseHours(input, now)
+    const now = dayMoment('Sat 1:30am')
+    const input = {days: [], from: '10:00am', to: '2:00am'}
+    const {open, close} = parseHours(input, now)
 
     expect(now.isBetween(open, close)).toBe(true)
   })
