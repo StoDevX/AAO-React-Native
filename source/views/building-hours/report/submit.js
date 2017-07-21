5a5f19c542d1f7a42b032518d37e2679196f2d42
Hawken Rives <hawkrives@gmail.com>
2017-07-21 14:06:25 -0500

    add a handy link to create an issue from a report email

--- source/views/building-hours/report/submit.js
+++ source/views/building-hours/report/submit.js
@@ -9,7 +9,7 @@ export function submitReport(current: BuildingType, suggestion: BuildingType) {
   const before = stringifyBuilding(current)
   const after = stringifyBuilding(suggestion)
 
-  const body = makeIssueBody(before, after)
+  const body = makeEmailBody(before, after, current.name)
 
   return email(
     ['allaboutolaf@stolaf.edu'],
@@ -20,20 +20,42 @@ export function submitReport(current: BuildingType, suggestion: BuildingType) {
   )
 }
 
-function makeIssueBody(before: string, after: string): string {
+
+function makeEmailBody(before: string, after: string, title: string): string {
   return dedent`
+    Hi! Thanks for letting us know about an hour change.
+
     Please do not change anything below this line.
 
     ------------
 
+    Project maintainers: ${makeIssueLink(before, after, title)}
+
+    ${makeBody(before, after)}
+  `
+}
+
+function makeBody(before, after) {
+  return dedent`
+    Before:
     \`\`\`yaml
     ${before}
     \`\`\`
 
+    After:
     \`\`\`yaml
     ${after}
     \`\`\`
   `
+}
+
+function makeIssueLink(before: string, after: string, title: string): string {
+  const q = querystring.stringify({
+    'labels[]': 'data/building',
+    title: `Building hours update for ${title}`,
+    body: makeBody(before, after),
+  })
+  return `https://github.com/StoDevX/AAO-React-Native/issues/new?${q}`
 }
 
 function stringifyBuilding(building: BuildingType): string {
