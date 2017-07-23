588ab4e3e73de5331005157a8ffa8d3c243aafed
Hawken Rives <hawkrives@gmail.com>
2017-07-23 13:40:00 -0500

    remove dedent from building-hours/submit

--- source/views/building-hours/report/submit.js
+++ source/views/building-hours/report/submit.js
@@ -3,7 +3,6 @@
 import jsYaml from 'js-yaml'
 import type {BuildingType} from '../types'
 import {email} from 'react-native-communications'
-import dedent from 'dedent'
 import querystring from 'querystring'
 
 export function submitReport(current: BuildingType, suggestion: BuildingType) {
@@ -23,47 +22,41 @@ export function submitReport(current: BuildingType, suggestion: BuildingType) {
 }
 
 function makeEmailBody(before: string, after: string, title: string): string {
-  return dedent`
-  Hi! Thanks for letting us know about a schedule change.
-
-  Please do not change anything below this line.
-
-  ------------
-
-  Project maintainers: ${makeIssueLink(before, after, title)}
-
-  ${makeHtmlBody(before, after)}
-  `
-}
-
-function makeMarkdownBody(before, after) {
-  // dedent works by removing all common whitespace at the beginning
-  // of lines, so the closing backtick has to be at the same level
-  // as the text.
-  return dedent`
-  ## Before:
-
-  \`\`\`yaml
-  ${before}
-  \`\`\`
-
-  ## After:
-
-  \`\`\`yaml
-  ${after}
-  \`\`\`
-  `
-}
-
-function makeHtmlBody(before, after) {
   return `
-  <p>Before:</p>
-  <pre><code>${before}</code></pre>
+Hi! Thanks for letting us know about a schedule change.
 
-  <p>After:</p>
-  <pre><code>${after}</code></pre>
-  `
+Please do not change anything below this line.
+
+------------
+
+Project maintainers: ${makeIssueLink(before, after, title)}
+
+${makeHtmlBody(before, after)}
+`
 }
+
+const makeMarkdownBody = (before, after) => `
+## Before:
+
+\`\`\`yaml
+${before}
+\`\`\`
+
+## After:
+
+\`\`\`yaml
+${after}
+\`\`\`
+
+`.trim()
+
+const makeHtmlBody = (before, after) => `
+<p>Before:</p>
+<pre><code>${before}</code></pre>
+
+<p>After:</p>
+<pre><code>${after}</code></pre>
+`
 
 function makeIssueLink(before: string, after: string, title: string): string {
   const q = querystring.stringify({
