3d6dcfee3bc7a3978d34a864cb2540e9a7a3a7c2
Drew Volz <volz.drew@gmail.com>
2017-07-30 19:55:50 -0400

    Add license to make ConstraintLayout happy

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -28,3 +28,10 @@ npm install -g npm@latest
 # chmod 600 "$DEPLOY_KEY"
 # eval "$(ssh-agent -s)"
 # ssh-add "$DEPLOY_KEY"
+
+# Adding this to accept ConstraintLayout's license... see https://git.io/v7lhM
+if [[ $ANDROID ]]; then
+  mkdir -p "$ANDROID_HOME/licenses"
+  echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
+  echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
+fi
