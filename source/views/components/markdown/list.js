fa21f3d3c9ce02c8772b7f766d2fb21c4954764c
Hawken Rives <hawkrives@gmail.com>
2017-08-06 20:36:35 -0500

    add bullet points back to the list items

--- source/views/components/markdown/list.js
+++ source/views/components/markdown/list.js
@@ -1,7 +1,28 @@
 // @flow
 
+import React from 'react'
+import {Text} from 'react-native'
 import glamorous from 'glamorous-native'
+import {Paragraph} from './formatting'
 
+// the list itself
 export const List = glamorous.view({})
 
-export const ListItem = glamorous.text({})
+// the list item's text
+export const ListText = glamorous(Paragraph)({})
+
+// the list item's container box thing
+export class ListItem extends React.PureComponent {
+  props: {
+    children?: React$Element<*>,
+  }
+
+  render() {
+    return (
+      <glamorous.View flexDirection="row">
+        <Text>• </Text>
+        <ListText {...this.props} />
+      </glamorous.View>
+    )
+  }
+}
