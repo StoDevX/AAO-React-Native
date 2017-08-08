2dde52d65472a9b2732894a93f668329e27bc8fd
Elijah Verdoorn <everdoorn@pandora.com>
2017-08-07 18:51:38 -0700

    Use SDK manager to accept ConstraintLayout License

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -30,8 +30,11 @@ npm install -g npm@latest
 # ssh-add "$DEPLOY_KEY"
 
 # Adding this to accept ConstraintLayout's license... see https://github.com/travis-ci/travis-ci/issues/6617
-if [[ $ANDROID ]]; then
-  mkdir -p "$ANDROID_HOME/licenses"
-  echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
-  echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
-fi
+echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.2"
+echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout-solver;1.0.2"
+
+#if [[ $ANDROID ]]; then
+  #mkdir -p "$ANDROID_HOME/licenses"
+  #echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
+  #echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
+#fi
