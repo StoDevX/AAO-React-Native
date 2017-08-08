301edb7be8bc617b795c5e2488247e4b035c5456
Hawken Rives <hawkrives@gmail.com>
2017-08-08 10:08:31 -0500

    fix up tests

--- source/views/calendar/__tests__/times.test.js
+++ source/views/calendar/__tests__/times.test.js
@@ -5,17 +5,31 @@ import {times, detailTimes} from '../times.js'
 
 describe('allDay', () => {
   test('should be true for a midnight-to-midnight event', () => {
-    const event = {startTime: moment('2017-08-07 12:00am'), endTime: moment('2017-08-08 12:00am')}
+    const event = {startTime: moment('2017-08-07T00:00:00Z'), endTime: moment('2017-08-08T00:00:00Z')}
     expect(times(event).allDay).toBe(true)
     expect(detailTimes(event).allDay).toBe(true)
   })
-  // should be true for a noon-to-noon event
-  // should be false for a non-24 hour event
+
+  test('should be true for a noon-to-noon event', () => {
+    const event = {startTime: moment('2017-08-07T12:00:00Z'), endTime: moment('2017-08-08T12:00:00Z')}
+    expect(times(event).allDay).toBe(true)
+    expect(detailTimes(event).allDay).toBe(true)
+  })
+
+  test('should be false for a non-24 hour event', () => {
+    const event = {startTime: moment('2017-08-07T12:00:00Z'), endTime: moment('2017-08-07T12:30:00Z')}
+    expect(times(event).allDay).toBe(false)
+    expect(detailTimes(event).allDay).toBe(false)
+  })
 })
 
-// describe: ongoing events
-// test: should be formatted
-// expect().toMatchSnapshot
+xdescribe('ongoing events', () => {
+  test('should be formatted', () => {
+    const event = {startTime: moment().subtract(1, 'hour'), endTime: moment().add(1, 'hour')}
+    expect(times(event)).toMatchSnapshot()
+    // expect(detailTimes(event)).toMatchSnapshot()
+  })
+})
 
 // multi-day events
 
