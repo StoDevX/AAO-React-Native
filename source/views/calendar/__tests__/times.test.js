d7a7bc2cf0e2091448290f1d21ff1e78289762ec
Hawken Rives <hawkrives@gmail.com>
2017-08-08 10:09:22 -0500

    prettify

--- source/views/calendar/__tests__/times.test.js
+++ source/views/calendar/__tests__/times.test.js
@@ -5,19 +5,28 @@ import {times, detailTimes} from '../times.js'
 
 describe('allDay', () => {
   test('should be true for a midnight-to-midnight event', () => {
-    const event = {startTime: moment('2017-08-07T00:00:00Z'), endTime: moment('2017-08-08T00:00:00Z')}
+    const event = {
+      startTime: moment('2017-08-07T00:00:00Z'),
+      endTime: moment('2017-08-08T00:00:00Z'),
+    }
     expect(times(event).allDay).toBe(true)
     expect(detailTimes(event).allDay).toBe(true)
   })
 
   test('should be true for a noon-to-noon event', () => {
-    const event = {startTime: moment('2017-08-07T12:00:00Z'), endTime: moment('2017-08-08T12:00:00Z')}
+    const event = {
+      startTime: moment('2017-08-07T12:00:00Z'),
+      endTime: moment('2017-08-08T12:00:00Z'),
+    }
     expect(times(event).allDay).toBe(true)
     expect(detailTimes(event).allDay).toBe(true)
   })
 
   test('should be false for a non-24 hour event', () => {
-    const event = {startTime: moment('2017-08-07T12:00:00Z'), endTime: moment('2017-08-07T12:30:00Z')}
+    const event = {
+      startTime: moment('2017-08-07T12:00:00Z'),
+      endTime: moment('2017-08-07T12:30:00Z'),
+    }
     expect(times(event).allDay).toBe(false)
     expect(detailTimes(event).allDay).toBe(false)
   })
@@ -25,7 +34,10 @@ describe('allDay', () => {
 
 xdescribe('ongoing events', () => {
   test('should be formatted', () => {
-    const event = {startTime: moment().subtract(1, 'hour'), endTime: moment().add(1, 'hour')}
+    const event = {
+      startTime: moment().subtract(1, 'hour'),
+      endTime: moment().add(1, 'hour'),
+    }
     expect(times(event)).toMatchSnapshot()
     // expect(detailTimes(event)).toMatchSnapshot()
   })
