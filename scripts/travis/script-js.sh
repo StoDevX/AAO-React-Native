2662a9bda66bb34b92777e5a59fd9f58d47ed86d
Hawken Rives <hawkrives@gmail.com>
2017-08-08 09:55:19 -0500

    time all commands in script-js

--- scripts/travis/script-js.sh
+++ scripts/travis/script-js.sh
@@ -2,22 +2,22 @@
 set -e -v -o pipefail
 
 # Ensure prettiness
-npm run prettier
+time npm run prettier
 if ! git diff --quiet ./*.js source/; then
   git diff ./*.js source/ > logs/prettier
 fi
 
 # Lint
-npm run lint | tee logs/eslint
+time npm run lint | tee logs/eslint
 
 # Validate data
-npm run validate-data -- --quiet | tee logs/validate-data
+time npm run validate-data -- --quiet | tee logs/validate-data
 
 # Update the data files
-npm run bundle-data
+time npm run bundle-data
 
 # Type check
-npm run flow -- check --quiet | tee logs/flow
+time npm run flow -- check --quiet | tee logs/flow
 
 # Run tests + collect coverage info
-npm run test -- --coverage 2>&1 | tee logs/jest
+time npm run test -- --coverage 2>&1 | tee logs/jest
