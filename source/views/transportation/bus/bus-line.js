7a6aea52054a8ca35032205c79a64d1f7ae770db
Hawken Rives <hawkrives@gmail.com>
2017-09-05 07:51:41 -0500

    Update bus-line.js

--- source/views/transportation/bus/bus-line.js
+++ source/views/transportation/bus/bus-line.js
@@ -123,8 +123,8 @@ export class BusLine extends React.PureComponent {
               isFirstRow={i === 0}
               isLastRow={i === list.length - 1}
             />
-            {i < list.length - 1 ? 
-              <Separator style={styles.separator} />
+            {i < list.length - 1
+              ? <Separator style={styles.separator} />
               : null}
           </View>,
         )}
