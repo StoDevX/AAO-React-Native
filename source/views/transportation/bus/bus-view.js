089533a335f79ae1f52ef4d92460668cddc5e08c
Hawken Rives <hawkrives@gmail.com>
2017-09-10 22:39:35 -0500

    fix time delay calculation in BusView

--- source/views/transportation/bus/bus-view.js
+++ source/views/transportation/bus/bus-view.js
@@ -56,7 +56,7 @@ export class BusView extends React.PureComponent {
     this.updateTime()
 
     // wait 0.5 seconds – if we let it go at normal speed, it feels broken.
-    const elapsed = start - Date.now()
+    const elapsed = Date.now() - start
     if (elapsed < 500) {
       await delay(500 - elapsed)
     }
