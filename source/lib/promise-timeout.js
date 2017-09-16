a845c317e7a2441804b19254b17dd3556d2da5ea
Hawken Rives <hawkrives@gmail.com>
2017-09-16 12:39:30 -0500

    add return annotation to promiseTimeout

--- source/lib/promise-timeout.js
+++ source/lib/promise-timeout.js
@@ -1,6 +1,6 @@
 // @flow
 
-export function promiseTimeout(ms: number, promise: Promise<any>) {
+export function promiseTimeout(ms: number, promise: Promise<any>): Promise<any> {
   return new Promise(function(resolve, reject) {
     // create a timeout to reject promise if not resolved
     let timer = setTimeout(function() {
