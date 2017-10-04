0b17f7ea97a6094ac9509b82b9cf3b90a0ae2e69
Hawken Rives <hawkrives@gmail.com>
2017-10-03 19:49:10 -0500

    update news for new flow

--- source/views/news/news-row.js
+++ source/views/news/news-row.js
@@ -6,13 +6,13 @@ import {Column, Row} from '../components/layout'
 import {ListRow, Detail, Title} from '../components/list'
 import type {StoryType} from './types'
 
-export class NewsRow extends React.PureComponent {
-  props: {
-    onPress: StoryType => any,
-    story: StoryType,
-    thumbnail: number,
-  }
+type Props = {
+  onPress: StoryType => any,
+  story: StoryType,
+  thumbnail: number,
+}
 
+export class NewsRow extends React.PureComponent<Props> {
   _onPress = () => this.props.onPress(this.props.story)
 
   render() {
