960d4bf772fd73414dd12a3be8bff93e2b89e39c
Kristofer Rye <kristofer.rye@gmail.com>
2017-10-12 11:35:12 -0500

    Fix a typo

--- source/user-agent.js
+++ source/user-agent.js
@@ -5,6 +5,6 @@ import {Platform} from 'react-native'
 
 const aaoVersion = version || 'unknown'
 const platformString = Platform.OS == 'ios' ? 'iOS' : 'Android' || 'unknown'
-const platformVersion = Platform.version || 'unknown'
+const platformVersion = Platform.Version || 'unknown'
 
 export const AAO_USER_AGENT = `AllAboutOlaf/${aaoVersion} (${platformString}/${platformVersion})`
