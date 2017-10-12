81c997290317808a09474ca633e3cac40b0a2244
Kristofer Rye <kristofer.rye@gmail.com>
2017-10-12 11:24:53 -0500

    Clean up the version stuff, use more constants

--- source/user-agent.js
+++ source/user-agent.js
@@ -3,7 +3,8 @@
 import {version} from '../package.json'
 import {Platform} from 'react-native'
 
-const platformString = Platform.OS == 'ios' ? 'iOS' : 'Android'
-const platformVersion = Platform.getVersion()
+const aaoVersion = version || 'unknown'
+const platformString = Platform.OS == 'ios' ? 'iOS' : 'Android' || 'unknown'
+const platformVersion = Platform.version || 'unknown'
 
-const AAO_USER_AGENT = `AllAboutOlaf/${version} (${platformString}/${platformVersion})`
+export const AAO_USER_AGENT = `AllAboutOlaf/${aaoVersion} (${platformString}/${platformVersion})`
