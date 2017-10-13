2338fa817c9e908613e3db8657f48584df21c432
Kristofer Rye <kristofer.rye@gmail.com>
2017-10-12 22:55:26 -0500

    Fix ternary logic to actually allow for "unknown" platforms
    Prettier is strange.

--- source/user-agent.js
+++ source/user-agent.js
@@ -4,7 +4,10 @@ import {version} from '../package.json'
 import {Platform} from 'react-native'
 
 const aaoVersion = version || 'unknown'
-const platformString = Platform.OS == 'ios' ? 'iOS' : 'Android' || 'unknown'
+const platformString =
+  Platform.OS == 'ios'
+    ? 'iOS'
+    : Platform.OS == 'android' ? 'Android' : 'unknown'
 const platformVersion = Platform.Version || 'unknown'
 
 export const AAO_USER_AGENT = `AllAboutOlaf/${aaoVersion} (${platformString}/${platformVersion})`
