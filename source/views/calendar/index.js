b39ccbbbc805a3adac7927baeaab5861a6da680d
Hawken Rives <hawkrives@gmail.com>
2017-10-17 09:54:37 -0500

    Update St. Olaf calendar URL
    We have changed calendar backend systems, so the old URL will go away soon. This is, I believe, the new ID for the master St. Olaf calendar.
    
    It has the (dis)advantage of including Alumni and Athletics events, but we're not sure how to exclude them yet.

--- source/views/calendar/index.js
+++ source/views/calendar/index.js
@@ -18,7 +18,7 @@ export default TabNavigator(
       screen: ({navigation}) => (
         <GoogleCalendarView
           navigation={navigation}
-          calendarId="le6tdd9i38vgb7fcmha0hu66u9gjus2e@import.calendar.google.com"
+          calendarId=" 5g91il39n0sv4c2bjdv1jrvcpq4ulm4r@import.calendar.google.com"
         />
       ),
       navigationOptions: {
