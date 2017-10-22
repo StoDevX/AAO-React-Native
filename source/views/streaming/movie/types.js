c2fcecae86d91b95891d301980866b8434983900
Hawken Rives <hawkrives@gmail.com>
2017-10-22 16:33:39 -0500

    add type information for the api endpoint

---
+++ source/views/streaming/movie/types.js
@@ -0,0 +1,45 @@
+// @flow
+
+export type RGBTuple = [number, number, number]
+
+export type PosterInfo = {
+  url: string,
+  filename: string,
+  width: number,
+  height: number,
+}
+
+export type MovieShowing = {time: string, location: string}
+
+export type MovieInfo = {
+  Title: string,
+  Year: string,
+  Rated: string,
+  Released: string,
+  Runtime: string,
+  Genre: string,
+  Director: string,
+  Writer: string,
+  Actors: string,
+  Plot: string,
+  Language: string,
+  Country: string,
+  Awards: string,
+  Ratings: Array<{Source: string, Value: string}>,
+  Type: string,
+  DVD: string,
+  BoxOffice: string,
+  Production: string,
+  Website: string,
+}
+
+export type Movie = {
+  root: string,
+  info: MovieInfo,
+  showings: Array<MovieShowing>,
+  posters: Array<PosterInfo>,
+  posterColors: {
+    dominant: RGBTuple,
+    palette: Array<RGBTuple>,
+  },
+}
