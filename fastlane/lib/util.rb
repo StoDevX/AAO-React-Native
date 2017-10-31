6443757ea4e2176968e9b6912a0b2918caf1a9ec
Kristofer Rye <kristofer.rye@gmail.com>
2017-10-30 23:32:57 -0500

    Add #should_deploy? and #cron? helper methods to fastlane util
    This can be used in place of inline logic and potentially centralizes
    the logic in that place.

--- fastlane/lib/util.rb
+++ fastlane/lib/util.rb
@@ -118,3 +118,13 @@ def auto_beta
     build
   end
 end
+
+def should_deploy?
+	cron? ||
+		!ENV['TRAVIS_TAG'].empty? ||
+		ENV['TRAVIS_COMMIT_MESSAGE'] =~ /\[ci run beta\]/
+end
+
+def cron?
+	ENV['TRAVIS_EVENT_TYPE'] == 'cron'
+end
