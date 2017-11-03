9b8f11df782d6c9e0168f50fe27106fbc70698f5
Hawken Rives <hawkrives@gmail.com>
2017-11-02 22:24:50 -0500

    handle the array mode of dietary icons

--- source/views/menus/components/dietary-tags.js
+++ source/views/menus/components/dietary-tags.js
@@ -29,7 +29,9 @@ export function DietaryTags({
   style?: any,
 }) {
   // filter the mapping of all icons by just the icons provided by this item
-  let filtered = pick(corIcons, keys(dietary))
+  let filtered = Array.isArray(dietary)
+    ? pick(corIcons, [])
+    : pick(corIcons, keys(dietary))
 
   // turn the remaining items into images
   let tags = map(filtered, (dietaryIcon, key) => (
