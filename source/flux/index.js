a2e9e23dca5016179aaee676ee4bc554012c1515
Hawken Rives <hawkrives@gmail.com>
2017-11-05 09:52:34 -0600

    use combineReducers in flux/index

--- source/flux/index.js
+++ source/flux/index.js
@@ -1,30 +1,34 @@
-/**
- * @flow
- * Root reducer for state storage
- */
+// @flow
 
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
 import {createLogger} from 'redux-logger'
 import reduxPromise from 'redux-promise'
 import reduxThunk from 'redux-thunk'
 
-import {app} from './parts/app'
-import {homescreen} from './parts/homescreen'
-import {menus} from './parts/menus'
-import {settings} from './parts/settings'
-import {sis} from './parts/sis'
+import {app, type State as AppState} from './parts/app'
+import {homescreen, type State as HomescreenState} from './parts/homescreen'
+import {menus, type State as MenusState} from './parts/menus'
+import {settings, type State as SettingsState} from './parts/settings'
+import {sis, type State as SisState} from './parts/sis'
 
-export function aao(state: Object = {}, action: Object) {
-  return {
-    app: app(state.app, action),
-    homescreen: homescreen(state.homescreen, action),
-    menus: menus(state.menus, action),
-    settings: settings(state.settings, action),
-    sis: sis(state.sis, action),
-  }
-}
 export {init as initRedux} from './init'
 export {updateMenuFilters} from './parts/menus'
+
+export type ReduxState = {
+  app?: AppState,
+  homescreen?: HomescreenState,
+  menus?: MenusState,
+  settings?: SettingsState,
+  sis?: SisState,
+}
+
+export const aao = combineReducers({
+  app,
+  homescreen,
+  menus,
+  settings,
+  sis,
+})
 
 const logger = createLogger({collapsed: () => true})
 export const store = createStore(
