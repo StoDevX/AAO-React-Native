e388d17c6430161a1bc359db17b082bfea809897
Hawken Rives <hawkrives@gmail.com>
2017-11-05 09:50:19 -0600

    remove the inactive courses and coursesearch files

--- source/views/sis/index.js
+++ source/views/sis/index.js
@@ -1,22 +1,14 @@
 // @flow
-/**
- * All About Olaf
- * iOS SIS page
- */
 
 import {TabNavigator} from '../components/tabbed-view'
 
 import BalancesView from './balances'
 import StudentWorkView from './student-work'
-// import CoursesView from './courses'
-// import SearchView from './search'
 
 export default TabNavigator(
   {
     BalancesView: {screen: BalancesView},
     StudentWorkView: {screen: StudentWorkView},
-    // CoursesView: {screen: CoursesView},
-    // CourseSearchView: {screen: CourseSearchView},
   },
   {
     navigationOptions: {
