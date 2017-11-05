57796549be93220196565205d550573ec1f16cd4
Hawken Rives <hawkrives@gmail.com>
2017-11-05 09:49:58 -0600

    retype storage.js

--- source/lib/storage.js
+++ source/lib/storage.js
@@ -10,7 +10,7 @@ export function clearAsyncStorage() {
 function setItem(key: string, value: any) {
   return AsyncStorage.setItem(`aao:${key}`, JSON.stringify(value))
 }
-function getItem(key: string): Promise<any> {
+function getItem(key: string): Promise<?any> {
   return AsyncStorage.getItem(`aao:${key}`).then(stored => JSON.parse(stored))
 }
 function removeItem(key: string): Promise<any> {
@@ -23,24 +23,24 @@ const analyticsOptOutKey = 'settings:opt-out'
 export function setAnalyticsOptOut(status: boolean) {
   return setItem(analyticsOptOutKey, status)
 }
-export function getAnalyticsOptOut(): Promise<boolean> {
-  return getItem(analyticsOptOutKey)
+export async function getAnalyticsOptOut(): Promise<boolean> {
+  return (await getItem(analyticsOptOutKey)) || false
 }
 
 const homescreenOrderKey = 'homescreen:view-order'
 export function setHomescreenOrder(order: string[]) {
   return setItem(homescreenOrderKey, order)
 }
-export function getHomescreenOrder(): Promise<string[]> {
-  return getItem(homescreenOrderKey)
+export async function getHomescreenOrder(): Promise<string[]> {
+  return (await getItem(homescreenOrderKey)) || []
 }
 
 const acknowledgementStatusKey = 'settings:ackd'
 export function setAcknowledgementStatus(status: boolean) {
   return setItem(acknowledgementStatusKey, status)
 }
-export function getAcknowledgementStatus(): Promise<?boolean> {
-  return getItem(acknowledgementStatusKey)
+export async function getAcknowledgementStatus(): Promise<boolean> {
+  return (await getItem(acknowledgementStatusKey)) || false
 }
 
 /// MARK: Credentials
@@ -49,8 +49,8 @@ const tokenValidKey = 'credentials:valid'
 export function setTokenValid(valid: boolean) {
   return setItem(tokenValidKey, valid)
 }
-export function getTokenValid(): Promise<boolean> {
-  return getItem(tokenValidKey)
+export async function getTokenValid(): Promise<boolean> {
+  return (await getItem(tokenValidKey)) || false
 }
 export function clearTokenValid(): Promise<any> {
   return removeItem(tokenValidKey)
@@ -60,6 +60,6 @@ const credentialsValidKey = 'olafCredentials:valid'
 export function setCredentialsValid(valid: boolean) {
   return setItem(credentialsValidKey, valid)
 }
-export function getCredentialsValid(): Promise<boolean> {
-  return getItem(credentialsValidKey)
+export async function getCredentialsValid(): Promise<boolean> {
+  return (await getItem(credentialsValidKey)) || false
 }
