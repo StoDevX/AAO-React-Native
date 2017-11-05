4dd2905eff54d4399972996c73dd767f856ba47f
Hawken Rives <hawkrives@gmail.com>
2017-11-05 14:29:46 -0600

    restrict arguments to get-urls

--- flow-typed/npm/get-urls_v7.x.x.js
+++ flow-typed/npm/get-urls_v7.x.x.js
@@ -1,7 +1,7 @@
 // flow-typed signature: 5826e9e5d0a2cd094907edb6ed550328
 // flow-typed version: <<STUB>>/get-urls_v7.0.0/flow_v0.56.0
 
-type $npm$getUrls$7_x_x$normalizeUrlOptions = {
+type $npm$getUrls$7_x_x$normalizeUrlOptions = {|
     normalizeProtocol: boolean,
     normalizeHttps: boolean,
     stripFragment: boolean,
@@ -9,7 +9,7 @@ type $npm$getUrls$7_x_x$normalizeUrlOptions = {
     removeQueryParameters: boolean|Array<RegExp|string>,
     removeTrailingSlash: boolean,
     removeDirectoryIndex: boolean|Array<RegExp|string>,
-};
+|};
 
 declare module 'get-urls' {
   declare export default function getUrls(
