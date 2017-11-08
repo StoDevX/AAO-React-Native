c4f362b4dd2eb135f61cd95c53d658e8ba83a4ac
Hawken Rives <hawkrives@gmail.com>
2017-11-07 20:00:58 -0600

    remove typedef for minimist
    I'm overriding this to specify "any" because React Native has a spot where they pass a
    possibly-boolean argument to path.resolve, which only takes strings.

--- flow-typed/npm/minimist_v1.x.x.js
+++ flow-typed/npm/minimist_v1.x.x.js
@@ -1,6 +1,7 @@
 // flow-typed signature: 50bc453586282fb18e63201750049659
 // flow-typed version: e6f7626e10/minimist_v1.x.x/flow_>=v0.28.x
 
+/*
 declare module 'minimist' {
   declare type minimistOptions = {
     string?: string | Array<string>,
@@ -19,4 +20,12 @@ declare module 'minimist' {
   };
 
   declare module.exports: (argv: Array<string>, opts?: minimistOptions) => minimistOutput;
+}
+*/
+
+declare module 'minimist' {
+  // I'm overriding this to specify "any" because React Native has a spot
+  // where they pass a possibly-boolean argument to path.resolve, which only
+  // takes strings.
+  declare module.exports: any;
 }
