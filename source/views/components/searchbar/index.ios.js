6108a428edcf2fc4697f79e0be464ca521f9f7c7
Hawken Rives <hawkrives@gmail.com>
2017-11-08 10:39:56 -0600

    use my fork of react-native-search-bar in the code

--- source/views/components/searchbar/index.ios.js
+++ source/views/components/searchbar/index.ios.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import {StyleSheet} from 'react-native'
-import NativeSearchBar from 'react-native-search-bar'
+import NativeSearchBar from '@hawkrives/react-native-search-bar'
 import * as c from '../colors'
 
 const styles = StyleSheet.create({
