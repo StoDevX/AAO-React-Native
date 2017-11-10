918b1f949d089c1d9d54ed3c2b7b96ac5ac1ddc2
Hawken Rives <hawkrives@gmail.com>
2017-11-09 20:04:33 -0600

    remove disabled imports of Courses/CourseSearchView

--- source/views/sis/index.js
+++ source/views/sis/index.js
@@ -8,15 +8,11 @@ import {TabNavigator} from '../components/tabbed-view'
 
 import BalancesView from './balances'
 import StudentWorkView from './student-work'
-// import CoursesView from './courses'
-// import SearchView from './search'
 
 export default TabNavigator(
   {
     BalancesView: {screen: BalancesView},
     StudentWorkView: {screen: StudentWorkView},
-    // CoursesView: {screen: CoursesView},
-    // CourseSearchView: {screen: CourseSearchView},
   },
   {
     navigationOptions: {
