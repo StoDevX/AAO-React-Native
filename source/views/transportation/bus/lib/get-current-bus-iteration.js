d3bdaca9c152108be92ccc39e90d0dc8921b3d70
Hawken Rives <hawkrives@gmail.com>
2017-11-12 00:44:06 -0600

    prettify

--- source/views/transportation/bus/lib/get-current-bus-iteration.js
+++ source/views/transportation/bus/lib/get-current-bus-iteration.js
@@ -79,7 +79,12 @@ export function getCurrentBusIteration(
 
     // Check if we're between two iterations
     if (lastStopTime && nextStart && now.isBetween(lastStopTime, nextStart)) {
-      return {status: 'between-rounds', times: nextTimes, index: index + 1, nextStart}
+      return {
+        status: 'between-rounds',
+        times: nextTimes,
+        index: index + 1,
+        nextStart,
+      }
     }
 
     // If we're not, then return "running"
