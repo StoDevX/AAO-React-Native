fef808e4ebd5dc7c3bb20eb393c8b168f854cb51
Hawken Rives <hawkrives@gmail.com>
2017-12-30 15:11:11 -0600

    remove use of __dirname from paths.mjs

--- scripts/paths.mjs
+++ scripts/paths.mjs
@@ -1,4 +1,8 @@
 import path from 'path'
 
-export const DATA_BASE = path.join(__dirname, '..', 'data')
-export const SCHEMA_BASE = path.join(__dirname, '..', 'data', '_schema')
+import findUp from 'find-up'
+
+const root = path.dirname(findUp.sync('package.json'))
+
+export const DATA_BASE = path.join(root, 'data')
+export const SCHEMA_BASE = path.join(root, 'data', '_schema')
