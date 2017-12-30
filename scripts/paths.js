b26fe6b179ed0e54801d837e67ced84fd15ec6f2
Hawken Rives <hawkrives@gmail.com>
2017-12-30 14:35:40 -0600

    move some files around

--- scripts/paths.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const path = require('path')
-
-module.exports.DATA_BASE = path.join(__dirname, '..', 'data')
-module.exports.SCHEMA_BASE = path.join(__dirname, '..', 'data', '_schemas')
---
+++ scripts/paths.mjs
@@ -0,0 +1,4 @@
+import path from 'path'
+
+export const DATA_BASE = path.join(__dirname, '..', 'data')
+export const SCHEMA_BASE = path.join(__dirname, '..', 'data', '_schema')
