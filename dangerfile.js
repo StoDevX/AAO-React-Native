bf97926be934012ef7a0fa8f53ff76fd80068734
Hawken Rives <hawkrives@gmail.com>
2017-12-30 21:55:30 -0600

    *pokes more*

--- dangerfile.js
+++ dangerfile.js
@@ -174,6 +174,7 @@ async function runGeneral() {
         const value = buildConfig[key]
         const searchPaths = value.buildSettings.LIBRARY_SEARCH_PATHS
         message(h.code(JSON.stringify(searchPaths)))
+        message(h.code(JSON.stringify(uniq(searchPaths))))
         return false
         return uniq(searchPaths).length === searchPaths.length
       })
