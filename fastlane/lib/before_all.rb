659ea668c20ab4865f40d166f8cb25bbc1a08c03
Hawken Rives <hawkrives@gmail.com>
2018-01-06 10:39:53 -0600

    remove XCBUILD_TARGET_BUILD_DIR override

--- fastlane/lib/before_all.rb
+++ fastlane/lib/before_all.rb
@@ -8,7 +8,6 @@ before_all do
   ENV['GYM_PROJECT'] = './ios/AllAboutOlaf.xcodeproj'
   ENV['GYM_SCHEME'] = 'AllAboutOlaf'
   ENV['GYM_OUTPUT_DIRECTORY'] = './ios/build'
-  ENV['XCBUILD_TARGET_BUILD_DIR'] = './build' # relative to the ./ios folder
   ENV['GYM_OUTPUT_NAME'] = 'AllAboutOlaf'
 
   # set the testflight itunesconnect provider ID from Appfile
@@ -22,12 +21,10 @@ before_all do
 
   lane_context[:GRADLE_FILE] = "#{ENV['FL_GRADLE_PROJECT_DIR']}/app/build.gradle"
 
-  UI.message "FL_HOCKEY_COMMIT_SHA is #{ENV['FL_HOCKEY_COMMIT_SHA']}"
   UI.message "GYM_PROJECT is #{ENV['GYM_PROJECT']}"
   UI.message "GYM_SCHEME is #{ENV['GYM_SCHEME']}"
   UI.message "GYM_OUTPUT_DIRECTORY is #{ENV['GYM_OUTPUT_DIRECTORY']}"
   UI.message "GYM_OUTPUT_NAME is #{ENV['GYM_OUTPUT_NAME']}"
-  UI.message "XCBUILD_TARGET_BUILD_DIR is #{ENV['XCBUILD_TARGET_BUILD_DIR']}"
   UI.message "FL_GRADLE_PROJECT_DIR is #{ENV['FL_GRADLE_PROJECT_DIR']}"
   UI.message "PRETTY_APP_NAME is #{lane_context[:PRETTY_APP_NAME]}"
   UI.message "GRADLE_FILE is #{lane_context[:GRADLE_FILE]}"
