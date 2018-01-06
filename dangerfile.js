f4eb4a595ee423680ff3f2cecba3a2e8a87fbd39
Hawken Rives <hawkrives@gmail.com>
2018-01-06 14:36:04 -0600

    flesh out danger task: jest

--- dangerfile.js
+++ dangerfile.js
@@ -200,6 +200,35 @@ function runJSのFlow() {
 //
 
 function runJSのJest() {
+  const jestLog = readLogFile('./logs/jest')
+
+  if (!jestLog) {
+    return
+  }
+
+  if (!jestLog.includes('FAIL')) {
+    return
+  }
+
+  const lines = getRelevantLinesJest(jestLog)
+
+  fileLog('Some Jest tests failed. Take a peek?', lines.join('\n'))
+}
+
+function getRelevantLinesJest(logContents) {
+  const file = logContents.split('\n')
+
+  const startIndex = findIndex(
+    file,
+    l => l.trim() === 'Summary of all failing tests',
+  )
+  const endIndex = findIndex(
+    file,
+    l => l.trim() === 'Ran all test suites.',
+    startIndex,
+  )
+
+  return file.slice(startIndex + 1, endIndex - 1)
 }
 
 //
