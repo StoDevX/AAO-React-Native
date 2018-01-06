39a72a3a3f07bacf4bcd4e1bc94f764738cd4881
Hawken Rives <hawkrives@gmail.com>
2018-01-06 14:40:13 -0600

    flesh out runiOS

--- dangerfile.js
+++ dangerfile.js
@@ -69,6 +69,43 @@ function runAndroid() {
 //
 
 function runiOS() {
+  const logFile = readLogFile('./logs/build').split('\n')
+  const buildStatus = readLogFile('./logs/build-status')
+
+  markdown('## iOS Report')
+
+  if (buildStatus !== '0') {
+    // returning early here because if the build fails, there's nothing to analyze
+    fastlaneBuildLogTail(logFile, 'iOS Build Failed')
+    return
+  }
+
+  // ideas:
+  // - tee the "fastlane" output to a log, and run the analysis script
+  //   to report back the longest compilation units
+  //   (maybe only on react-native / package.json changes?)
+  const analysisFile = readFile('./logs/analysis')
+  markdown(
+    h.details(
+      h.summary('Analysis of slow build times (>20s)'),
+      m.code({}, analysisFile),
+    ),
+  )
+
+  /*
+  // - report the .ipa size
+  // - report the .ipa file list
+  const appPaths = readJsonLogFile('./logs/products')
+
+  appPaths.forEach(appPath => {
+    const info = listDirectoryTree(appPath)
+    markdown(`## <code>.app</code>
+Total <code>.app</code> size: ${info.size}
+
+${h.details(h.summary('.app contents'), m.json(info))}
+`)
+  })
+*/
 }
 
 //
