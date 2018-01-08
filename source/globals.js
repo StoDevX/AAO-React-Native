a370ce0ffa7a3341241d51ef9b4dca37a703b12c
Kristofer Rye <kristofer.rye@gmail.com>
2018-01-07 21:09:42 -0600

    Run prettier

--- source/globals.js
+++ source/globals.js
@@ -4,4 +4,5 @@ export const GH_PAGES_URL = (relativePath: string) => {
   return `https://stodevx.github.io/AAO-React-Native/${relativePath}`
 }
 
-export const GH_NEW_ISSUE_URL = 'https://github.com/StoDevX/AAO-React-Native/issues/new'
+export const GH_NEW_ISSUE_URL =
+  'https://github.com/StoDevX/AAO-React-Native/issues/new'
