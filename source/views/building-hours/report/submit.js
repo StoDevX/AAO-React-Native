fdaf175ae855516868a171551f8b06ef5613c47d
Kristofer Rye <kristofer.rye@gmail.com>
2018-01-07 21:23:05 -0600

    Fix import for Building Hours

--- source/views/building-hours/report/submit.js
+++ source/views/building-hours/report/submit.js
@@ -4,7 +4,7 @@ import jsYaml from 'js-yaml'
 import type {BuildingType} from '../types'
 import {email} from 'react-native-communications'
 import querystring from 'querystring'
-import {GH_NEW_ISSUE_URL} from '../../globals'
+import {GH_NEW_ISSUE_URL} from '../../../globals'
 
 export function submitReport(current: BuildingType, suggestion: BuildingType) {
   // calling trim() on these to remove the trailing newlines
