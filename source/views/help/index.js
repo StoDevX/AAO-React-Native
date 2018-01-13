7d2bddc4b71827b2bcf2495a9caaa8aeb1c246cf
Hawken Rives <hawkrives@gmail.com>
2018-01-13 12:39:03 -0600

    connect Help to redux

--- source/views/help/index.js
+++ source/views/help/index.js
@@ -1,54 +1,102 @@
 // @flow
 
 import * as React from 'react'
+import {connect} from 'react-redux'
 import {ScrollView, StyleSheet, View, Text} from 'react-native'
+import {type TopLevelViewPropsType} from '../types'
+import {type ReduxState} from '../../flux'
+import {getEnabledTools} from '../../flux/parts/help'
 import * as wifi from './wifi'
 import * as helpdesk from './helpdesk'
 import * as facilities from './facilities'
+
+const ALL_TOOLS = [wifi, helpdesk, facilities]
 
 export type ReportProblemToolNamesEnum =
   | wifi.ToolName
   | facilities.ToolName
   | helpdesk.ToolName
 
-export type ReportProblemToolOptions = {
-  wifi?: wifi.ToolOptions,
-  'it-helpdesk'?: helpdesk.ToolOptions,
-  'facilities-work-order'?: facilities.ToolOptions,
+export type ReportProblemTools =
+  | wifi.ToolOptions
+  | helpdesk.ToolOptions
+  | facilities.ToolOptions
+
+type ReduxStateProps = {
+  fetching: boolean,
+  tools: Array<ReportProblemTools>,
 }
 
-type Props = {
-  enabledTools: Array<ReportProblemToolNamesEnum>,
-  toolOptions: ReportProblemToolOptions,
+type ReduxDispatchProps = {
+  getEnabledTools: () => mixed,
 }
 
-export default class HelpView extends React.Component<Props> {
+type Props = TopLevelViewPropsType & ReduxStateProps & ReduxDispatchProps
+
+export class HelpView extends React.Component<Props> {
   static navigationOptions = {
     title: 'Help',
   }
 
+  componentWillMount() {
+    this.props.getEnabledTools()
+  }
+
   render() {
-    const allTools = [wifi, helpdesk, facilities]
-    const enabled = allTools
-      .filter(tool => this.props.enabledTools.includes(tool.toolName))
-      .map(tool => (
-        <tool.ToolView
-          key={tool.toolName}
-          options={this.props.toolOptions[tool.toolName]}
-        />
-      ))
+    if (this.props.fetching) {
+      return <LoadingScreen />
+    }
+
+    const tools = ALL_TOOLS.map(tool => [
+      tool,
+      this.props.tools.find(conf => conf.key === tool.toolName),
+    ])
+
+    const views = tools.map(
+      ([tool, config]) =>
+        config ? <tool.ToolView key={config.key} options={config} /> : null,
+    )
 
     return (
       <ScrollView style={styles.contentContainer}>
-        {enabled.length ? enabled : <EmptyListView />}
+        {views.length ? views : <EmptyListView />}
       </ScrollView>
     )
   }
 }
 
+function mapState(state: ReduxState): ReduxStateProps {
+  if (!state.help) {
+    return {
+      fetching: false,
+      tools: [],
+    }
+  }
+
+  return {
+    fetching: state.help.fetching,
+    tools: state.help.tools,
+    lastFetchError: state.help.lastFetchError,
+  }
+}
+
+function mapDispatch(dispatch): ReduxDispatchProps {
+  return {
+    getEnabledTools: () => dispatch(getEnabledTools()),
+  }
+}
+
+export default connect(mapState, mapDispatch)(HelpView)
+
 const EmptyListView = () => (
   <View>
     <Text>No tools are enabled.</Text>
+  </View>
+)
+
+const LoadingScreen = () => (
+  <View>
+    <Text>Loading…</Text>
   </View>
 )
 
