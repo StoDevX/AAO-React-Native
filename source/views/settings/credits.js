f1903b1302028cd5cb4372a70199abd788b3f71b
Hawken Rives <hawkrives@gmail.com>
2018-01-14 20:07:52 -0600

    tweak the Credits view, including adding a white background

--- source/views/settings/credits.js
+++ source/views/settings/credits.js
@@ -1,71 +1,75 @@
 // @flow
 import * as React from 'react'
-import {Text, ScrollView, Image, StyleSheet} from 'react-native'
+import * as c from '../components/colors'
 import {data as credits} from '../../../docs/credits.json'
+import glamorous from 'glamorous-native'
+import {Platform} from 'react-native'
+import {iOSUIKit, material} from 'react-native-typography'
 
 const image = require('../../../images/about/IconTrans.png')
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginLeft: 5,
-    marginRight: 5,
-    paddingBottom: 10,
-  },
-  aboutText: {
-    paddingLeft: 25,
-    paddingRight: 25,
-    paddingTop: 10,
-    lineHeight: 20,
-    textAlign: 'justify',
-  },
-  title: {
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    fontSize: 18,
-    paddingBottom: 5,
-  },
-  logo: {
-    width: 100,
-    height: 100,
-    margin: 20,
-    marginBottom: 10,
-    alignSelf: 'center',
-  },
-  nameList: {
-    textAlign: 'center',
-    paddingLeft: 25,
-    paddingRight: 25,
-    paddingBottom: 20,
-    lineHeight: 20,
-  },
-  last: {
-    marginBottom: 30,
-  },
+const Container = glamorous.scrollView({
+  backgroundColor: c.white,
+  paddingHorizontal: 5,
+  paddingVertical: 10,
 })
 
+const Logo = glamorous.image({
+  width: 100,
+  height: 100,
+  alignSelf: 'center',
+})
+
+const Title = glamorous.text({
+  ...Platform.select({
+    ios: iOSUIKit.largeTitleEmphasizedObject,
+    material: material.display4Object,
+  }),
+  textAlign: 'center',
+  marginTop: 10,
+  marginBottom: 5,
+})
+
+const Heading = glamorous.text({
+  ...Platform.select({
+    ios: iOSUIKit.subheadEmphasizedObject,
+    android: material.titleObject,
+  }),
+})
+
+const About = glamorous.text({
+  ...Platform.select({
+    ios: iOSUIKit.bodyObject,
+    android: material.body1Object,
+  }),
+  paddingHorizontal: 25,
+  paddingTop: 10,
+})
+
+const Contributors = glamorous(About)({
+  ...Platform.select({
+    ios: iOSUIKit.footnoteEmphasizedObject,
+    android: material.body1Object,
+  }),
+  textAlign: 'center',
+})
+
+const formatPeopleList = arr => arr.map(w => w.replace(' ', ' ')).join(' • ')
+
 export default function CreditsView() {
-  let formattedContributors = credits.contributors
-    .map(w => w.replace(' ', ' '))
-    .join(' • ')
-
-  let formattedAcks = credits.acknowledgements
-    .map(w => w.replace(' ', ' '))
-    .join(' • ')
-
   return (
-    <ScrollView style={styles.container}>
-      <Image source={image} style={styles.logo} />
+    <Container contentInsetAdjustmentBehavior="automatic">
+      <Logo source={image} />
 
-      <Text style={styles.title}>{credits.name}</Text>
-      <Text style={styles.aboutText}>{credits.content}</Text>
+      <Title>{credits.name}</Title>
+      <About>{credits.content}</About>
 
-      <Text style={styles.title}>Contributors</Text>
-      <Text style={styles.nameList}>{formattedContributors}</Text>
+      <Heading>Contributors</Heading>
+      <Contributors>{formatPeopleList(credits.contributors)}</Contributors>
 
-      <Text style={styles.title}>Acknowledgements</Text>
-      <Text style={[styles.nameList, styles.last]}>{formattedAcks}</Text>
-    </ScrollView>
+      <Heading>Acknowledgements</Heading>
+      <Contributors>{formatPeopleList(credits.acknowledgements)}</Contributors>
+    </Container>
   )
 }
 CreditsView.navigationOptions = {
