cba545c33ea08e3d444f646d84e90bcef3ec148c
Johannes Carlsen <hannes.mcman@gmail.com>
2018-01-23 14:36:39 -0600

    Convert to NoticeView and add header to NoticeView

--- source/views/sis/tes.js
+++ source/views/sis/tes.js
@@ -1,22 +1,46 @@
 // @flow
 
 import * as React from 'react'
-import {View, Component} from 'react-native'
+import {View, StyleSheet} from 'react-native'
 import {TabBarIcon} from '../components/tabbar-icon'
 import openUrl from '../components/open-url'
+import type {TopLevelViewPropsType} from '../types'
+import * as c from '../components/colors'
+import {NoticeView} from '../components/notice'
 
-class TESView extends React.Component {
-  static navigationOptions = {
+type Props = TopLevelViewPropsType
+
+export default class TESView extends React.PureComponent<Props> {
+	static navigationOptions = {
 		tabBarLabel: 'TES',
-		tabBarIcon: TabBarIcon('card'),
+		tabBarIcon: TabBarIcon('cash'),
 	}
 
-  componentDidMount() {
-    openUrl('https://www.stolaf.edu/apps/tes/')
-  }
+	launchSite = () => {
+		openUrl('https://www.stolaf.edu/apps/tes/')
+	}
 
-  render() {
-    return (<View></View>)
-  }
-
+	render() {
+		return (
+			<View style={styles.container}>
+				<NoticeView
+					buttonText="Open TES"
+					header="Time Entry System"
+					onPress={this.launchSite}
+					text="The St. Olaf Time Entry System (TES) is the place to report your work hours, for both students and hourly staff."
+				/>
+			</View>
+		)
+	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		flexGrow: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		backgroundColor: c.white,
+		padding: 20,
+	},
+})
