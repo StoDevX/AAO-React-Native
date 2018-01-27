f10f450fac9afe444848a6ae58e1d4519fdb7b1f
Hawken Rives <hawkrives@gmail.com>
2018-01-26 19:48:10 -0600

    prettify

--- source/views/help/wifi.js
+++ source/views/help/wifi.js
@@ -38,7 +38,9 @@ export class ToolView extends React.Component<Props, State> {
 	}
 
 	start = async () => {
-		let reportUrl = 'https://www.stolaf.edu/apps/all-about-olaf/wifi/index.cfm?fuseaction=Submit'
+		let reportUrl =
+			'https://www.stolaf.edu/apps/all-about-olaf/wifi/index.cfm?fuseaction=Submit'
+ 
 
 		if (this.props.config.buttons && this.props.config.buttons.length >= 1) {
 			const btnConfig = this.props.config.buttons[0]
