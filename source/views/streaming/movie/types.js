e090a64d4669d67f9a564143f0b4a4b8918eb867
Drew Volz <volz.drew@gmail.com>
2018-01-28 11:01:27 -0700

    Prettier, remove temp file

--- source/views/streaming/movie/types.js
+++ source/views/streaming/movie/types.js
@@ -3,44 +3,44 @@
 export type RGBTuple = [number, number, number]
 
 export type PosterInfo = {
-  url: string,
-  filename: string,
-  width: number,
-  height: number,
+	url: string,
+	filename: string,
+	width: number,
+	height: number,
 }
 
 export type MovieShowing = {time: string, location: string}
 
 export type MovieInfo = {
-  Title: string,
-  Year: string,
-  Rated: string,
-  Released: string,
-  Runtime: string,
-  Genre: string,
-  Director: string,
-  Writer: string,
-  Actors: string,
-  Plot: string,
-  Language: string,
-  Country: string,
-  Awards: string,
-  Ratings: Array<{Source: string, Value: string}>,
-  Type: string,
-  DVD: string,
-  BoxOffice: string,
-  Production: string,
-  imdbID: string,
-  Website: string,
+	Title: string,
+	Year: string,
+	Rated: string,
+	Released: string,
+	Runtime: string,
+	Genre: string,
+	Director: string,
+	Writer: string,
+	Actors: string,
+	Plot: string,
+	Language: string,
+	Country: string,
+	Awards: string,
+	Ratings: Array<{Source: string, Value: string}>,
+	Type: string,
+	DVD: string,
+	BoxOffice: string,
+	Production: string,
+	imdbID: string,
+	Website: string,
 }
 
 export type Movie = {
-  root: string,
-  info: MovieInfo,
-  showings: Array<MovieShowing>,
-  posters: Array<PosterInfo>,
-  posterColors: {
-    dominant: RGBTuple,
-    palette: Array<RGBTuple>,
-  },
+	root: string,
+	info: MovieInfo,
+	showings: Array<MovieShowing>,
+	posters: Array<PosterInfo>,
+	posterColors: {
+		dominant: RGBTuple,
+		palette: Array<RGBTuple>,
+	},
 }
