11b0dff36f075d750614e2485d5a52de6b710ada
Johannes Carlsen <hannes.mcman@gmail.com>
2018-02-04 13:05:55 -0600

    list terms more accurately

--- source/lib/course-search/parse-term.js
+++ source/lib/course-search/parse-term.js
@@ -3,21 +3,22 @@
 export function parseTerm(term: string) {
 	const semester = parseInt(term.slice(-1, term.length))
 	const year = term.slice(0, term.length - 1)
+	const currentYear = parseInt(year)
 	switch (semester) {
 		case 0:
-			return 'Abroad ' + year
+			return `Abroad ${currentYear}/${currentYear + 1}`
 		case 1:
-			return 'Fall ' + year
+			return `Fall ${currentYear}/${currentYear + 1}`
 		case 2:
-			return 'Interim ' + year
+			return `Interim ${currentYear}/${currentYear + 1}`
 		case 3:
-			return 'Spring ' + year
+			return `Spring ${currentYear}/${currentYear + 1}`
 		case 4:
-			return 'Summer Session 1 ' + year
+			return `Summer Term 1 ${currentYear}/${currentYear + 1}`
 		case 5:
-			return 'Summer Session 2 ' + year
+			return `Summer Term 2 ${currentYear}/${currentYear + 1}`
 		case 9:
-			return 'Non-St. Olaf ' + year
+			return `Non-St. Olaf ${currentYear}/${currentYear + 1}`
 		default:
 			return 'No term found'
 	}
