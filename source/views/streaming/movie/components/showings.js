59491fe723beae8d83e6a3722c99a40d0ccf97e8
Hawken Rives <hawkrives@gmail.com>
2018-02-04 16:21:57 -0600

    give a background color to the dates on the showings

--- source/views/streaming/movie/components/showings.js
+++ source/views/streaming/movie/components/showings.js
@@ -26,13 +26,12 @@ export const Showings = ({showings}: {showings: ?Array<MovieShowing>}) => {
 	)
 }
 
+const PADDINGS = {left: 10, right: 10, top: 10, bottom: 8}
+
 const PaddedShowingsCard = ({children}) => (
 	<Card
 		marginHorizontal={10}
 		marginVertical={16}
-		paddingBottom={8}
-		paddingHorizontal={10}
-		paddingTop={10}
 	>
 		{children}
 	</Card>
@@ -60,12 +59,11 @@ const ShowingTile = ({item}: {item: GroupedShowing}) => {
 	return (
 		<PaddedShowingsCard>
 			<Row>
-				<Column alignItems="center">
+				<Column backgroundColor={c.sto.lightGold} borderTopLeftRadius={8} borderBottomLeftRadius={8} alignItems="center" paddingTop={PADDINGS.top} paddingLeft={PADDINGS.left} paddingBottom={PADDINGS.bottom} paddingRight={PADDINGS.left / 3 * 2}>
 					<BigText>{item.date}</BigText>
 					<SmallText>{item.month}</SmallText>
 				</Column>
-				<glamorous.View width={10} />
-				<Column>
+				<Column paddingTop={PADDINGS.top} paddingLeft={PADDINGS.left / 2} paddingBottom={PADDINGS.bottom} paddingRight={PADDINGS.right}>
 					<DimText lines={1}>{item.location}</DimText>
 					<SmallText>{item.times.join(' • ')}</SmallText>
 				</Column>
