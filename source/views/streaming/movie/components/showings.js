d30f854ae20c1e5712822733d08953087d8dc1b2
Hawken Rives <hawkrives@gmail.com>
2018-02-04 15:34:44 -0600

    fix up Showings tiles

--- source/views/streaming/movie/components/showings.js
+++ source/views/streaming/movie/components/showings.js
@@ -4,15 +4,9 @@ import * as React from 'react'
 import * as c from '../../../components/colors'
 import moment from 'moment-timezone'
 import glamorous from 'glamorous-native'
-import {Row} from '../../../components/layout'
+import {Row, Column} from '../../../components/layout'
 import type {MovieShowing} from '../types'
 import {Card} from './parts'
-
-const PaddedShowingsCard = ({children}) => (
-	<Card marginHorizontal={10} marginVertical={16} paddingHorizontal={10}>
-		{children}
-	</Card>
-)
 
 export const Showings = ({showings}: {showings: ?Array<MovieShowing>}) => {
 	if (!showings || !showings.length) {
@@ -22,6 +16,7 @@ export const Showings = ({showings}: {showings: ?Array<MovieShowing>}) => {
 	return (
 		<glamorous.ScrollView
 			horizontal={true}
+			overflow="hidden"
 			showsHorizontalScrollIndicator={false}
 		>
 			{showings.map(s => <ShowingTile key={s.time} item={s} />)}
@@ -29,36 +24,62 @@ export const Showings = ({showings}: {showings: ?Array<MovieShowing>}) => {
 	)
 }
 
-const ShowingsDay = glamorous.text({
+const PaddedShowingsCard = ({children}) => (
+	<Card
+		marginHorizontal={10}
+		marginVertical={16}
+		paddingBottom={8}
+		paddingHorizontal={10}
+		paddingTop={10}
+	>
+		{children}
+	</Card>
+)
+
+const BigText = glamorous.text({
 	color: c.black,
 	fontSize: 22,
+	lineHeight: 22,
 })
 
-const ShowingsLocation = glamorous.text({
+const DimText = glamorous.text({
+	color: c.iosDisabledText,
 	fontSize: 14,
+	lineHeight: 22,
 })
 
-const ShowingsTime = glamorous.text({
+const SmallText = glamorous.text({
 	color: c.black,
 	fontSize: 13,
+	fontVariant: ['small-caps'],
 })
 
-const ShowingTile = ({item}: {item: MovieShowing}) => (
-	<PaddedShowingsCard>
-		<Row alignItems="flex-start">
-			<Row alignItems="flex-end" marginBottom={3}>
-				<ShowingsDay lines={1}>{moment(item.time).format('D')}</ShowingsDay>
-				<ShowingsLocation lines={1}>
-					{item.location.toUpperCase()}
-				</ShowingsLocation>
+const ShowingTile = ({item}: {item: MovieShowing}) => {
+	const m = moment(item.time)
+
+	const date = m.format('D')
+	const month = m.format('MMM').toLowerCase()
+
+	const time =
+		m.minutes() === 0
+			? m.format('hA').toLowerCase()
+			: m.format('h:mmA').toLowerCase()
+
+	const location = item.location
+
+	return (
+		<PaddedShowingsCard>
+			<Row>
+				<Column alignItems="center">
+					<BigText>{date}</BigText>
+					<SmallText>{month}</SmallText>
+				</Column>
+				<glamorous.View width={10} />
+				<Column>
+					<DimText lines={1}>{location}</DimText>
+					<SmallText>{time}</SmallText>
+				</Column>
 			</Row>
-			<Row alignItems="flex-end">
-				<ShowingsTime lines={1}>
-					{moment(item.time)
-						.format('MMM h:mmA')
-						.toUpperCase()}
-				</ShowingsTime>
-			</Row>
-		</Row>
-	</PaddedShowingsCard>
-)
+		</PaddedShowingsCard>
+	)
+}
