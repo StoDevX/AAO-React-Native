f57f0e0b454002751b230cdf76b0a56e90537aff
Johannes Carlsen <hannes.mcman@gmail.com>
2018-02-04 22:36:28 -0600

    abbreviate course terms

--- source/lib/course-search/parse-term.js
+++ source/lib/course-search/parse-term.js
@@ -4,21 +4,23 @@ export function parseTerm(term: string) {
 	const semester = parseInt(term.slice(-1, term.length))
 	const year = term.slice(0, term.length - 1)
 	const currentYear = parseInt(year)
+	const nextYear = (currentYear + 1).toString()
+	const nextYearAbbrev = nextYear.slice(-2, nextYear.length)
 	switch (semester) {
 		case 0:
-			return `Abroad ${currentYear}/${currentYear + 1}`
+			return `Abroad ${currentYear}/${nextYearAbbrev}`
 		case 1:
-			return `Fall ${currentYear}/${currentYear + 1}`
+			return `Fall ${currentYear}/${nextYearAbbrev}`
 		case 2:
-			return `Interim ${currentYear}/${currentYear + 1}`
+			return `Interim ${currentYear}/${nextYearAbbrev}`
 		case 3:
-			return `Spring ${currentYear}/${currentYear + 1}`
+			return `Spring ${currentYear}/${nextYearAbbrev}`
 		case 4:
-			return `Summer Term 1 ${currentYear}/${currentYear + 1}`
+			return `Summer Term 1 ${currentYear}/${nextYearAbbrev}`
 		case 5:
-			return `Summer Term 2 ${currentYear}/${currentYear + 1}`
+			return `Summer Term 2 ${currentYear}/${nextYearAbbrev}`
 		case 9:
-			return `Non-St. Olaf ${currentYear}/${currentYear + 1}`
+			return `Non-St. Olaf ${currentYear}/${nextYearAbbrev}`
 		default:
 			return 'No term found'
 	}
