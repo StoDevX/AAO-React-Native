e5a6e17539b8f3913d383c23f408f15f5605d0d7
Hawken Rives <hawkrives@gmail.com>
2018-02-16 20:36:22 -0600

    sigh… remove ReactShallowTestRenderer from NoticeView tests

--- source/views/components/__tests__/notice.test.js
+++ source/views/components/__tests__/notice.test.js
@@ -3,27 +3,21 @@
 
 import 'react-native'
 import * as React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import ReactTestRenderer from 'react-test-renderer'
 
 import {NoticeView} from '../notice'
 
-const shallow = component => {
-	const r = new ReactShallowRenderer()
-	r.render(component)
-	return r.getRenderOutput()
-}
-
 test('renders the given text', () => {
-	const tree = shallow(<NoticeView text="A Label I Am" />)
+	const tree = ReactTestRenderer.create(<NoticeView text="A Label I Am" />)
 	expect(tree).toMatchSnapshot()
 })
 
 test('renders a button, if given', () => {
-	const tree = shallow(<NoticeView buttonText="Button" text="Label" />)
+	const tree = ReactTestRenderer.create(<NoticeView buttonText="Button" text="Label" />)
 	expect(tree).toMatchSnapshot()
 })
 
 test('shows an ActivityIndicator if given [spinner]', () => {
-	const tree = shallow(<NoticeView spinner={true} text="Label" />)
+	const tree = ReactTestRenderer.create(<NoticeView spinner={true} text="Label" />)
 	expect(tree).toMatchSnapshot()
 })
