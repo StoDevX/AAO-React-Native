865188890a77b8886000b258a5c63f7fda186735
Hawken Rives <rives@stolaf.edu>
2018-03-30 09:30:38 -0500

    remove need for maintaining current player state in Player
    resolves the last remaining bug in this branch, where you couldn't play
    the stream.

--- source/views/streaming/radio/player.js
+++ source/views/streaming/radio/player.js
@@ -18,10 +18,6 @@ type Props = {
 	streamSourceUrl: string,
 }
 
-type State = {
-	playState: PlayState,
-}
-
 type HtmlAudioState =
 	| 'waiting'
 	| 'ended'
@@ -35,16 +31,8 @@ type HtmlAudioEvent =
 	| {type: 'error', error: HtmlAudioError}
 
 export class StreamPlayer extends React.PureComponent<Props, State> {
-	static deriveStateFromProps(nextProps: Props) {
-		return {playState: nextProps.playState}
-	}
-
-	state = {
-		playState: this.props.playState,
-	}
-
 	componentDidUpdate() {
-		this.dispatchEvent(this.state.playState)
+		this.dispatchEvent(this.props.playState)
 	}
 
 	componentWillUnmount() {
