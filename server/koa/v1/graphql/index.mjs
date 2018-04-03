05a634f49c87f3d2fa4a7ff836524d53e7f06019
Hawken Rives <rives@stolaf.edu>
2018-04-03 18:50:57 -0500

    start expanding graphql schema

--- server/koa/v1/graphql/index.mjs
+++ server/koa/v1/graphql/index.mjs
@@ -16,7 +16,78 @@ const books = [
 // The GraphQL schema in string form
 const typeDefs = `
   type Query { books: [Book] }
+
+  type CorIcon {
+    id: ID!
+    label: string
+    sort: number
+  }
+
+  type MenuItem {
+    id: ID!
+    label: string
+    description: string
+    cor: [CorIcon]
+    price: string
+    sizes: [string]
+    nutrition: nutrition
+    special: boolean
+    tier: number
+    rating: string
+    connector: string
+    options: [Option]
+    station: Station
+    substation: SubStation
+    monotony: Monotony
+  }
+
+  type Option {}
+
+  type Monotony {}
+
+  type SubStation {
+    id: ID!
+    label: string
+    sort: string
+    items: [MenuItem]
+  }
+
+  type Station {
+    id: ID!
+    sort: string
+    label: string
+    price: string
+    note: string
+    items: [MenuItem]
+  }
+
+  type MenuPart {
+    id: ID!
+    start: string
+    end: string
+    label: string
+    abbreviation: string
+    stations: [Station]
+  }
+
+  type Menu {
+    id: ID!
+    name: string!
+    parts: [MenuPart]
+  }
+
+  type Cafe {
+    menu: Menu
+  }
+
   type Book { title: String, author: String }
+
+  type Mutation {}
+
+  schema {
+    query: Query
+    mutation: Mutation
+  }
 `;
 
 // The resolvers
--- server/koa/v1/index.mjs
+++ server/koa/v1/index.mjs
@@ -15,7 +15,6 @@ api.get('/food/cafe/:cafeId', cafe)
 // graphql
 api.post('/graphql', koaBody(), graphqlKoa({ schema }));
 api.get('/graphql', graphqlKoa({ schema }));
-
 api.get('/graphiql', graphiqlKoa({ endpointURL: '/api/v1/graphql' }));
 
 export {api as v1}
