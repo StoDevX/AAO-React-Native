f0fc09658531f9daaf9b78df23d7f4792a4fc27c
Hawken Rives <rives@stolaf.edu>
2018-04-03 14:47:17 -0500

    add caching for menus

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const { send } = require('micro')
 const { router, get } = require('microrouter')
+const cache = require('micro-cacheable')
 const got = require('got')
 
 const notfound = (req, res) => send(res, 404, {type: 'error', payload: 'Route not found'})
@@ -8,17 +9,15 @@ const menu = async (req, res) => {
   const bonappMenuBaseUrl = 'http://legacy.cafebonappetit.com/api/2/menus'
   const bonappCafeBaseUrl = 'http://legacy.cafebonappetit.com/api/2/cafes'
 
-  console.log('in thing')
-
   try {
     let resp = await got.get(bonappMenuBaseUrl, {json: true, query: {cafe: req.params.cafeId}})
-    return send(res, 200, resp.body)
+    return resp.body
   } catch (err) {
-    return send(res, 200, {error: true, message: err.message})
+    return {error: true, message: err.message}
   }
 }
 
 module.exports = router(
-  get('/menu/:cafeId', menu),
+  get('/menu/:cafeId', cache(24 * 60 * 60 * 1000, menu)),
   get('/*', notfound),
 )
