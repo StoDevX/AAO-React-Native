87efe4e6d5de5e2417640675f6739910cb5c7447
Hawken Rives <hawkrives@gmail.com>
2018-04-04 00:23:20 -0500

    update cafe endpoints for graphql

--- server/v1/graphql/index.mjs
+++ server/v1/graphql/index.mjs
@@ -1,4 +1,5 @@
 import graphqlTools from 'graphql-tools'
+import {getCafe, getCafeMenu, getCafeInfo} from '../menu'
 const { makeExecutableSchema } = graphqlTools
 
 // Some fake data
@@ -15,84 +16,174 @@ const books = [
 
 // The GraphQL schema in string form
 const typeDefs = `
-  type Query { books: [Book] }
+  scalar HtmlString
+  scalar TimeString24Hour
+  scalar DateString
+
+  type Query {
+    books: [Book]
+    cafe(id: Int): Cafe
+    cafes(ids: [Int]): [Cafe]
+  }
 
   type CorIcon {
     id: ID!
-    label: string
-    sort: number
+    label: String
+    sort: Int
   }
 
   type MenuItem {
     id: ID!
-    label: string
-    description: string
+    label: String
+    description: String
     cor: [CorIcon]
-    price: string
-    sizes: [string]
-    nutrition: nutrition
-    special: boolean
-    tier: number
-    rating: string
-    connector: string
-    options: [Option]
+    price: String
+    sizes: [String]
+    nutrition: Nutrition
+    special: Boolean
+    tier: Int
+    rating: String
+    connector: String
+    #options: [Option]
     station: Station
     substation: SubStation
-    monotony: Monotony
+    #monotony: Monotony
   }
 
-  type Option {}
+  type Nutrition {
+    kcal: Int
+  }
 
-  type Monotony {}
+  #type Option {}
+
+  #type Monotony {}
 
   type SubStation {
     id: ID!
-    label: string
-    sort: string
+    label: String
+    sort: String
     items: [MenuItem]
   }
 
   type Station {
     id: ID!
-    sort: string
-    label: string
-    price: string
-    note: string
+    sort: String
+    label: String
+    price: String
+    note: String
     items: [MenuItem]
   }
 
   type MenuPart {
     id: ID!
-    start: string
-    end: string
-    label: string
-    abbreviation: string
+    start: String
+    end: String
+    label: String
+    abbreviation: String
     stations: [Station]
   }
 
-  type Menu {
+  type Menu @cacheControl(maxAge: 3600) {
     id: ID!
-    name: string!
+    name: String!
     parts: [MenuPart]
   }
 
-  type Cafe {
+  """
+  A café
+  """
+  type Cafe @cacheControl(maxAge: 3600) {
     menu: Menu
+    name: String
+    address: String
+    city: String
+    state: String
+    zip: String
+    latitude: Float
+    longitude: Float
+    description: HtmlString
+    message: HtmlString
+    eod: TimeString24Hour
+    timezone: String
+    menuType: MenuTypeEnum
+    menuHtml: HtmlString
+    locationDetail: String
+    weeklySchedule: HtmlString
+    schedule: [CafeSchedule]
   }
 
-  type Book { title: String, author: String }
+  type CafeSchedule {
+    date: DateString
+    message: String
+    status: CafeScheduleStatusEnum
+    shifts: [CafeScheduleShift]
+  }
 
-  type Mutation {}
+  type CafeScheduleShift {
+    id: ID!
+    start: TimeString24Hour
+    end: TimeString24Hour
+    message: String
+    label: String
+    hide: Boolean
+  }
+
+  enum CafeScheduleStatusEnum {
+    open
+  }
+
+  enum MenuTypeEnum {
+    dynamic
+  }
+
+  type Book {
+    title: String
+    author: String
+  }
+
+  #type Mutation {}
 
   schema {
     query: Query
-    mutation: Mutation
+    #mutation: Mutation
   }
 `;
 
+const coerceCafeDays = (day) => ({
+  date: day.date,
+  status: day.status,
+  message: day.message,
+  shifts: day.dayparts.map(s => ({
+    id: s.id,
+    message: s.message,
+    label: s.label,
+    start: s.starttime,
+    end: s.endtime,
+    hide: s.hide === '1',
+  })),
+})
+
+const extractCafeInfo = (cafeId) => (data) => {
+  let [cafeMenu, cafeInfo] = data
+  let cafe = cafeInfo.body.cafes[cafeId]
+  let menu = cafeMenu.body.items
+  return Object.assign({}, cafe, {
+    menuType: cafe.menu_type,
+    menuHtml: cafe.menu_html,
+    locationDetail: cafe.location_detail,
+    weeklySchedule: cafe.weekly_schedule,
+    schedule: cafe.days.map(coerceCafeDays),
+    menu: menu,
+  })
+}
+
 // The resolvers
 const resolvers = {
-  Query: { books: () => books },
+  Query: {
+    books: () => books,
+    cafe: (root, args) => getCafe(args.id).then(extractCafeInfo(args.id)),
+    cafes: (root, args) => Promise.all(args.ids.map(id => getCafe(id).then(extractCafeInfo(id)))),
+  },
 };
 
 // Put together a schema
--- server/v1/menu/index.mjs
+++ server/v1/menu/index.mjs
@@ -7,8 +7,9 @@ const GET = mem(got.get, {maxAge: ONE_DAY})
 const menuBase = 'http://legacy.cafebonappetit.com/api/2/menus'
 const cafeBase = 'http://legacy.cafebonappetit.com/api/2/cafes'
 
-const getCafeMenu = (cafeId) => GET(menuBase, {json: true, query: {cafe: cafeId}})
-const getCafeInfo = (cafeId) => GET(menuBase, {json: true, query: {cafe: cafeId}})
+export const getCafeMenu = (cafeId) => GET(menuBase, {json: true, query: {cafe: cafeId}})
+export const getCafeInfo = (cafeId) => GET(cafeBase, {json: true, query: {cafe: cafeId}})
+export const getCafe = (cafeId) => Promise.all([getCafeMenu(cafeId), getCafeInfo(cafeId)])
 
 export async function menu(ctx) {
 	let {cafeId} = ctx.params
