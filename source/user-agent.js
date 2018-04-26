83c030b399e39ad7b8f4a46d9fff0d6666348d0f
Drew Volz <volz.drew@gmail.com>
2018-04-25 21:41:27 -0600

    prettier changes

--- source/user-agent.js
+++ source/user-agent.js
@@ -7,7 +7,9 @@ const aaoVersion = version || 'unknown'
 const platformString =
 	Platform.OS === 'ios'
 		? 'iOS'
-		: Platform.OS === 'android' ? 'Android' : 'unknown'
+		: Platform.OS === 'android'
+			? 'Android'
+			: 'unknown'
 const platformVersion = Platform.Version || 'unknown'
 
 export const AAO_USER_AGENT = `AllAboutOlaf/${aaoVersion} (${platformString}/${platformVersion})`
