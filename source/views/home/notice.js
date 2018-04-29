fb0e6f0fc6460124506512790e2cad705d6c496e
Drew Volz <volz.drew@gmail.com>
2018-04-28 18:17:17 -0600

    use constants for margins

--- source/views/home/notice.js
+++ source/views/home/notice.js
@@ -4,6 +4,7 @@ import * as React from 'react'
 import * as glamorous from 'glamorous-native'
 import * as c from '../components/colors'
 import sample from 'lodash/sample'
+import {CELL_MARGIN} from './button'
 
 const messages = [
 	'☃️ An Unofficial App Project ☃️',
@@ -19,8 +20,8 @@ export function UnofficialAppNotice() {
 	return (
 		<glamorous.View
 			justifyContent="center"
-			marginBottom={10}
-			marginHorizontal={10}
+			marginBottom={CELL_MARGIN}
+			marginHorizontal={CELL_MARGIN}
 		>
 			<glamorous.View backgroundColor="rgba(0,0,0,0.05)" borderRadius={7}>
 				<glamorous.Text color={c.black25Percent} padding={8} textAlign="center">
