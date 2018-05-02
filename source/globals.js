e341fbe37e2e5993a2c2bd5074e216151e68daee
Hawken Rives <hawkrives@gmail.com>
2018-05-01 23:06:08 -0500

    prettify globals

--- source/globals.js
+++ source/globals.js
@@ -3,13 +3,13 @@
 import qs from 'querystring'
 
 const root = 'https://stolaf.api.frogpond.tech/v1'
-export const API = (pth: string, query: ?Object=null) => {
+export const API = (pth: string, query: ?Object = null) => {
 	if (process.env.NODE_ENV !== 'production') {
 		if (!pth.startsWith('/')) {
 			throw new Error('invalid path requested from the api!')
 		}
 	}
-	let url = root + pth 
+	let url = root + pth
 	if (query) {
 		url += `?${qs.stringify(query)}`
 	}
