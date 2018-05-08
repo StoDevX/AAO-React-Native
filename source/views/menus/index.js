d9950863fa56e8d05d1eaa3220fd52857253b9e2
Hawken Rives <rives@stolaf.edu>
2018-05-07 19:45:08 -0500

    update how titles are set for tabnavigators

--- source/views/menus/index.js
+++ source/views/menus/index.js
@@ -16,85 +16,81 @@ export {
 	CarletonSaylesMenuScreen,
 } from './carleton-menus'
 
-export const MenusView = TabNavigator(
-	{
-		StavHallMenuView: {
-			screen: ({navigation}) => (
-				<BonAppHostedMenu
-					cafeId="261"
-					loadingMessage={[
-						'Hunting Ferndale Turkey…',
-						'Tracking wild vegan burgers…',
-						'"Cooking" some lutefisk…',
-						'Finding more mugs…',
-						'Waiting for omelets…',
-						'Putting out more cookies…',
-					]}
-					name="Stav Hall"
-					navigation={navigation}
-				/>
-			),
-			navigationOptions: {
-				tabBarLabel: 'Stav Hall',
-				tabBarIcon: TabBarIcon('nutrition'),
-			},
-		},
-
-		TheCageMenuView: {
-			screen: ({navigation}) => (
-				<BonAppHostedMenu
-					cafeId="262"
-					ignoreProvidedMenus={true}
-					loadingMessage={[
-						'Checking for vegan cookies…',
-						'Serving up some shakes…',
-						'Waiting for menu screens to change…',
-						'Frying chicken…',
-						'Brewing coffee…',
-					]}
-					name="The Cage"
-					navigation={navigation}
-				/>
-			),
-			navigationOptions: {
-				tabBarLabel: 'The Cage',
-				tabBarIcon: TabBarIcon('cafe'),
-			},
-		},
-
-		ThePauseMenuView: {
-			screen: ({navigation}) => (
-				<GitHubHostedMenu
-					loadingMessage={[
-						'Mixing up a shake…',
-						'Spinning up pizzas…',
-						'Turning up the music…',
-						'Putting ice cream on the cookies…',
-						'Fixing the oven…',
-					]}
-					name="The Pause"
-					navigation={navigation}
-				/>
-			),
-			navigationOptions: {
-				tabBarLabel: 'The Pause',
-				tabBarIcon: TabBarIcon('paw'),
-			},
-		},
-
-		CarletonMenuListView: {
-			screen: CarletonCafeIndex,
-			navigationOptions: {
-				tabBarLabel: 'Carleton',
-				tabBarIcon: TabBarIcon('menu'),
-			},
-		},
-
-		// BonAppDevToolView: {screen: BonAppPickerView},
-	},
-	{
+export const MenusView = TabNavigator({
+	StavHallMenuView: {
+		screen: ({navigation}) => (
+			<BonAppHostedMenu
+				cafeId="261"
+				loadingMessage={[
+					'Hunting Ferndale Turkey…',
+					'Tracking wild vegan burgers…',
+					'"Cooking" some lutefisk…',
+					'Finding more mugs…',
+					'Waiting for omelets…',
+					'Putting out more cookies…',
+				]}
+				name="Stav Hall"
+				navigation={navigation}
+			/>
+		),
 		navigationOptions: {
-			title: 'Menus',
+			tabBarLabel: 'Stav Hall',
+			tabBarIcon: TabBarIcon('nutrition'),
 		},
 	},
-)
+
+	TheCageMenuView: {
+		screen: ({navigation}) => (
+			<BonAppHostedMenu
+				cafeId="262"
+				ignoreProvidedMenus={true}
+				loadingMessage={[
+					'Checking for vegan cookies…',
+					'Serving up some shakes…',
+					'Waiting for menu screens to change…',
+					'Frying chicken…',
+					'Brewing coffee…',
+				]}
+				name="The Cage"
+				navigation={navigation}
+			/>
+		),
+		navigationOptions: {
+			tabBarLabel: 'The Cage',
+			tabBarIcon: TabBarIcon('cafe'),
+		},
+	},
+
+	ThePauseMenuView: {
+		screen: ({navigation}) => (
+			<GitHubHostedMenu
+				loadingMessage={[
+					'Mixing up a shake…',
+					'Spinning up pizzas…',
+					'Turning up the music…',
+					'Putting ice cream on the cookies…',
+					'Fixing the oven…',
+				]}
+				name="The Pause"
+				navigation={navigation}
+			/>
+		),
+		navigationOptions: {
+			tabBarLabel: 'The Pause',
+			tabBarIcon: TabBarIcon('paw'),
+		},
+	},
+
+	CarletonMenuListView: {
+		screen: CarletonCafeIndex,
+		navigationOptions: {
+			tabBarLabel: 'Carleton',
+			tabBarIcon: TabBarIcon('menu'),
+		},
+	},
+
+	// BonAppDevToolView: {screen: BonAppPickerView},
+})
+MenusView.navigationOptions = {
+	title: 'Menus',
+}
--- source/views/sis/index.js
+++ source/views/sis/index.js
@@ -7,16 +7,15 @@ import StudentWorkView from './student-work'
 import CourseSearchView from './course-search'
 import TESView from './tes'
 
-export default TabNavigator(
-	{
-		BalancesView: {screen: BalancesView},
-		CourseSearchView: {screen: CourseSearchView},
-		TESView: {screen: TESView},
-		StudentWorkView: {screen: StudentWorkView},
-	},
-	{
-		navigationOptions: {
-			title: 'SIS',
-		},
-	},
-)
+const SisView = TabNavigator({
+	BalancesView: {screen: BalancesView},
+	CourseSearchView: {screen: CourseSearchView},
+	TESView: {screen: TESView},
+	StudentWorkView: {screen: StudentWorkView},
+})
+
+SisView.navigationOptions = {
+	title: 'SIS',
+}
+
+export default SisView
