d4d96692af5123449f1b4e098b69e440a9743c30
Hawken Rives <rives@stolaf.edu>
2018-06-12 21:27:16 -0500

    ignore "ordinary" yellowbox warnings

--- source/root.js
+++ source/root.js
@@ -1,10 +1,17 @@
 // @flow
 
-import {AppRegistry} from 'react-native'
+import {AppRegistry, YellowBox} from 'react-native'
 import App from './app'
 
 // I'm not importing the exported variable because I just want to initialize
 // the file here.
 import './bugsnag'
+
+YellowBox.ignoreWarnings([
+	// TODO: remove me after upgrading to RN 0.56
+	'Warning: isMounted(...) is deprecated',
+	// TODO: remove me after upgrading to RN 0.56
+	'Module RCTImageLoader',
+])
 
 AppRegistry.registerComponent('AllAboutOlaf', () => App)
