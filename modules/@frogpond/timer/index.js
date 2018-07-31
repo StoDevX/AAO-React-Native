d5a9f55c5f539dbdc1f83169ee45a34561741818
Hawken Rives <hawkrives@fastmail.fm>
2018-07-30 22:44:59 -0500

    update <Timer/> to delay until the next $interval before starting the repetition

--- modules/@frogpond/timer/index.js
+++ modules/@frogpond/timer/index.js
@@ -13,16 +13,28 @@ type State = {
 
 export class Timer extends React.Component<Props, State> {
 	_intervalId: ?IntervalID
+	_timeoutId: ?TimeoutID
 
 	state = {
 		now: new Date(),
 	}
 
 	componentDidMount() {
-		this._intervalId = setInterval(this.updateTime, this.props.interval)
+		// get the time remaining until the next $interval
+		let {interval} = this.props
+		let nowMs = this.state.now.getTime()
+		let untilNextInterval = interval - (nowMs % interval)
+
+		this._timeoutId = setTimeout(() => {
+			this.updateTime()
+			this._intervalId = setInterval(this.updateTime, this.props.interval)
+		}, untilNextInterval)
 	}
 
 	componentWillUnmount() {
+		if (this._timeoutId) {
+			clearTimeout(this._timeoutId)
+		}
 		if (this._intervalId) {
 			clearInterval(this._intervalId)
 		}
