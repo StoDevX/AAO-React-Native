a91c1214bf229e49d78f1fccc9849634419c2261
Hawken Rives <hawkrives@fastmail.fm>
2018-08-03 19:52:58 -0500

    CourseSearch: move filters into state & pass off searching to CourseList

--- source/views/sis/course-search/search.js
+++ source/views/sis/course-search/search.js
@@ -7,7 +7,6 @@ import * as c from '../../components/colors'
 import {
 	updateCourseData,
 	loadCourseDataIntoMemory,
-	updateCourseFilters,
 	updateRecentSearches,
 	updateRecentFilters,
 } from '../../../flux/parts/courses'
@@ -15,13 +14,8 @@ import {type CourseType, areAnyTermsCached} from '../../../lib/course-search'
 import type {ReduxState} from '../../../flux'
 import type {TopLevelViewPropsType} from '../../types'
 import {connect} from 'react-redux'
-import groupBy from 'lodash/groupBy'
-import sortBy from 'lodash/sortBy'
-import toPairs from 'lodash/toPairs'
-import debounce from 'lodash/debounce'
 import {CourseSearchResultsList} from './list'
 import LoadingView from '../../components/loading'
-import {deptNum} from './lib/format-dept-num'
 import {NoticeView} from '../../components/notice'
 import {AnimatedSearchbox} from '../components/animated-searchbox'
 import {applyFiltersToItem, type FilterType} from '../../components/filter'
@@ -29,7 +23,6 @@ import {RecentItemsList} from '../components/recents-list'
 import {Separator} from '../../components/separator'
 import {buildFilters} from './lib/build-filters'
 import type {FilterComboType} from './lib/format-filter-combo'
-import keywordSearch from 'keyword-search'
 
 const PROMPT_TEXT =
 	'We need to download the courses from the server. This will take a few seconds.'
@@ -41,7 +34,6 @@ type ReactProps = TopLevelViewPropsType
 type ReduxStateProps = {
 	allCourses: Array<CourseType>,
 	courseDataState: string,
-	filters: Array<FilterType>,
 	isConnected: boolean,
 	recentFilters: FilterComboType[],
 	recentSearches: string[],
@@ -50,7 +42,6 @@ type ReduxStateProps = {
 type ReduxDispatchProps = {
 	updateCourseData: () => Promise<any>,
 	loadCourseDataIntoMemory: () => Promise<any>,
-	onFiltersChange: (filters: FilterType[]) => any,
 	updateRecentFilters: (filters: FilterType[]) => any,
 	updateRecentSearches: (query: string) => any,
 }
@@ -62,83 +53,10 @@ type DefaultProps = {
 type Props = ReactProps & ReduxStateProps & ReduxDispatchProps & DefaultProps
 
 type State = {
-	cachedFilters: Array<FilterType>,
-	searchResults: Array<{title: string, data: Array<CourseType>}>,
-	searchPerformed: boolean,
+	filters: Array<FilterType>,
 	query: string,
 	mode: 'loading' | 'browsing' | 'searching' | 'ready',
 };
-
-function executeSearch(args: {
-	text: string,
-	filters: FilterType[],
-	applyFilters: (filters: FilterType[], item: CourseType) => boolean,
-	allCourses: Array<CourseType>,
-	updateRecentSearches: (query: string) => any,
-}) {
-	const {text, filters, applyFilters, allCourses, updateRecentSearches} = args
-	const query = text.toLowerCase()
-
-	const filteredCourses = allCourses.filter(course =>
-		applyFilters(filters, course),
-	)
-
-	const results = filteredCourses.filter(
-		course =>
-			keywordSearch(query, course.name.toLowerCase(), 1) ||
-			keywordSearch(query, (course.title || '').toLowerCase(), 1) ||
-			(course.instructors || []).some(name =>
-				keywordSearch(query, name.toLowerCase(), 1),
-			) ||
-			deptNum(course)
-				.toLowerCase()
-				.startsWith(query) ||
-			(course.gereqs || []).some(gereq =>
-				gereq.toLowerCase().startsWith(query),
-			),
-	)
-	const sortedResults = sortBy(results, course => deptNum(course))
-	const grouped = groupBy(sortedResults, r => r.term)
-	const groupedCourses = toPairs(grouped).map(([key, value]) => ({
-		title: key,
-		data: value,
-	}))
-
-	const sortedTerms = sortBy(groupedCourses, course => course.title).reverse()
-
-	if (text.length !== 0) {
-		updateRecentSearches(text)
-	}
-
-	return {
-		searchResults: sortedTerms,
-		searchPerformed: true,
-		query: text,
-	}
-}
-
-function applyFiltersAndQuery(args: {
-	filters: FilterType[],
-	applyFilters: (filters: FilterType[], item: CourseType) => boolean,
-	allCourses: Array<CourseType>,
-}) {
-	const {filters, applyFilters, allCourses} = args
-	const filteredCourses = allCourses.filter(course =>
-		applyFilters(filters, course),
-	)
-	const sortedCourses = sortBy(filteredCourses, course => deptNum(course))
-	const grouped = groupBy(sortedCourses, r => r.term)
-	const groupedCourses = toPairs(grouped).map(([key, value]) => ({
-		title: key,
-		data: value,
-	}))
-
-	const sortedTerms = sortBy(groupedCourses, course => course.title).reverse()
-
-	return {
-		searchResults: sortedTerms,
-	}
-}
 
 class CourseSearchView extends React.Component<Props, State> {
 	static navigationOptions = {
@@ -153,41 +71,13 @@ class CourseSearchView extends React.Component<Props, State> {
 
 	state = {
 		mode: 'loading',
-		cachedFilters: this.props.filters,
-		searchResults: [],
-		searchPerformed: false,
+		filters: [],
 		query: '',
-	}
-
-	static getDerivedStateFromProps(nextProps: Props, prevState: State) {
-		if (prevState.browsing) {
-			return applyFiltersAndQuery({
-				filters: nextProps.filters,
-				applyFilters: nextProps.applyFilters,
-				allCourses: nextProps.allCourses,
-			})
-		}
-
-		if (!prevState.query) {
-			return null
-		}
-
-		if (nextProps.filters === prevState.cachedFilters) {
-			return null
-		}
-
-		return executeSearch({
-			text: prevState.query,
-			filters: nextProps.filters,
-			applyFilters: nextProps.applyFilters,
-			allCourses: nextProps.allCourses,
-			updateRecentSearches: nextProps.updateRecentSearches,
-		})
 	}
 
 	componentDidMount() {
 		this.loadData()
-		this.updateFilters(this.props)
+		this.resetFilters()
 	}
 
 	loadData = async () => {
@@ -216,16 +106,15 @@ class CourseSearchView extends React.Component<Props, State> {
 
 	handleSearchSubmit = () => {
 		this.setState(() => ({mode: 'searching'}))
-		this.performSearch(this.state.query)
+
+		let {query} = this.state
+		if (query.length !== 0) {
+			this.props.updateRecentSearches(query)
+		}
 	}
 
 	handleSearchCancel = () => {
-		this.setState(() => ({
-			query: '',
-			searchResults: [],
-			searchPerformed: false,
-			mode: 'ready',
-		}))
+		this.setState(() => ({query: '', mode: 'ready'}))
 		this.resetFilters()
 	}
 
@@ -237,34 +126,8 @@ class CourseSearchView extends React.Component<Props, State> {
 		this.setState(() => ({mode: 'searching'}))
 	}
 
-	_performSearch = (query: string) => {
-		const results = executeSearch({
-			text: query,
-			filters: this.props.filters,
-			applyFilters: this.props.applyFilters,
-			allCourses: this.props.allCourses,
-			updateRecentSearches: this.props.updateRecentSearches,
-		})
-
-		this.setState(() => results)
-	}
-
-	performSearch = debounce(this._performSearch, 20)
-
-	browseAll = () => {
-		this.setState(() =>
-			applyFiltersAndQuery({
-				filters: this.props.filters,
-				applyFilters: this.props.applyFilters,
-				allCourses: this.props.allCourses,
-			}),
-		)
-	}
-
 	onRecentSearchPress = (text: string) => {
 		this.setState(() => ({query: text, mode: 'searching'}))
-
-		this.performSearch(text)
 	}
 
 	onRecentFilterPress = async (text: string) => {
@@ -280,17 +143,15 @@ class CourseSearchView extends React.Component<Props, State> {
 			  )
 			: resetFilters
 
-		this.props.onFiltersChange(selectedFilters)
 
-		this.browseAll()
+		this.setState(() => ({filters: selectedFilters}))
 	}
 
 	openFilterView = () => {
 		this.props.navigation.navigate('FilterView', {
 			title: 'Add Filters',
-			pathToFilters: ['courses', 'filters'],
-			onChange: filters => this.props.onFiltersChange(filters),
-			onLeave: filters => this.props.updateRecentFilters(filters),
+			filters: this.state.filters,
+			onDismiss: filters => this.setState(() => ({filters})),
 		})
 	}
 
@@ -301,27 +162,11 @@ class CourseSearchView extends React.Component<Props, State> {
 
 	resetFilters = async () => {
 		const newFilters = await buildFilters()
-		this.props.onFiltersChange(newFilters)
-	}
-
-	updateFilters = (props: Props) => {
-		const {filters} = props
-
-		// prevent ourselves from overwriting the filters from redux on mount
-		if (filters.length) {
-			return
-		}
-
-		this.resetFilters()
+		this.setState(() => ({filters: newFilters}))
 	}
 
 	render() {
-		const {
-			searchPerformed,
-			searchResults,
-			query,
-			mode,
-		} = this.state
+		let {query, mode} = this.state
 
 		if (mode === 'loading') {
 			return <LoadingView text="Loading Course Data…" />
@@ -366,12 +211,13 @@ class CourseSearchView extends React.Component<Props, State> {
 				<Separator />
 				{mode !== 'ready' ? (
 					<CourseSearchResultsList
+						applyFilters={this.props.applyFilters}
 						browsing={mode === 'browsing'}
-						filters={this.props.filters}
+						courses={this.props.allCourses}
+						filters={this.state.filters}
 						navigation={this.props.navigation}
-						onFiltersChange={this.props.onFiltersChange}
-						searchPerformed={searchPerformed}
-						terms={searchResults}
+						openFilterView={this.openFilterView}
+						query={this.state.query}
 						updateRecentFilters={this.props.updateRecentFilters}
 					/>
 				) : (
@@ -404,7 +250,6 @@ function mapState(state: ReduxState): ReduxStateProps {
 		isConnected: state.app ? state.app.isConnected : false,
 		allCourses: state.courses ? state.courses.allCourses : [],
 		courseDataState: state.courses ? state.courses.readyState : '',
-		filters: state.courses ? state.courses.filters : [],
 		recentFilters: state.courses ? state.courses.recentFilters : [],
 		recentSearches: state.courses ? state.courses.recentSearches : [],
 	}
@@ -413,8 +258,6 @@ function mapState(state: ReduxState): ReduxStateProps {
 function mapDispatch(dispatch): ReduxDispatchProps {
 	return {
 		loadCourseDataIntoMemory: () => dispatch(loadCourseDataIntoMemory()),
-		onFiltersChange: (filters: FilterType[]) =>
-			dispatch(updateCourseFilters(filters)),
 		updateCourseData: () => dispatch(updateCourseData()),
 		updateRecentSearches: (query: string) =>
 			dispatch(updateRecentSearches(query)),
