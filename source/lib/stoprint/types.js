5f6cff0dc8142b5bac0abf98317fbbd50cd9373f
Johannes Carlsen <hannes.mcman@gmail.com>
2018-08-19 00:34:37 -0500

    move stoprint types to lib

--- source/lib/stoprint/types.js
+++ source/lib/stoprint/types.js
@@ -10,7 +10,7 @@ export type PrintJob = {
 	printerName: string,
 	serverName: string,
 	status: string | 'DENIED',
-	statusDetail?: string,
+	statusDetail: string,
 	statusFormatted: string,
 	totalPages: number,
 	usageCostFormatted: string,
@@ -20,7 +20,18 @@ export type PrintJob = {
 export type HeldJob = {
 	canRelease: boolean,
 	client: string,
-} & PrintJob
+	isDenied: boolean,
+	copies: number,
+	documentName: string,
+	deniedReasonFormatted?: string,
+	grayscaleFormatted: 'Yes' | 'No',
+	id: string,
+	paperSizeFormatted: string,
+	printerName: string,
+	serverName: string,
+	usageCostFormatted: string,
+	usageTimeFormatted: string,
+}
 
 // https://papercut.stolaf.edu:9192/rpc/api/rest/internal/webclient/users/rives/jobs/status
 export type StatusResponse = {
@@ -42,6 +53,19 @@ export type RecentPopularPrintersResponse = {
 	recentPrinters: Array<Printer>,
 }
 
+export type ReleaseResponse = {
+	numJobsReleased: number,
+	statusMessage: string,
+}
+
+export type ReleaseResponseOrErrorType =
+	| {error: true, value: Error}
+	| {error: false, value: ReleaseResponse}
+
+export type CancelResponseOrErrorType =
+	| {error: true, value: Error}
+	| {error: false, value: Response}
+
 // https://papercut.stolaf.edu:9192/rpc/api/rest/internal/mobilerelease/api/all-printers
 // ?username=rives
 export type AllPrintersResponse = Array<Printer>
@@ -50,3 +74,6 @@ export type AllPrintersResponse = Array<Printer>
 // ?username=rives
 // &printerName=printers\mfc-it
 export type HeldJobsResponse = Array<HeldJob>
+export type HeldJobsResponseOrErrorType =
+	| {error: true, value: Error}
+	| {error: false, value: HeldJobsResponse}
