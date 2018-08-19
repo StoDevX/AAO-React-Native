aed2a89f80c6f40387a7c10d84bb92e8d93036ee
Johannes Carlsen <hannes.mcman@gmail.com>
2018-08-19 00:31:11 -0500

    add stoprint exports

--- source/lib/stoprint/index.js
+++ source/lib/stoprint/index.js
@@ -1,12 +1,27 @@
 // @flow
 
-export {PAPERCUT, PAPERCUT_API, PAPERCUT_MOBILE_RELEASE_API} from './urls'
+export {
+	PAPERCUT,
+	PAPERCUT_API,
+	PAPERCUT_MOBILE_RELEASE_API,
+	STOPRINT_HELP_PAGE,
+} from './urls'
 export {
 	cancelPrintJobForUser,
 	fetchAllPrinters,
-  fetchJobs,
+	fetchJobs,
 	fetchRecentPrinters,
 	heldJobsAvailableAtPrinterForUser,
 	logIn,
 	releasePrintJobToPrinterForUser,
 } from './api'
+export type {
+	HeldJob,
+	Printer,
+	PrintJob,
+	RecentPopularPrintersResponse,
+	ReleaseResponseOrErrorType,
+	CancelResponseOrErrorType,
+	HeldJobsResponseOrErrorType,
+} from './types'
+export {showGeneralError} from './errors'
