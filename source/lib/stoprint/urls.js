fe619b2f8ed5256a2380bc0bcb5c7abd4302712a
Johannes Carlsen <hannes.mcman@gmail.com>
2018-08-19 00:31:22 -0500

    add stoprint help page url

--- source/lib/stoprint/urls.js
+++ source/lib/stoprint/urls.js
@@ -3,3 +3,5 @@
 export const PAPERCUT = 'https://papercut.stolaf.edu'
 export const PAPERCUT_API = 'https://papercut.stolaf.edu/rpc/api/rest/internal'
 export const PAPERCUT_MOBILE_RELEASE_API = `${PAPERCUT_API}/mobilerelease/api`
+
+export const STOPRINT_HELP_PAGE = 'https://wp.stolaf.edu/it/stoprint/'
