0efce4ba69017a98b4841980befd07b38bef2f25
Hawken Rives <hawkrives@fastmail.fm>
2018-09-02 15:13:24 -0500

    rework connect()ed components in OddsAndEnds screen

--- source/views/settings/screens/overview/odds-and-ends.js
+++ source/views/settings/screens/overview/odds-and-ends.js
@@ -3,13 +3,11 @@ import * as React from 'react'
 import {Cell, Section, CellToggle} from '@frogpond/tableview'
 import {APP_VERSION} from '../../../../lib/constants'
 import {setFeedbackStatus} from '../../../../redux/parts/settings'
+import type {ReduxState} from '../../../../redux'
 import {connect} from 'react-redux'
 import {sectionBgColor} from '@frogpond/colors'
 
-type Props = {
-	onChangeFeedbackToggle: (feedbackDisabled: boolean) => any,
-	feedbackDisabled: boolean,
-}
+type Props = {}
 
 export class OddsAndEndsSection extends React.Component<Props> {
 	render() {
@@ -22,31 +20,35 @@ export class OddsAndEndsSection extends React.Component<Props> {
 					<Cell cellStyle="RightDetail" detail={build} title="Build Number" />
 				)}
 
-				<CellToggle
-					label="Share Analytics"
-					// These are both inverted because the toggle makes more sense as
-					// optout/optin, but the code works better as optin/optout.
-					onChange={val => this.props.onChangeFeedbackToggle(!val)}
-					value={!this.props.feedbackDisabled}
-				/>
+				<ConnectedAnalyticsCell />
 			</Section>
 		)
 	}
 }
 
-function mapStateToProps(state) {
-	return {
-		feedbackDisabled: state.settings.feedbackDisabled,
-	}
+type AnalyticsCellProps = {
+	feedbackDisabled: boolean,
+	onChangeFeedbackToggle: boolean => any,
 }
 
-function mapDispatchToProps(dispatch) {
-	return {
-		onChangeFeedbackToggle: s => dispatch(setFeedbackStatus(s)),
-	}
+function AnalyticsCell(props: AnalyticsCellProps) {
+	let {feedbackDisabled, onChangeFeedbackToggle} = props
+	return (
+		<CellToggle
+			label="Share Analytics"
+			// These are both inverted because the toggle makes more sense as
+			// optout/optin, but the code works better as optin/optout.
+			onChange={val => onChangeFeedbackToggle(!val)}
+			value={!feedbackDisabled}
+		/>
+	)
 }
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps,
-)(OddsAndEndsSection)
+const ConnectedAnalyticsCell = connect(
+	(state: ReduxState) => ({
+		feedbackDisabled: state.settings ? state.settings.feedbackDisabled : false,
+	}),
+	dispatch => ({
+		onChangeFeedbackToggle: (s: boolean) => dispatch(setFeedbackStatus(s)),
+	}),
+)(AnalyticsCell)
