0f74e4d17717c1c7d347f55905bd17100a87a12b
Hawken Rives <hawkrives@fastmail.fm>
2018-09-08 13:12:44 -0500

    move around a bunch of code

---
+++ source/views/settings/screens/push-notifications/index.js
@@ -0,0 +1,114 @@
+// @flow
+
+import * as React from 'react'
+import {Platform} from 'react-native'
+import {ScrollView} from 'glamorous-native'
+import {TableView, Section, CellToggle} from '@frogpond/tableview'
+import {connect} from 'react-redux'
+import groupBy from 'lodash/groupBy'
+import toPairs from 'lodash/toPairs'
+import flatten from 'lodash/flatten'
+import type {ReduxState} from '../../../../redux'
+import {prompt, toggleSubscription} from '../../../../redux/parts/notifications'
+import {CheckForPushSettingsOnResume} from './on-resume'
+import {IosNotificationSettingsButton} from './ios-settings-button'
+import {
+	notificationTypes,
+	type NotificationChannel,
+	type NotificationChannelName,
+} from './channels'
+
+type ReduxStateProps = {|
+	+channels: Set<NotificationChannelName>,
+	+enabled: boolean,
+	+hasPrompted: boolean,
+|}
+
+type ReduxDispatchProps = {|
+	+onChangeEnabledToggle: () => any,
+	+onToggleChannel: (channelName: NotificationChannelName) => any,
+|}
+
+type Props = ReduxStateProps & ReduxDispatchProps
+
+class PushNotificationsSettingsView extends React.Component<Props> {
+	static navigationOptions = {
+		title: 'Notifications',
+	}
+
+	render() {
+		let {
+			enabled,
+			hasPrompted,
+			onChangeEnabledToggle,
+			onToggleChannel,
+			channels,
+		} = this.props
+
+		return (
+			<ScrollView contentInsetAdjustmentBehavior="automatic">
+				<CheckForPushSettingsOnResume />
+
+				<TableView>
+					{Platform.OS === 'ios' && (
+						<IosNotificationSettingsButton
+							hasPrompted={hasPrompted}
+							onEnable={onChangeEnabledToggle}
+							pushesEnabled={enabled}
+						/>
+					)}
+
+					<Section header="CHANNELS">
+						<ChannelsList
+							channels={channels}
+							onToggleChannel={onToggleChannel}
+							pushesEnabled={enabled}
+						/>
+					</Section>
+				</TableView>
+			</ScrollView>
+		)
+	}
+}
+
+function ChannelsList({pushesEnabled, onToggleChannel, channels}) {
+	let paired: Array<[string, Array<NotificationChannel>]> = toPairs(
+		groupBy(notificationTypes, c => c.group),
+	)
+
+	return flatten(
+		paired.map(([groupName, theseChannels]) =>
+			theseChannels.map(channel => {
+				// if the channel is forced, then we want to show the switch
+				// as being "on", unless notifications are disabled, in which
+				// case we can't send the forced notification, so we want to
+				// show it as being "off".
+				let forcedOn = channel.forced && pushesEnabled
+
+				return (
+					<CellToggle
+						key={channel.key}
+						detail={groupName}
+						disabled={pushesEnabled === false || channel.forced}
+						label={channel.label}
+						onChange={() => onToggleChannel(channel.key)}
+						value={forcedOn || channels.has(channel.key)}
+					/>
+				)
+			}),
+		),
+	)
+}
+
+export const ConnectedPushNotificationsSettingsView = connect(
+	(state: ReduxState): ReduxStateProps => ({
+		channels: state.notifications ? state.notifications.channels : new Set(),
+		enabled: state.notifications ? state.notifications.enabled : false,
+		hasPrompted: state.notifications ? state.notifications.hasPrompted : false,
+	}),
+	(dispatch): ReduxDispatchProps => ({
+		onChangeEnabledToggle: () => dispatch(prompt()),
+		onToggleChannel: (name: NotificationChannelName) =>
+			dispatch(toggleSubscription(name)),
+	}),
+)(PushNotificationsSettingsView)
