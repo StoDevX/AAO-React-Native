554cedbf4900bcdb8a861373e31f0949b140f944
Hawken Rives <hawkrives@fastmail.fm>
2018-09-08 14:06:27 -0500

    prettify

--- modules/tableview/index.js
+++ modules/tableview/index.js
@@ -4,11 +4,17 @@ import * as React from 'react'
 import {Platform} from 'react-native'
 import {androidLightBackground} from '@frogpond/colors'
 
-import {TableView, Section  as IosSection, Cell} from 'react-native-tableview-simple'
+import {
+	TableView,
+	Section as IosSection,
+	Cell,
+} from 'react-native-tableview-simple'
 
 export * from './cells'
 
-let AndroidSection = (props) => <IosSection sectionTintColor={androidLightBackground} {...props} />
+let AndroidSection = props => (
+	<IosSection sectionTintColor={androidLightBackground} {...props} />
+)
 let Section
 
 if (Platform.OS === 'android') {
