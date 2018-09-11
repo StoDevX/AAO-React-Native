d662a84c12ca400202b3c87aade2d1003887c4f2
Hawken Rives <hawkrives@fastmail.fm>
2018-09-10 20:55:52 -0500

    expect timer to return $interval if interval divides in evenly

--- modules/timer/__tests__/timer.test.js
+++ modules/timer/__tests__/timer.test.js
@@ -5,13 +5,13 @@ import {msUntilIntervalRepeat} from '../index'
 describe('msUntilIntervalRepeat', () => {
 	test.each`
 		now              | interval | expected
-		${100}           | ${10}    | ${0}
-		${1536625260000} | ${60000} | ${0}
-		${60000}         | ${60000} | ${0}
+		${100}           | ${10}    | ${10}
+		${1536625260000} | ${60000} | ${60000}
+		${60000}         | ${60000} | ${60000}
 		${35}            | ${60}    | ${25}
 		${110}           | ${100}   | ${90}
 		${1}             | ${9}     | ${8}
-		${1000}          | ${20}    | ${0}
+		${1000}          | ${20}    | ${20}
 		${9}             | ${20}    | ${11}
 	`(
 		'returns remaining: $expected with now: $now and interval: $interval',
