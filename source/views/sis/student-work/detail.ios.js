8bd10aec6925c093ea98a93b516dd1bf2af833f0
Drew Volz <volz.drew@gmail.com>
2018-09-10 18:31:02 -0600

    update ios detail with new job fields

--- source/views/sis/student-work/detail.ios.js
+++ source/views/sis/student-work/detail.ios.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import {Text, ScrollView, StyleSheet} from 'react-native'
 import {sendEmail} from '../../../components/send-email'
+import {callPhone} from '../../../components/call-phone'
 import {Cell, Section, TableView, SelectableCell} from '@frogpond/tableview'
 import moment from 'moment'
 import * as c from '@frogpond/colors'
@@ -34,18 +35,32 @@ function Information({job}: {job: JobType}) {
 		<Cell cellStyle="LeftDetail" detail="Office" title={job.office} />
 	) : null
 
-	let contactName = job.contactName || job.contactEmail
-	const contact = contactName ? (
+	const name = job.contactName
+	const contactName = name ? (
+		<Cell cellStyle="LeftDetail" detail="Contact" title={name} />
+	) : null
+
+	const email = job.contactEmail
+	const contactEmail = name ? (
 		<Cell
-			accessory={job.contactEmail ? 'DisclosureIndicator' : undefined}
+			accessory={email ? 'DisclosureIndicator' : undefined}
 			cellStyle="LeftDetail"
-			detail="Contact"
+			detail="Email"
 			onPress={() =>
-				job.contactEmail
-					? sendEmail({to: [job.contactEmail], subject: job.title, body: ''})
-					: null
+				email ? sendEmail({to: [email], subject: job.title, body: ''}) : null
 			}
-			title={contactName}
+			title={email}
+		/>
+	) : null
+
+	const contactNumber = job.contactPhone
+	const contactPhone = contactNumber ? (
+		<Cell
+			accessory={contactNumber ? 'DisclosureIndicator' : undefined}
+			cellStyle="LeftDetail"
+			detail="Phone"
+			onPress={() => (contactNumber ? callPhone(contactNumber) : null)}
+			title={contactNumber}
 		/>
 	) : null
 
@@ -66,13 +81,25 @@ function Information({job}: {job: JobType}) {
 		<Cell cellStyle="LeftDetail" detail="Category" title={job.type} />
 	) : null
 
+	const openPositions = job.openPositions ? (
+		<Cell cellStyle="LeftDetail" detail="Positions" title={job.openPositions} />
+	) : null
+
+	const year = job.year ? (
+		<Cell cellStyle="LeftDetail" detail="Time Period" title={job.year} />
+	) : null
+
 	return (
 		<Section header="INFORMATION">
 			{office}
-			{contact}
+			{contactName}
+			{contactEmail}
+			{contactPhone}
 			{hours}
 			{amount}
+			{year}
 			{category}
+			{openPositions}
 		</Section>
 	)
 }
@@ -97,6 +124,30 @@ function Comments({job}: {job: JobType}) {
 	return job.comments ? (
 		<Section header="COMMENTS">
 			<SelectableCell text={entities.decode(job.comments)} />
+		</Section>
+	) : null
+}
+
+function FirstYearAppropriate({job}: {job: JobType}) {
+	return job.goodForIncomingStudents ? (
+		<Section header="APPROPRIATE FOR FIRST-YEAR STUDENTS">
+			<SelectableCell text={job.goodForIncomingStudents ? 'Yes' : 'No'} />
+		</Section>
+	) : null
+}
+
+function Timeline({job}: {job: JobType}) {
+	return job.timeline ? (
+		<Section header="TIMELINE">
+			<SelectableCell text={entities.decode(job.timeline)} />
+		</Section>
+	) : null
+}
+
+function HowToApply({job}: {job: JobType}) {
+	return job.howToApply ? (
+		<Section header="HOW TO APPLY">
+			<SelectableCell text={entities.decode(job.howToApply)} />
 		</Section>
 	) : null
 }
@@ -132,9 +183,12 @@ export class JobDetailView extends React.PureComponent<Props> {
 				<Title selectable={true}>{job.title}</Title>
 				<TableView>
 					<Information job={job} />
+					<FirstYearAppropriate job={job} />
 					<Description job={job} />
 					<Skills job={job} />
 					<Comments job={job} />
+					<HowToApply job={job} />
+					<Timeline job={job} />
 				</TableView>
 				<LastUpdated when={job.lastModified} />
 			</ScrollView>
