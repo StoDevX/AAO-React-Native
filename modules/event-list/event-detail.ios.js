bfcdfd53e77a8852c0695101a5f56eead5055023
Drew Volz <volz.drew@gmail.com>
2018-09-12 14:49:51 -0600

    remove links from ios calendar detail

--- modules/event-list/event-detail.ios.js
+++ modules/event-list/event-detail.ios.js
@@ -2,7 +2,6 @@
 import * as React from 'react'
 import {ScrollView} from 'react-native'
 import {
-	Cell,
 	Section,
 	TableView,
 	ButtonCell,
@@ -12,7 +11,6 @@ import type {EventType} from '@frogpond/event-type'
 import type {PoweredBy} from './types'
 import type {NavigationScreenProp} from 'react-navigation'
 import {ShareButton} from '@frogpond/navigation-buttons'
-import {openUrl} from '@frogpond/open-url'
 import {ListFooter} from '@frogpond/lists'
 import {shareEvent, getTimes} from './calendar-util'
 import {AddToCalendar} from '@frogpond/add-to-device-calendar'
@@ -21,21 +19,6 @@ function MaybeSection({header, content}: {header: string, content: string}) {
 	return content.trim() ? (
 		<Section header={header}>
 			<SelectableCell text={content} />
-		</Section>
-	) : null
-}
-
-function Links({header, event}: {header: string, event: EventType}) {
-	return event.links.length ? (
-		<Section header={header}>
-			{event.links.map(url => (
-				<Cell
-					key={url}
-					accessory="DisclosureIndicator"
-					onPress={() => openUrl(url)}
-					title={url}
-				/>
-			))}
 		</Section>
 	) : null
 }
@@ -68,7 +51,6 @@ export class EventDetail extends React.Component<Props> {
 					<MaybeSection content={getTimes(event)} header="TIME" />
 					<MaybeSection content={event.location} header="LOCATION" />
 					<MaybeSection content={event.description} header="DESCRIPTION" />
-					<Links event={event} header="LINKS" />
 
 					<AddToCalendar
 						event={event}
