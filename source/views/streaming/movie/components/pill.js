87a293ccb9beeb7952300376ad826d67a4944e9b
Drew Volz <volz.drew@gmail.com>
2018-09-15 16:20:27 -0600

    pass colors directly and run prettier

--- source/views/streaming/movie/components/pill.js
+++ source/views/streaming/movie/components/pill.js
@@ -5,28 +5,24 @@ import * as c from '@frogpond/colors'
 import {firstReadable} from '@frogpond/colors'
 import glamorous from 'glamorous-native'
 
-type Colors = $Keys<typeof c.sto>
-
 type Props = {
 	children: React.Node,
-	bgColorName: Colors,
+	bgColor: string,
 }
 
-export const Pill = ({children, bgColorName, ...props}: Props) => (
-	<glamorous.View
-		backgroundColor={c.sto[bgColorName]}
-		borderRadius={50}
-		{...props}
-	>
-		<glamorous.Text
-			color={c.stoText[bgColorName]}
-			fontSize={12}
-			fontVariant={['small-caps']}
-			paddingBottom={3}
-			paddingHorizontal={8}
-			paddingTop={1}
-		>
-			{children}
-		</glamorous.Text>
-	</glamorous.View>
-)
+export const Pill = ({children, bgColor, ...props}: Props) => {
+	return (
+		<glamorous.View backgroundColor={bgColor} borderRadius={50} {...props}>
+			<glamorous.Text
+				color={firstReadable(bgColor, [c.black, c.white])}
+				fontSize={12}
+				fontVariant={['small-caps']}
+				paddingBottom={3}
+				paddingHorizontal={8}
+				paddingTop={1}
+			>
+				{children}
+			</glamorous.Text>
+		</glamorous.View>
+	)
+}
