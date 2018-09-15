4e2962190b92732bf584d94411358fec7dfbc871
Hawken Rives <rives@stolaf.edu>
2018-09-15 12:38:26 -0500

    show the bonapp dev tool on beta/nightly

--- source/views/menus/index.js
+++ source/views/menus/index.js
@@ -2,7 +2,7 @@
 
 import * as React from 'react'
 import {TabNavigator, TabBarIcon} from '@frogpond/navigation-tabs'
-import {IS_PRODUCTION} from '@frogpond/constants'
+import {isReleaseBuild} from '@frogpond/constants'
 
 import {BonAppHostedMenu} from './menu-bonapp'
 import {GitHubHostedMenu} from './menu-github'
@@ -89,7 +89,7 @@ export const MenusView = TabNavigator({
 		},
 	},
 
-	...(!IS_PRODUCTION ? {BonAppDevToolView: {screen: BonAppPickerView}} : {}),
+	...(!isReleaseBuild() ? {BonAppDevToolView: {screen: BonAppPickerView}} : {}),
 })
 MenusView.navigationOptions = {
 	title: 'Menus',
