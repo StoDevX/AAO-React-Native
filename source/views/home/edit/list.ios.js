591a6d317641d040e5c635673ea80b01bf19bcfe
Drew Volz <volz.drew@gmail.com>
2018-09-19 15:24:23 -0600

    swap out sortable-list for native tableview

--- source/views/home/edit/list.ios.js
+++ source/views/home/edit/list.ios.js
@@ -5,27 +5,33 @@ import {StyleSheet} from 'react-native'
 import {type ReduxState} from '../../../redux'
 import {saveHomescreenOrder} from '../../../redux/parts/homescreen'
 import {connect} from 'react-redux'
-import * as c from '@frogpond/colors'
 import fromPairs from 'lodash/fromPairs'
+import map from 'lodash/map'
 import {Viewport} from '@frogpond/viewport'
 
-import SortableList from '@hawkrives/react-native-sortable-list'
+import TableView from 'react-native-tableview'
 import debounce from 'lodash/debounce'
 
-import type {ViewType} from '../../views'
 import {allViews} from '../../views'
+// todo: might need an eslint rule to ignore this just once... reactModuleForCell is using it`
 import {EditHomeRow} from './row'
 import {toggleViewDisabled} from '../../../redux/parts/homescreen'
+
+const {Item, Section} = TableView
+
+/*
+ * tableViewCellEditingStyle
+ *  - None:   0
+ *  - Delete: 1
+ *  - Insert: 2
+ */
+const EDITING_STYLE = 0
 
 const objViews = fromPairs(allViews.map(v => [v.view, v]))
 
 const styles = StyleSheet.create({
-	contentContainer: {
-		backgroundColor: c.iosLightBackground,
-		paddingTop: 10,
-		paddingBottom: 20,
-		flexDirection: 'column',
-		alignItems: 'stretch',
+	table: {
+		flex: 1,
 	},
 })
 
@@ -46,20 +52,7 @@ class EditHomeView extends React.PureComponent<Props> {
 		title: 'Edit Home',
 	}
 
-	renderRow = (args: {data: ViewType, active: boolean, width: number}) => {
-		const {data, active, width} = args
-		const enabled = !this.props.inactiveViews.includes(data.view)
-		return (
-			<EditHomeRow
-				active={active}
-				data={data}
-				isEnabled={enabled}
-				onToggle={this.props.onToggleViewDisabled}
-				width={width}
-			/>
-		)
-	}
-
+	// todo: change the save order logic in redux/parts/homescreen because this returns the row order info
 	_saveOrder = (order: string[]) => this.props.onSaveOrder(order)
 
 	onChangeOrder = debounce(this._saveOrder, 100)
@@ -67,14 +60,41 @@ class EditHomeView extends React.PureComponent<Props> {
 	render() {
 		return (
 			<Viewport
-				render={({width}) => (
-					<SortableList
-						contentContainerStyle={[styles.contentContainer, {width}]}
-						data={objViews}
-						onChangeOrder={this.onChangeOrder}
-						order={this.props.order}
-						renderRow={props => this.renderRow({...props, width})}
-					/>
+				render={() => (
+					// <SortableList
+					// 	data={objViews}
+					// 	onChangeOrder={this.onChangeOrder}
+					// 	order={this.props.order}
+					// 	renderRow={props => this.renderRow({...props, width})}
+					// />
+
+					// project:         https://github.com/aksonov/react-native-tableview
+					// all types:       https://github.com/aksonov/react-native-tableview/blob/master/src/index.d.ts
+					// editing example: https://github.com/aksonov/react-native-tableview/blob/master/example/src/screens/Example6.js
+
+					<TableView
+						editing={true}
+						onChange={order => this.onChangeOrder(order)}
+						reactModuleForCell="EditHomeRow"
+						style={styles.table}
+						tableViewCellEditingStyle={EDITING_STYLE}
+					>
+						<Section canEdit={true} canMove={true}>
+							{map(objViews, (view, key) => (
+								<Item
+									key={key}
+									active={view.active}
+									gradient={view.gradient}
+									icon={view.icon}
+									isEnabled={!this.props.inactiveViews.includes(objViews.view)}
+									// todo: can only pass primitives... move this to the list row?
+									onToggle={this.props.onToggleViewDisabled}
+									tint={view.tint}
+									title={view.title}
+								/>
+							))}
+						</Section>
+					</TableView>
 				)}
 			/>
 		)
