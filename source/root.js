a85d63685a7c4366e46a2a66b6a8e8ed9489ca4d
Drew Volz <volz.drew@gmail.com>
2018-09-19 15:19:53 -0600

    prevent the calendar events module from showing warnings

--- source/root.js
+++ source/root.js
@@ -8,6 +8,8 @@ YellowBox.ignoreWarnings([
 	'Warning: isMounted(...) is deprecated',
 	// TODO: remove me after upgrading to RN 0.56
 	'Module RCTImageLoader',
+	// TODO: remove me (working with new native iOS tableview renders this once per cell)
+	'Module RNCalendarEvents',
 ])
 
 AppRegistry.registerComponent('AllAboutOlaf', () => App)
