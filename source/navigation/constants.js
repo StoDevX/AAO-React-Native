64bb508e00027cb48d4ff315ce51400a4059e655
Hawken Rives <rives@stolaf.edu>
2018-11-06 18:58:36 -0600

    remove IS_PRODUCTION import

--- source/navigation/constants.js
+++ source/navigation/constants.js
@@ -1,7 +1,7 @@
 // @flow
 
-import {IS_PRODUCTION} from '@frogpond/constants'
+export const persistenceKey = null
 
 // disabling navigation persistence until we finish an audit of the app
+// import {IS_PRODUCTION} from '@frogpond/constants'
 // export const persistenceKey = IS_PRODUCTION ? 'NavState' : null
-export const persistenceKey = null
