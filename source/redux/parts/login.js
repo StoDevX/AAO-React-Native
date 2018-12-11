24cea55864b871cb2b2f08c057291291ad4a4316
Hawken Rives <hawkrives@fastmail.fm>
2018-12-10 19:45:55 -0600

    remove CREDENTIALS_ prefix from all login actions

--- source/redux/parts/login.js
+++ source/redux/parts/login.js
@@ -22,10 +22,10 @@ type Dispatch<A: Action> = (action: A | Promise<A> | ThunkAction<A>) => any
 type GetState = () => ReduxState
 type ThunkAction<A: Action> = (dispatch: Dispatch<A>, getState: GetState) => any
 
-const CREDENTIALS_LOGIN_START = 'settings/CREDENTIALS_LOGIN_START'
-const CREDENTIALS_LOGIN_SUCCESS = 'settings/CREDENTIALS_LOGIN_SUCCESS'
-const CREDENTIALS_LOGIN_FAILURE = 'settings/CREDENTIALS_LOGIN_FAILURE'
-const CREDENTIALS_LOGOUT = 'settings/CREDENTIALS_LOGOUT'
+const LOGIN_START = 'settings/CREDENTIALS_LOGIN_START'
+const LOGIN_SUCCESS = 'settings/CREDENTIALS_LOGIN_SUCCESS'
+const LOGIN_FAILURE = 'settings/CREDENTIALS_LOGIN_FAILURE'
+const LOGOUT = 'settings/CREDENTIALS_LOGOUT'
 
 type LoginStartAction = {|type: 'settings/CREDENTIALS_LOGIN_START'|}
 type LoginSuccessAction = {|type: 'settings/CREDENTIALS_LOGIN_SUCCESS'|}
@@ -58,24 +58,24 @@ export function logInViaCredentials(
 	password: string,
 ): ThunkAction<LogInActions> {
 	return async dispatch => {
-		dispatch({type: CREDENTIALS_LOGIN_START})
+		dispatch({type: LOGIN_START})
 
 		await saveLoginCredentials({username, password})
 
 		let result = await performLogin()
 		if (result === 'success') {
-			dispatch({type: CREDENTIALS_LOGIN_SUCCESS})
+			dispatch({type: LOGIN_SUCCESS})
 			trackLogIn()
 		} else if (result === 'bad-credentials') {
-			dispatch({type: CREDENTIALS_LOGIN_FAILURE})
+			dispatch({type: LOGIN_FAILURE})
 			trackLoginFailure('Bad credentials')
 			showInvalidLoginMessage()
 		} else if (result === 'no-credentials') {
-			dispatch({type: CREDENTIALS_LOGIN_FAILURE})
+			dispatch({type: LOGIN_FAILURE})
 			trackLoginFailure('No credentials')
 			showUnknownLoginMessage()
 		} else if (result === 'network') {
-			dispatch({type: CREDENTIALS_LOGIN_FAILURE})
+			dispatch({type: LOGIN_FAILURE})
 			trackLoginFailure('No network')
 			showNetworkFailureMessage()
 		} else {
@@ -88,7 +88,7 @@ type LogOutAction = {|type: 'settings/CREDENTIALS_LOGOUT'|}
 export async function logOutViaCredentials(): Promise<LogOutAction> {
 	trackLogOut()
 	await clearLoginCredentials()
-	return {type: CREDENTIALS_LOGOUT}
+	return {type: LOGOUT}
 }
 
 type Action = CredentialsActions
@@ -105,16 +105,16 @@ const initialState = {
 
 export function login(state: State = initialState, action: Action) {
 	switch (action.type) {
-		case CREDENTIALS_LOGIN_START:
+		case LOGIN_START:
 			return {...state, status: 'checking'}
 
-		case CREDENTIALS_LOGIN_SUCCESS:
+		case LOGIN_SUCCESS:
 			return {...state, status: 'logged-in'}
 
-		case CREDENTIALS_LOGIN_FAILURE:
+		case LOGIN_FAILURE:
 			return {...state, status: 'invalid'}
 
-		case CREDENTIALS_LOGOUT:
+		case LOGOUT:
 			return {...state, status: 'logged-out'}
 
 		default:
