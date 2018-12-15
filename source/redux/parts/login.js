bedffca6ef1faf0b75d6742111a40efe7a2a3d10
Hawken Rives <hawkrives@fastmail.fm>
2018-12-15 17:58:55 -0600

    handle new error types

--- source/redux/parts/login.js
+++ source/redux/parts/login.js
@@ -39,6 +39,13 @@ const showNetworkFailureMessage = () =>
 		[{text: 'OK'}],
 	)
 
+const showServerErrorMessage = () =>
+	Alert.alert(
+		'Server Failure',
+		'The OleCard Server is having issues right now. Please try again later.',
+		[{text: 'OK'}],
+	)
+
 const showInvalidLoginMessage = () =>
 	Alert.alert(
 		'Invalid Login',
@@ -85,6 +92,14 @@ export function logInViaCredentials(
 			dispatch({type: LOGIN_FAILURE})
 			trackLoginFailure('No network')
 			showNetworkFailureMessage()
+		} else if (result === 'server-error') {
+			dispatch({type: LOGIN_FAILURE})
+			trackLoginFailure('Server error')
+			showServerErrorMessage()
+		} else if (result === 'other') {
+			dispatch({type: LOGIN_FAILURE})
+			trackLoginFailure('Unknown problem')
+			showUnknownFailureMessage()
 		} else {
 			showUnknownFailureMessage()
 			;(result: empty)
