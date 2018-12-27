018874718c04ae42515acd1d08857672ddc36214
Hawken Rives <hawkrives@fastmail.fm>
2018-12-27 11:45:15 -0600

    s|US/Central|America/Chicago|g

--- source/views/transportation/bus/lib/__tests__/moment.helper.js
+++ source/views/transportation/bus/lib/__tests__/moment.helper.js
@@ -1,7 +1,7 @@
 // @flow
 import moment from 'moment-timezone'
 
-const CENTRAL_TZ = 'US/Central'
+const CENTRAL_TZ = 'America/Chicago'
 
 export const time = (time: string) => moment.tz(time, 'h:mma', true, CENTRAL_TZ)
 
