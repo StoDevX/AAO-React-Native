990fe6566ff2b647ea12d5a9c4c3771558bc43c5
Kristofer Rye <kristofer.rye@gmail.com>
2019-01-01 07:56:06 -0600

    parseHours: Guard against open times far in the future
    Similar to #3376, this commit prevents an open time from showing up
    more than six months in the future, by subtracting a year from any
    that are more than 6 months ahead of the provided context moment.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- source/views/building-hours/lib/parse-hours.js
+++ source/views/building-hours/lib/parse-hours.js
@@ -23,9 +23,12 @@ export function parseHours(
 	open.dayOfYear(dayOfYear)
 
 	let sixMonthsAgo = moment(m).subtract(6, 'months')
+	let sixMonthsFromNow = moment(m).add(6, 'months')
 
 	if (open.isBefore(sixMonthsAgo)) {
 		open.add(1, 'year')
+	} else if (open.isAfter(sixMonthsFromNow)) {
+		open.subtract(1, 'year')
 	}
 
 	let close = moment.tz(toTime, TIME_FORMAT, true, CENTRAL_TZ)
@@ -33,6 +36,8 @@ export function parseHours(
 
 	if (close.isBefore(sixMonthsAgo)) {
 		close.add(1, 'year')
+	} else if (close.isAfter(sixMonthsFromNow)) {
+		close.subtract(1, 'year')
 	}
 
 	if (close.isBefore(open)) {
