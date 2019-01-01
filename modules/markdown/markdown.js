03d27f29929a2f6c2ef2bdced82014e45621f93d
Drew Volz <volz.drew@gmail.com>
2019-01-01 12:05:04 -0800

    remove unused text import

--- modules/markdown/markdown.js
+++ modules/markdown/markdown.js
@@ -1,7 +1,7 @@
 // @flow
 
 import * as React from 'react'
-import {View, Text} from 'react-native'
+import {View} from 'react-native'
 import glamorous from 'glamorous-native'
 import ReactMarkdown from 'react-markdown'
 
