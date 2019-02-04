bb885aa8c29d915926864ba2c08ca4d5df2410d4
Hawken Rives <hawkrives@fastmail.fm>
2019-02-03 20:20:21 -0600

    update a few things to use ViewStyleProp

--- modules/layout/styled-component.js
+++ modules/layout/styled-component.js
@@ -1,8 +1,13 @@
 // @flow
 import * as React from 'react'
 import {View} from 'react-native'
+import type {ViewStyleProp} from 'react-native/Libraries/StyleSheet/StyleSheet'
 
-export type PropsType = {children?: any, style?: any, props: mixed}
+export type PropsType = {
+	children?: React.Node,
+	style?: ViewStyleProp,
+	props: ViewStyleProp,
+}
 export const StyledComponent = ({children, style, ...props}: PropsType) => {
-	return <View style={[props, style]}>{children}</View>
+	return <View style={[(props: any), style]}>{children}</View>
 }
