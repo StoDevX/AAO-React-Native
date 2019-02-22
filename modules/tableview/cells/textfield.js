49a73d91e94248087d176357cc555a9069367c89
Hawken Rives <hawkrives@fastmail.fm>
2019-02-21 21:50:43 -0600

    mark CellTextField's props as "exact"
    prevents us from accidentally passing extra props

--- modules/tableview/cells/textfield.js
+++ modules/tableview/cells/textfield.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
 	},
 })
 
-type Props = {
+type Props = {|
 	label?: string,
 	_ref: any => any,
 	disabled: boolean,
@@ -45,7 +45,7 @@ type Props = {
 	autoCapitalize: 'characters' | 'words' | 'sentences' | 'none',
 	value: string,
 	labelWidth?: number,
-}
+|}
 
 export class CellTextField extends React.Component<Props> {
 	_input: any
