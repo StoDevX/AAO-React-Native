f94a19f87882bf099a26c9c2030d4ceb718d59c3
Hawken Rives <hawkrives@fastmail.fm>
2019-03-16 13:53:15 -0500

    trim the react-navigation stub

--- flow-typed/npm/react-navigation_vx.x.x.js
+++ flow-typed/npm/react-navigation_vx.x.x.js
@@ -16,38 +16,3 @@
 declare module 'react-navigation' {
   declare module.exports: any;
 }
-
-/**
- * We include stubs for each file inside this npm package in case you need to
- * require those files directly. Feel free to delete any files that aren't
- * needed.
- */
-declare module 'react-navigation/NavigationTestUtils' {
-  declare module.exports: any;
-}
-
-declare module 'react-navigation/src/navigators/__tests__/SwitchNavigator-test' {
-  declare module.exports: any;
-}
-
-declare module 'react-navigation/src/react-navigation' {
-  declare module.exports: any;
-}
-
-declare module 'react-navigation/src/utils/docsUrl' {
-  declare module.exports: any;
-}
-
-// Filename aliases
-declare module 'react-navigation/NavigationTestUtils.js' {
-  declare module.exports: $Exports<'react-navigation/NavigationTestUtils'>;
-}
-declare module 'react-navigation/src/navigators/__tests__/SwitchNavigator-test.js' {
-  declare module.exports: $Exports<'react-navigation/src/navigators/__tests__/SwitchNavigator-test'>;
-}
-declare module 'react-navigation/src/react-navigation.js' {
-  declare module.exports: $Exports<'react-navigation/src/react-navigation'>;
-}
-declare module 'react-navigation/src/utils/docsUrl.js' {
-  declare module.exports: $Exports<'react-navigation/src/utils/docsUrl'>;
-}
