c77bfbfbc70f437506886a68d6caa733f3c47b9e
Hawken Rives <hawkrives@fastmail.fm>
2019-03-16 12:45:49 -0500

    update flow-typed defs

---
+++ flow-typed/npm/@frogpond/titlecase_vx.x.x.js
@@ -0,0 +1,8 @@
+// flow-typed signature: 90c195ce117519b8e64e2c38ab94a90d
+// flow-typed version: <<STUB>>/@frogpond/titlecase_v1.0.0/flow_v0.92.1
+
+declare module '@frogpond/titlecase' {
+  declare export function toLaxTitleCase(str: string): string;
+  declare export function toTitleCase(str: string): string;
+  declare export default toTitleCase;
+}
