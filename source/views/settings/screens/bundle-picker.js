2434dd958c4002dee285ab69ebe7728744ca3642
Drew Volz <volz.drew@gmail.com>
2019-03-23 00:04:01 -0700

    flow and lint cleanup

--- source/views/settings/screens/bundle-picker.js
+++ source/views/settings/screens/bundle-picker.js
@@ -1,28 +1,24 @@
 // @flow
 import * as React from 'react'
-import {Platform, View} from 'react-native'
+import {View} from 'react-native'
 import {fetch} from '@frogpond/fetch'
-import filter from 'lodash/filter'
 import {CIRCLE_API_BASE_URL} from '../../../lib/constants'
 import {refreshApp} from '../../../lib/refresh'
 import RNFS from 'react-native-fs'
 import {
 	setActiveBundle,
 	registerBundle,
-	unregisterBundle,
 	reloadBundle,
-	getActiveBundle,
-	getBundles,
 } from 'react-native-dynamic-bundle'
-
-const platformBundlePath =
-	Platform.OS === 'ios' ? RNFS.MainBundlePath : RNFS.DocumentDirectoryPath
+import {type TopLevelViewPropsType} from '../../types'
 
 const CIRCLE_CI_TOKEN = ''
 const CIRCLE_BUILD = '50413'
 const CIRCLE_CI_JS_BUNDLE_URI = `${CIRCLE_API_BASE_URL}/${CIRCLE_BUILD}/artifacts?circle-token=${CIRCLE_CI_TOKEN}`
 
-export class BundlePickerView extends React.Component {
+type Props = TopLevelViewPropsType
+
+export class BundlePickerView extends React.Component<Props> {
 	static navigationOptions = {
 		title: 'Bundle Picker',
 	}
@@ -34,21 +30,13 @@ export class BundlePickerView extends React.Component {
 	updateBundle = async () => {
 		const circleResponse = await fetch(CIRCLE_CI_JS_BUNDLE_URI).json()
 
-		const result = await RNFS.downloadFile({
+		await RNFS.downloadFile({
 			fromUrl: circleResponse[0].url,
-			toFile: `${platformBundlePath}/test.bundle`,
+			toFile: `${RNFS.DocumentDirectoryPath}/test.bundle`,
 		})
 
-		// unregisterBundle('test')
 		registerBundle('test', 'test.bundle')
 		setActiveBundle('test')
-
-		const bundles = await getBundles()
-		const activeBundle = await getActiveBundle()
-
-		console.log(bundles)
-		console.log(activeBundle)
-
 		reloadBundle()
 		refreshApp()
 	}
