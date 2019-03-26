8cd3108b3c6ece0c6d6febd0caad22b315c169a7
Kristofer Rye <kristofer.rye@gmail.com>
2019-03-26 10:49:13 -0500

    navigation-buttons: Migrate to platformPrefixIconName
    [GitHub] Part of #3461.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- modules/navigation-buttons/share.js
+++ modules/navigation-buttons/share.js
@@ -1,8 +1,7 @@
 // @flow
 
 import * as React from 'react'
-import {Platform} from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
+import {Icon, platformPrefixIconName} from '@frogpond/icon'
 import {Touchable} from '@frogpond/touchable'
 import {rightButtonStyles as styles} from './styles'
 
@@ -10,12 +9,10 @@ type Props = {
 	onPress: () => any,
 }
 
-const iconName = Platform.OS === 'ios' ? 'ios-share' : 'md-share'
-
 export function ShareButton(props: Props) {
 	return (
 		<Touchable highlight={false} onPress={props.onPress} style={styles.button}>
-			<Icon name={iconName} style={styles.icon} />
+			<Icon name={platformPrefixIconName('share')} style={styles.icon} />
 		</Touchable>
 	)
 }
