562c1ec13d3c7e452c7431b889ef88176a10e9f9
Drew Volz <volz.drew@gmail.com>
2019-04-05 16:55:44 -0700

    Rename styles, add elevation and more margin to credits

--- source/views/streaming/movie/components/credits/credits-android.js
+++ source/views/streaming/movie/components/credits/credits-android.js
@@ -2,7 +2,7 @@
 
 import * as React from 'react'
 import {StyleSheet} from 'react-native'
-import {Card, Paragraph} from 'react-native-paper'
+import {Card, Subheading, Paragraph} from 'react-native-paper'
 
 type Props = {
 	directors: string,
@@ -14,43 +14,51 @@ export const AndroidCredits = ({directors, writers, actors}: Props) => {
 	return (
 		<>
 			{writers === directors ? (
-				<Card style={styles.androidCard}>
-					<Card.Title title="Written and Directed By" />
-					<Card.Content>
+				<Card style={styles.card}>
+					<Card.Title title="Credits" />
+					<Card.Content style={styles.content}>
+						<Subheading>Written and Directed By</Subheading>
 						<Paragraph>{directors}</Paragraph>
+					</Card.Content>
+
+					<Card.Content style={styles.content}>
+						<Subheading>Cast</Subheading>
+						<Paragraph>{actors}</Paragraph>
 					</Card.Content>
 				</Card>
 			) : (
 				<>
-					<Card style={styles.androidCard}>
-						<Card.Title title="Directed By" />
-						<Card.Content>
+					<Card style={styles.card}>
+						<Card.Title title="Credits" />
+						<Card.Content style={styles.content}>
+							<Subheading>Directed By</Subheading>
 							<Paragraph>{directors}</Paragraph>
 						</Card.Content>
-					</Card>
 
-					<Card style={styles.androidCard}>
-						<Card.Title title="Written By" />
-						<Card.Content>
+						<Card.Content style={styles.content}>
+							<Subheading>Written By</Subheading>
 							<Paragraph>{writers}</Paragraph>
+						</Card.Content>
+
+						<Card.Content style={styles.content}>
+							<Subheading>Cast</Subheading>
+							<Paragraph>{actors}</Paragraph>
 						</Card.Content>
 					</Card>
 				</>
 			)}
-
-			<Card style={styles.androidCard}>
-				<Card.Title title="Cast" />
-				<Card.Content>
-					<Paragraph>{actors}</Paragraph>
-				</Card.Content>
-			</Card>
 		</>
 	)
 }
 
 const styles = StyleSheet.create({
-	androidCard: {
-		marginHorizontal: 10,
+	card: {
+		elevation: 2,
 		marginBottom: 10,
+		marginHorizontal: 10,
+	},
+
+	content: {
+		marginBottom: 12,
 	},
 })
