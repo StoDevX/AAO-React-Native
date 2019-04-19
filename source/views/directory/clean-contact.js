e10e8f2149abb6871f30ab0c727fbe5f85c5afc9
Drew Volz <volz.drew@gmail.com>
2019-04-19 16:16:40 -0700

    simplify contact locations (server handles most of that)

--- source/views/directory/clean-contact.js
+++ source/views/directory/clean-contact.js
@@ -1,17 +1,19 @@
 // @flow
-import type {DirectoryType} from './types'
+import type {DirectoryType, LocationType} from './types'
 
 export default function cleanContact(contact: DirectoryType): DirectoryType {
 	const name = contact.displayName
 	const title = contact.displayTitle
 	const photo = contact.thumbnail
-	const building = contact.building
-	const room = contact.room ? contact.room : ''
-	const office = building || room ? `${building} ${room}` : ''
 	const officeHours = contact.officeHours
 	const departments = contact.departments
 	const phone = contact.officePhone
 	const email = contact.email
+
+	let locations = contact.campusLocations.map(location => ({
+		display: location.display,
+		phone: location.phone,
+	}))
 
 	let profile = contact.profileUrl
 	if (profile && !/^https?:\/\//u.test(profile)) {
@@ -23,7 +25,7 @@ export default function cleanContact(contact: DirectoryType): DirectoryType {
 		name,
 		title,
 		photo,
-		office,
+		locations,
 		officeHours,
 		departments,
 		phone,
