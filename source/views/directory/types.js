b7d76afcb073a00fe610f20b7427a7b9cb3dfc7e
Drew Volz <volz.drew@gmail.com>
2019-04-19 16:16:57 -0700

    update the API returned directory types

--- source/views/directory/types.js
+++ source/views/directory/types.js
@@ -1,36 +1,43 @@
 // @flow
 
-export type DirectoryType = {
+type CampusLocationsType = {
+	display: string,
+	buildingabbr: string,
+	building: string,
+	phone: string,
+	room: number,
+}
+
+type DepartmentType = {
+	href: string,
+	name: string,
+}
+
+type HomeAddressType = {
+	zip: string,
+	city: string,
+	country: string,
+	state: string,
+	street: Array<string>,
+}
+
+type DirectoryType = {
 	displayName: string,
 	classYear: number,
 	suffixName: string,
-	extension: number,
 	firstName: string,
 	thumbnail: URL,
 	departments: Array<DepartmentType>,
 	email: string,
 	homePhone: string,
 	onLeave: boolean,
-	officePhone: string,
-	homeAddress: {
-		zip: string,
-		city: string,
-		country: string,
-		state: string,
-		street: Array<string>,
-	},
-	building: string,
+	homeAddress: HomeAddressType,
 	lastName: string,
 	profileUrl: string,
 	username: string,
+	campusLocations: Array<CampusLocationsType>,
 	title: string,
 	photo: string,
 	officeHours: string,
-	room: number,
 	displayTitle: string,
-}
-
-export type DepartmentType = {
-	href: string,
-	name: string,
 }
