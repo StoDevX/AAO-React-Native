218ead477289e693348a80fe486e811f7779dfd2
Hawken Rives <rives@stolaf.edu>
2019-04-28 23:59:38 -0500

    ignore netinfo warning

--- source/root.js
+++ source/root.js
@@ -5,6 +5,7 @@ import App from './app'
 
 YellowBox.ignoreWarnings([
 	'Failed prop type: Invalid prop `containerTagName` of type `object` supplied to `ReactMarkdown`, expected `function`',
+	'NetInfo',
 ])
 
 AppRegistry.registerComponent('AllAboutOlaf', () => App)
