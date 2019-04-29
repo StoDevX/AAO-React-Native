6120fd83ceef940182b453c9d1ac5cc9e24edea9
Hawken Rives <rives@stolaf.edu>
2019-04-28 23:59:34 -0500

    redesign homescreen into grouped views

--- source/views/home/home.js
+++ source/views/home/home.js
@@ -1,78 +1,40 @@
 // @flow
 
 import * as React from 'react'
-import {ScrollView, View, StyleSheet, StatusBar} from 'react-native'
+import {ScrollView, Platform, StatusBar} from 'react-native'
 import {SafeAreaView} from 'react-navigation'
-
-import {connect} from 'react-redux'
+import {List} from 'react-native-paper'
+import Ionicon from 'react-native-vector-icons/Ionicons'
 import {getTheme} from '@frogpond/app-theme'
-import sortBy from 'lodash/sortBy'
+import {TableView, Section, Cell} from 'react-native-tableview-simple'
+import {groupedViews, type ViewType} from '../views'
 import {type TopLevelViewPropsType} from '../types'
-import {type ViewType} from '../views'
-import {type ReduxState} from '../../redux'
-import {allViews} from '../views'
-import {Column} from '@frogpond/layout'
-import {partitionByIndex} from '../../lib/partition-by-index'
-import {HomeScreenButton, CELL_MARGIN} from './button'
 import {trackedOpenUrl, openUrlInBrowser} from '@frogpond/open-url'
 import {EditHomeButton, OpenSettingsButton} from '@frogpond/navigation-buttons'
 import {UnofficialAppNotice} from './notice'
 
-type ReactProps = TopLevelViewPropsType & {
-	views: Array<ViewType>,
-}
-type ReduxStateProps = {
-	order: Array<string>,
-	inactiveViews: Array<string>,
-}
-
-type Props = ReactProps & ReduxStateProps
-
-function HomePage({navigation, order, inactiveViews, views = allViews}: Props) {
-	const sortedViews = sortBy(views, view => order.indexOf(view.view))
-
-	const enabledViews = sortedViews.filter(
-		view => !inactiveViews.includes(view.view),
-	)
-
-	const columns = partitionByIndex(enabledViews)
-
+export default function HomePage({navigation}: TopLevelViewPropsType) {
 	let {androidStatusBarColor, statusBarStyle} = getTheme()
 
 	return (
-		<ScrollView
-			alwaysBounceHorizontal={false}
-			showsHorizontalScrollIndicator={false}
-			showsVerticalScrollIndicator={false}
-			testID="screen-homescreen"
-		>
+		<ScrollView testID="screen-homescreen">
 			<StatusBar
 				backgroundColor={androidStatusBarColor}
 				barStyle={statusBarStyle}
 			/>
 
 			<SafeAreaView>
-				<View style={styles.cells}>
-					{columns.map((contents, i) => (
-						<Column key={i} style={styles.column}>
-							{contents.map(view => (
-								<HomeScreenButton
-									key={view.view}
-									onPress={() => {
-										if (view.type === 'url') {
-											return trackedOpenUrl({url: view.url, id: view.view})
-										} else if (view.type === 'browser-url') {
-											return openUrlInBrowser({url: view.url, id: view.view})
-										} else {
-											return navigation.navigate(view.view)
-										}
-									}}
-									view={view}
-								/>
-							))}
-						</Column>
-					))}
-				</View>
+				<ContentView
+					onPress={view => {
+						if (view.type === 'url') {
+							return trackedOpenUrl({url: view.url, id: view.view})
+						} else if (view.type === 'external-url') {
+							return openUrlInBrowser({url: view.url, id: view.view})
+						} else {
+							return navigation.navigate(view.view)
+						}
+					}}
+				/>
 
 				<UnofficialAppNotice />
 			</SafeAreaView>
@@ -88,27 +50,63 @@ HomePage.navigationOptions = ({navigation}) => {
 	}
 }
 
-function mapStateToProps(state: ReduxState): ReduxStateProps {
-	if (!state.homescreen) {
-		return {order: [], inactiveViews: []}
-	}
+function IosContent({onPress}: {onPress: ViewType => mixed}) {
+	return (
+		<TableView>
+			{[...groupedViews.entries()].map(([group, items]) => (
+				<Section
+					key={group}
+					header={group.toUpperCase()}
+					onActionPress={() => console.warn('pressed')}
+					rounded={true}
+					withSafeAreaView={false}
+					{...(group === 'Menus' ? {action: 'SHOW CARLETON'} : {})}
+				>
+					{items.map((view, i) => {
+						let iconName =
+							view.type === 'external-url'
+								? 'ios-log-out'
+								: view.type === 'url'
+								? 'ios-link'
+								: 'ios-more'
 
-	return {
-		order: state.homescreen.order,
-		inactiveViews: state.homescreen.inactiveViews,
-	}
+						let icon = <Ionicon color="#c7c7cc" name={iconName} size={18} />
+
+						return (
+							<Cell
+								key={view.view + i}
+								{...(view.type === 'view'
+									? {accessory: 'DisclosureIndicator'}
+									: {cellAccessoryView: icon})}
+								onPress={() => onPress(view)}
+								title={view.title}
+							/>
+						)
+					})}
+				</Section>
+			))}
+		</TableView>
+	)
 }
 
-export default connect(mapStateToProps)(HomePage)
+function AndroidContent({onPress}: {onPress: ViewType => mixed}) {
+	return (
+		<>
+			{[...groupedViews.entries()].map(([group, items]) => (
+				<List.Section key={group}>
+					<List.Subheader>{group}</List.Subheader>
 
-const styles = StyleSheet.create({
-	cells: {
-		marginHorizontal: CELL_MARGIN / 2,
-		paddingTop: CELL_MARGIN,
+					{items.map((view, i) => (
+						<List.Item
+							key={view.view + i}
+							onPress={() => onPress(view)}
+							title={view.title}
+						/>
+					))}
+				</List.Section>
+			))}
+		</>
+	)
+}
 
-		flexDirection: 'row',
-	},
-	column: {
-		flex: 1,
-	},
-})
+const ContentView = Platform.OS === 'ios' ? IosContent : AndroidContent
