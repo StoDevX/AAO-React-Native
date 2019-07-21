4b1a80dbafada0f352305a6478da022ab79205a1
Drew Volz <volz.drew@gmail.com>
2019-07-20 19:47:24 -0700

    making flow happy with the type of possibilities

--- modules/colors/util.js
+++ modules/colors/util.js
@@ -8,10 +8,7 @@ import {black, white} from './colors'
  * foreground color that is readable (at WCAG AA-Small), or black/white if
  * none of the options were readable.
  */
-export function firstReadable(
-	background: string,
-	possibilities: Array<string>,
-) {
+export function firstReadable(background: string, possibilities: Array<any>) {
 	possibilities = possibilities.map(c => tinycolor(c))
 	let readable = possibilities.find(c => tinycolor.isReadable(c, background))
 	if (readable) {
