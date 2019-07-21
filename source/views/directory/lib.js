1edf5de5fb22531380c796e8f72f5d0896b51637
Drew Volz <volz.drew@gmail.com>
2019-07-21 16:43:11 -0700

    office hours and on-leave helpers

---
+++ source/views/directory/lib.js
@@ -0,0 +1,31 @@
+// @flow
+
+import type {OfficeHours, OnLeave} from './types'
+
+export const officeHoursTitle = (officeHours: OfficeHours) => {
+	let content = officeHours.content || ''
+	let label = officeHours.hrefLabel || ''
+
+	return `${content} ${label}`
+}
+
+export const officeHoursLabel = (
+	officeHours: OfficeHours,
+	onLeave: OnLeave,
+) => {
+	const prefix = officeHours.prefix || ''
+	const content = officeHours.content || ''
+
+	const sabbaticalTitle = () => {
+		const leaveType = onLeave.type || ''
+		return /sabbatical/u.test(leaveType)
+	}
+
+	if (prefix) {
+		return prefix.substring(0, prefix.length - 1)
+	} else if (onLeave && sabbaticalTitle()) {
+		return 'On Leave'
+	} else {
+		return content
+	}
+}
