2f0cbd788449048d0c1a0e162c4d77777d96d746
Hawken Rives <hawkrives@fastmail.fm>
2019-09-07 11:50:17 -0500

    ignore a false-positive from eslint-plugin-react-hooks

--- source/init/navigation.js
+++ source/init/navigation.js
@@ -2,4 +2,8 @@
 
 import {useScreens} from 'react-native-screens'
 
+// eslint-plugin-react-hooks occasionally has a false-positive;
+// this method, while named use*, is not a hook.
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
 useScreens()
