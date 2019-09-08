71f93021367b6e96f938fc802f2542fa52cd2604
Hawken Rives <hawkrives@fastmail.fm>
2019-09-08 15:09:37 -0500

    use auto-linking on android app

--- android/app/src/main/java/com/allaboutolaf/MainApplication.java
+++ android/app/src/main/java/com/allaboutolaf/MainApplication.java
@@ -5,33 +5,13 @@ import android.net.http.HttpResponseCache;
 import android.util.Log;
 
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
-
-// keep these sorted alphabetically
-// import com.avishayil.rnrestart.ReactNativeRestartPackage;
-// import com.BV.LinearGradient.LinearGradientPackage;
-// import com.calendarevents.CalendarEventsPackage;
-// import com.facebook.react.ReactApplication;
-// import com.github.reactnativecommunity.location.RNLocationPackage;
-// import com.reactnativecommunity.webview.RNCWebViewPackage;
-// import com.reactnativecommunity.netinfo.NetInfoPackage;
-// import com.facebook.react.ReactNativeHost;
-// import com.facebook.react.ReactPackage;
-// import com.facebook.react.shell.MainReactPackage;
-// import com.facebook.soloader.SoLoader;
-// import com.github.droibit.android.reactnative.customtabs.CustomTabsPackage;
-// import com.learnium.RNDeviceInfo.RNDeviceInfo;
-// import com.mapbox.rctmgl.RCTMGLPackage;
-// import com.oblador.keychain.KeychainPackage;
-// import com.oblador.vectoricons.VectorIconsPackage;
-// import com.pusherman.networkinfo.RNNetworkInfoPackage;
-// import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-// import io.sentry.RNSentryPackage;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -45,28 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
-       List<ReactPackage> packages = new PackageList(this).getPackages();
+      List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
-      // return Arrays.asList(
-      //   new MainReactPackage(),
-      //   // please keep these sorted alphabetically
-      //   new CalendarEventsPackage(),
-      //   new CustomTabsPackage(),
-      //   new KeychainPackage(),
-      //   new LinearGradientPackage(),
-      //   new NetInfoPackage(),
-      //   new RCTMGLPackage(),
-      //   new ReactNativeRestartPackage(),
-      //   new RNCWebViewPackage(),
-      //   new RNDeviceInfo(),
-      //   new RNGestureHandlerPackage(),
-      //   new RNLocationPackage(),
-      //   new RNNetworkInfoPackage(),
-      //   new RNSentryPackage(),
-      //   new VectorIconsPackage()
-      // );
     }
 
     @Override
