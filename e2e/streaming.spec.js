ca242b7726b7edf579563c1c7284905dbd38d2d5
Kristofer Rye <kristofer.rye@gmail.com>
2019-10-20 08:55:00 -0500

    e2e: Break apart the Streaming tests a bit more
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- e2e/streaming.spec.js
+++ e2e/streaming.spec.js
@@ -2,29 +2,36 @@
 /* global element, by */
 
 describe('Streaming Media View', () => {
-	it('has the Stream List visible by default', async () => {
+	it('is reachable from the homescreen', async () => {
+		// Start at the home screen
 		await expect(element(by.id('screen-homescreen'))).toBeVisible()
+
+		// Expect the StreamingView button to be present, and tap on it
 		await expect(
 			element(by.id('homescreen-button-StreamingView')),
 		).toBeVisible()
 		await element(by.id('homescreen-button-StreamingView')).tap()
-		await expect(element(by.id('screen-homescreen'))).toBeNotVisible()
 
+		// Verify that the navigation took us away from the homescreen
+		await expect(element(by.id('screen-homescreen'))).toBeNotVisible()
+	})
+
+	it('has the Stream List visible by default', async () => {
+		// Navigate into StreamingView
+		await element(by.id('homescreen-button-StreamingView')).tap()
+
+		// The stream-list should be visible now
 		await expect(element(by.id('stream-list'))).toBeVisible()
 	})
-})
 
-describe('KSTO Radio View', () => {
-	it('is reachable under the home screen', async () => {
-		await expect(element(by.id('screen-homescreen'))).toBeVisible()
-		await expect(
-			element(by.id('homescreen-button-StreamingView')),
-		).toBeVisible()
-		await element(by.id('homescreen-button-StreamingView')).tap()
-		await expect(element(by.id('screen-homescreen'))).toBeNotVisible()
-		await expect(element(by.id('stream-list'))).toBeVisible()
+	describe('KSTO Radio View', () => {
+		it('is reachable under the home screen', async () => {
+			// Navigate into StreamingView
+			await element(by.id('homescreen-button-StreamingView')).tap()
 
-		await element(by.id('button-ksto')).tap()
-		await expect(element(by.id('screen-streaming-radio-ksto'))).toBeVisible()
+			// Enter the ksto view
+			await element(by.id('button-ksto')).tap()
+			await expect(element(by.id('screen-streaming-radio-ksto'))).toBeVisible()
+		})
 	})
 })
