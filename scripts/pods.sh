733ee6eb410ce6432749832ff52faf8f2aae3d6b
Hawken Rives <hawkrives@fastmail.fm>
2019-11-02 15:56:17 -0500

    update pods repo before installing

--- scripts/pods.sh
+++ scripts/pods.sh
@@ -7,7 +7,7 @@ USE_PODS=${FP_PODS:-yes}
 if [[ $USE_PODS = 'yes' ]]; then
 	if [[ $(uname) = 'Darwin' ]]; then
 		bundle install --path .bundle
-		cd ios && bundle exec pod install
+		cd ios && bundle exec pod repo update && bundle exec pod install
 	else
 		echo 'not on macos; not installing cocoapods'
 	fi
