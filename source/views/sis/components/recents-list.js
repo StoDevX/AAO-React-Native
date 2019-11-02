08cea3461e8e35d4aa8ddbf462a4f4047d85d9d6
Hawken Rives <hawkrives@fastmail.fm>
2019-11-02 18:29:30 -0500

    remove nested ScrollView / FlatLists from the search screen

--- source/views/sis/components/recents-list.js
+++ source/views/sis/components/recents-list.js
@@ -18,69 +18,64 @@ type Props = {
 	theme: AppTheme,
 }
 
-class RecentItemsList extends React.PureComponent<Props> {
-	renderSeparator = () => <ListSeparator spacing={{left: 17, right: 17}} />
+function RecentItemsList(props: Props) {
+	let {
+		items,
+		actionLabel,
+		onAction,
+		title,
+		emptyHeader,
+		emptyText,
+		theme,
+	} = props
 
-	renderItem = ({item}: {item: string}) => {
-		let foreground = {color: this.props.theme.iosPushButtonCellForeground}
+	let foreground = {color: theme.iosPushButtonCellForeground}
 
-		return (
-			<ListRow arrowPosition="none" onPress={() => this.onPressRow(item)}>
-				<Text numberOfLines={1} style={[foreground, styles.listItem]}>
-					{item}
-				</Text>
-			</ListRow>
-		)
-	}
-
-	onPressRow = (item: string) => {
-		this.props.onItemPress(item)
-	}
-
-	keyExtractor = (item: string) => item
-
-	render() {
-		let {
-			items,
-			actionLabel,
-			onAction,
-			title,
-			emptyHeader,
-			emptyText,
-			theme,
-		} = this.props
-
-		let foreground = {color: theme.iosPushButtonCellForeground}
-
-		return (
-			<View>
-				<View style={styles.rowFlex}>
-					{title && <Text style={styles.subHeader}>{title}</Text>}
-					{onAction && (
-						<Text onPress={onAction} style={[foreground, styles.sideButton]}>
-							{actionLabel}
-						</Text>
-					)}
-				</View>
-				<FlatList
-					ItemSeparatorComponent={this.renderSeparator}
-					ListEmptyComponent={
-						<NoticeView
-							header={emptyHeader}
-							style={styles.notice}
-							text={emptyText}
-							textStyle={styles.noticeText}
-						/>
-					}
-					data={items}
-					keyExtractor={this.keyExtractor}
-					renderItem={this.renderItem}
-					// TODO(react-native@0.58): enable this once React Native no longer dies when opening Settings
-					//scrollEnabled={false}
-				/>
+	return (
+		<>
+			<View style={styles.rowFlex}>
+				{title && <Text style={styles.subHeader}>{title}</Text>}
+				{onAction && (
+					<Text onPress={onAction} style={[foreground, styles.sideButton]}>
+						{actionLabel}
+					</Text>
+				)}
 			</View>
-		)
-	}
+
+			{items.length === 0 ? (
+				<NoticeView
+					header={emptyHeader}
+					style={styles.notice}
+					text={emptyText}
+					textStyle={styles.noticeText}
+				/>
+			) : (
+				items.map((item, i) => (
+					<>
+						<ListRow
+							key={item}
+							arrowPosition="none"
+							onPress={() => props.onItemPress(item)}
+						>
+							<Text
+								numberOfLines={1}
+								style={[
+									{color: props.theme.iosPushButtonCellForeground},
+									styles.listItem,
+								]}
+							>
+								{item}
+							</Text>
+						</ListRow>
+
+						{i < items.length - 1 ? (
+							<ListSeparator spacing={{left: 17, right: 17}} />
+						) : null}
+					</>
+				))
+			)}
+		</>
+	)
 }
 
 export const RawRecentItemsList = RecentItemsList
