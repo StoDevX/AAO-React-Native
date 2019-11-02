5c3708a7164b57c5a95232453da704e02a85ee98
Hawken Rives <hawkrives@fastmail.fm>
2019-11-02 14:19:34 -0500

    add automatic installation of cocoapods to postinstall
    you can use `export FP_PODS=no` to skip cocoapods installation

---
+++ scripts/pods.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+USE_PODS=${FP_PODS:-yes}
+
+if [[ $USE_PODS = 'yes' ]]; then
+	if [[ $(uname) = 'Darwin' ]]; then
+		cd ios && bundle exec pod install
+	else
+		echo 'not on macos; not installing cocoapods'
+	fi
+else
+	echo 'FP_PODS=no; not installing cocoapods'
+fi
