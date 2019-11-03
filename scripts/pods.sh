64f7cdc597c57b7f0967a3ac67fb7f3d403f8586
Hawken Rives <hawkrives@fastmail.fm>
2019-11-03 12:31:38 -0600

    remove auto-run of `pod repo update`

--- scripts/pods.sh
+++ scripts/pods.sh
@@ -7,7 +7,11 @@ USE_PODS=${FP_PODS:-yes}
 if [[ $USE_PODS = 'yes' ]]; then
 	if [[ $(uname) = 'Darwin' ]]; then
 		bundle install --path .bundle
-		cd ios && bundle exec pod repo update && bundle exec pod install
+		cd ios && bundle exec pod install
+
+		if [[ $? != 0 ]]; then
+			echo 'try running `bundle exec pod repo update`' 1>&2
+		fi
 	else
 		echo 'not on macos; not installing cocoapods'
 	fi
