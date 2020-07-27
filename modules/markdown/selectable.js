64ee05f0490fb3d8c990873ce1a6bbcd1c77acaf
Hawken Rives <rives@stolaf.edu>
2020-07-26 20:09:28 -0500

    convert another to "any"

--- modules/markdown/selectable.js
+++ modules/markdown/selectable.js
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import {Text} from 'react-native'
 
-type Props = {}
+type Props = any
 
 export function SelectableText(props: Props) {
 	return <Text selectable={true} {...props} />
