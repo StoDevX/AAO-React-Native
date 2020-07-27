ec2a38a6888e66edb1bf34aa89b3546c7cff4182
Hawken Rives <rives@stolaf.edu>
2020-07-26 20:07:43 -0500

    convert styled-component to "any"

--- modules/layout/styled-component.js
+++ modules/layout/styled-component.js
@@ -3,11 +3,13 @@ import * as React from 'react'
 import {View} from 'react-native'
 import type {ViewStyleProp} from 'react-native/Libraries/StyleSheet/StyleSheet'
 
-export type PropsType = {
-	children?: React.Node,
-	style?: ViewStyleProp,
-	props: ViewStyleProp,
-}
+export type PropsType = any
+// export type PropsType = {
+// 	children?: React.Node,
+// 	style?: ViewStyleProp,
+// 	props: ViewStyleProp,
+// }
+
 export const StyledComponent = ({children, style, ...props}: PropsType) => {
-	return <View style={[(props: any), style]}>{children}</View>
+	return <View style={[props, style]}>{children}</View>
 }
