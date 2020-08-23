704ba5948dc0b28bf0b7cd8654c00699b5ca92bf
Kristofer Rye <kristofer.rye@gmail.com>
2020-08-23 15:41:04 -0500

    stav-covid: Parse each table's prior h3-sib
    We build a regular expression to match on text that looks like a date,
    then take the first match of that, parse it using strptime, and try to
    bring that into the current year in a smart way.  (First, start with the
    date we parsed, then replace the year component of that with the current
    year, and then add a year if that happens to be before the current date
    by more than 180 days.)
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- scripts/stav-covid.py
+++ scripts/stav-covid.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
+import re
 
 # Fetch the output to a string, `output`.
 url = "https://wp.stolaf.edu/reslife/dining-hours/"
@@ -12,9 +14,35 @@ output = BeautifulSoup(output)
 # so please double-check the output of this script.
 tables = output.find_all("table")
 
+# A regex to match strings like "Sunday, August 23"
+date_re = re.compile('\w+, \w+ \d{1,2}')
+
 # Start with an empty schedule set
 schedules = {}
 
 # For each of the HTML tables we found,
 for table in tables:
 	print("Processing a table...")
+
+	# Find the prior sibling, which is an h3 containing the day in some format.
+	# Each table should have an h3 before it that has date information in it.
+	header = table.find_previous_sibling("h3").get_text().strip()
+	print('Found a header sibling with text "{}"'.format(header))
+
+	# Search the text for just the date
+	matching_text = date_re.match(header).group(0)
+
+	# NOTE(rye): Parses text like "Sunday, August 23"
+	date = datetime.datetime.strptime(matching_text, "%A, %B %d")
+
+	# Move the date to the current year
+	date = datetime.datetime(date.today().year, date.month, date.day)
+
+	# If the computed date is more than six months before today, assume it actually belongs in the next year.
+	if date < date.today() + datetime.timedelta(days=-180):
+		date = datetime.datetime(date.year + 1, date.month, date.day)
+
+	# We only know the date, so let's back out to that.
+	date = date.date()
+
+	print("Inferred date of {}; processing table now.".format(date))
