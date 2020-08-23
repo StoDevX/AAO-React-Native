89b9b65631d265a68254ba022c67c8f03ac2ea24
Kristofer Rye <kristofer.rye@gmail.com>
2020-08-23 15:39:48 -0500

    stav-covid: Clean up imports slightly
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- scripts/stav-covid.py
+++ scripts/stav-covid.py
@@ -1,7 +1,4 @@
-# For loading HTTP data, just use requests...
 import requests
-
-# ...and BeautifulSoup for parsing and getting data out.
 from bs4 import BeautifulSoup
 
 # Fetch the output to a string, `output`.
