a9dc6690ae543592d07e4fc0ada5aec13412f49b
Kristofer Rye <kristofer.rye@gmail.com>
2020-08-23 15:38:56 -0500

    stav-covid: Loop over the tables
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- scripts/stav-covid.py
+++ scripts/stav-covid.py
@@ -10,3 +10,14 @@ output = requests.get(url).text
 
 # Now, parse it.
 output = BeautifulSoup(output)
+
+# NOTE(rye): There is no filtering to prevent loading tables that aren't valid,
+# so please double-check the output of this script.
+tables = output.find_all("table")
+
+# Start with an empty schedule set
+schedules = {}
+
+# For each of the HTML tables we found,
+for table in tables:
+	print("Processing a table...")
