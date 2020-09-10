92a5976c0eb19ee243b988a9d66a2169b542ce7b
Kristofer Rye <kristofer.rye@gmail.com>
2020-09-09 19:36:59 -0500

    Delete flow-typed type definitions
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- flow-typed/npm/@frogpond/titlecase_vx.x.x.js
+++
@@ -1,8 +0,0 @@
-// flow-typed signature: 43ebef9724d6353c509b34d53104aa78
-// flow-typed version: <<STUB>>/@frogpond/titlecase_v1.0.0/flow_v0.105.2
-
-declare module '@frogpond/titlecase' {
-  declare export function toLaxTitleCase(str: string): string;
-  declare export function toTitleCase(str: string): string;
-  declare export default typeof toTitleCase;
-}
