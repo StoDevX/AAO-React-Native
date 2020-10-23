a957475942f593c53a1e08bce8bea3a08659d9ec
Kristofer Rye <kristofer.rye@gmail.com>
2020-10-23 18:44:20 -0500

    l/stoprint/errors: Change return type of onDismiss to match Alert param
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- source/lib/stoprint/errors.ts
+++ source/lib/stoprint/errors.ts
@@ -1,6 +1,6 @@
 import {Alert} from 'react-native'
 
-export const showGeneralError = (onDismiss: () => any): void => {
+export const showGeneralError = (onDismiss: () => void): void => {
 	Alert.alert(
 		'An unexpected error occurred',
 		"We're sorry, but we have lost communication with Papercut. Please try again.",
