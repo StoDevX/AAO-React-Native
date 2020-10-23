9825b786ee83d5fa9b0e5d49f3e1565fa68dc8c4
Kristofer Rye <kristofer.rye@gmail.com>
2020-10-23 18:44:20 -0500

    l/stoprint: Add return types on boundary functions
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- source/lib/stoprint/errors.ts
+++ source/lib/stoprint/errors.ts
@@ -1,6 +1,6 @@
 import {Alert} from 'react-native'
 
-export const showGeneralError = (onDismiss: () => any) => {
+export const showGeneralError = (onDismiss: () => any): void => {
 	Alert.alert(
 		'An unexpected error occurred',
 		"We're sorry, but we have lost communication with Papercut. Please try again.",
