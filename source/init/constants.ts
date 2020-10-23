48a145e12d9fc9bf36ad7e0f1f5a1769691c6762
Kristofer Rye <kristofer.rye@gmail.com>
2020-10-23 18:44:16 -0500

    i/constants: Fix import of version and name from package.json
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- source/init/constants.ts
+++ source/init/constants.ts
@@ -3,7 +3,7 @@ import {setVersionInfo, setAppName, setTimezone} from '@frogpond/constants'
 export const SENTRY_DSN =
 	'https://6f70285364b7417181e17db8bcf4de11@sentry.frogpond.tech/2'
 
-const {version, name} = require('../../package.json')
+import {version, name} from '../../package.json'
 
 setVersionInfo(version)
 setAppName(name)
