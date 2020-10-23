27b54b48f6eb44d867194064affb90a99db297a9
Kristofer Rye <kristofer.rye@gmail.com>
2020-10-23 18:44:29 -0500

    m/api: Convert to typescript
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- modules/api/index.ts
+++ modules/api/index.ts
@@ -4,11 +4,11 @@ import {IS_PRODUCTION} from '@frogpond/constants'
 
 let root: string
 
-export function setApiRoot(url: string) {
+export function setApiRoot(url: string): void {
 	root = url
 }
 
-export const API = (path: string, query: ?unknown = null) => {
+export const API = (path: string, query: unknown = null): string => {
 	if (!IS_PRODUCTION) {
 		if (!path.startsWith('/')) {
 			throw new Error('invalid path requested from the api!')
