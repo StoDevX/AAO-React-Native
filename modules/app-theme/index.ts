db14ced0229c73c8ac622cf11c025bd474933948
Kristofer Rye <kristofer.rye@gmail.com>
2020-10-23 20:38:47 -0500

    m/app-theme: Reformat
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- modules/app-theme/index.ts
+++ modules/app-theme/index.ts
@@ -1,31 +1,33 @@
 import {createTheming} from '@callstack/react-theme-provider'
 import type {ThemingType} from '@callstack/react-theme-provider'
 
-let {ThemeProvider, withTheme, useTheme} = createTheming() as ThemingType<AppTheme | undefined>
+let {ThemeProvider, withTheme, useTheme} = createTheming() as ThemingType<
+	AppTheme | undefined
+>
 
 export {ThemeProvider, withTheme, useTheme}
 
 export type AppTheme = {
-	accent: string,
-	androidListHeaderBackground: string,
-	androidListHeaderForeground: string,
-	androidStatusBarColor: string,
-	androidTabBarBackground: string,
-	androidTabBarForeground: string,
-	buttonBackground: string,
-	buttonForeground: string,
-	iosPushButtonCellBackground: string,
-	iosPushButtonCellForeground: string,
-	iosTabBarActiveColor: string,
-	iosTabBarBackground: string,
-	navigationBackground: string,
-	navigationForeground: string,
-	statusBarStyle: 'dark-content' | 'light-content',
-	switchThumbTint?: string,
-	switchTintOff?: string,
-	switchTintOn?: string,
-	toolbarButtonBackground: string,
-	toolbarButtonForeground: string,
+	accent: string
+	androidListHeaderBackground: string
+	androidListHeaderForeground: string
+	androidStatusBarColor: string
+	androidTabBarBackground: string
+	androidTabBarForeground: string
+	buttonBackground: string
+	buttonForeground: string
+	iosPushButtonCellBackground: string
+	iosPushButtonCellForeground: string
+	iosTabBarActiveColor: string
+	iosTabBarBackground: string
+	navigationBackground: string
+	navigationForeground: string
+	statusBarStyle: 'dark-content' | 'light-content'
+	switchThumbTint?: string
+	switchTintOff?: string
+	switchTintOn?: string
+	toolbarButtonBackground: string
+	toolbarButtonForeground: string
 }
 
 let theme: AppTheme
