53977b38d79b746adc53c3c0bb2b163db1d23bcc
Drew Volz <volz.drew@gmail.com>
2020-11-18 11:54:16 -0700

    Remove eslint directive

--- source/init/navigation.js
+++ source/init/navigation.js
@@ -2,8 +2,4 @@
 
 import {enableScreens} from 'react-native-screens'
 
-// eslint-plugin-react-hooks occasionally has a false-positive;
-// this method, while named use*, is not a hook.
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
 enableScreens()
