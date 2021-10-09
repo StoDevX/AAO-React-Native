af29b7f3a9d38bba784884ff47a690de478a5332
Drew Volz <volz.drew@gmail.com>
2021-10-09 15:42:08 -0700

    Fix onPress return type for custom cell

--- source/views/directory/detail.ios.tsx
+++ source/views/directory/detail.ios.tsx
@@ -59,7 +59,7 @@ export function DirectoryDetailView(props: Props): JSX.Element {
 								accessory={officeHours.href ? 'DisclosureIndicator' : undefined}
 								detail={officeHours.title}
 								onPress={
-									officeHours.href ? () => openUrl(officeHours.href) : null
+									officeHours.href ? () => openUrl(officeHours.href) : undefined
 								}
 								title={officeHours.description}
 							/>
