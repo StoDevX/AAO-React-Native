3c49812950d1a2637709cc055fc5aae200a4ad4f
Hawken Rives <hawkrives@fastmail.fm>
2021-10-28 06:39:59 -0400

    remove commented-out imports

--- images/news-sources/index.ts
+++ images/news-sources/index.ts
@@ -1,6 +1,1 @@
-// import mess from './mess.png'
-// import oleville from './oleville.png'
-// import politicole from './politicole.png'
-// import stolaf from './stolaf.png'
-
 export {ksto} from '../streaming'
