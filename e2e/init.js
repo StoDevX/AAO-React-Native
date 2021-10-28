acbf64a72a150ec4165d689cafe887f9481ba9fb
Hawken Rives <hawkrives@fastmail.fm>
2021-10-28 12:36:46 -0400

    format init.js

--- e2e/init.js
+++ e2e/init.js
@@ -1,10 +1,13 @@
 /* eslint-env jest */
 /* global device */
 
-import { init, cleanup } from 'detox'
-import { getEnv } from 'jest-jasmine2'
-import { detox as config } from '../package.json'
-import adapter, { beforeEach as beforeEachDetox, afterAll as afterAllDetox } from 'detox/runners/jest/adapter'
+import {init, cleanup} from 'detox'
+import {getEnv} from 'jest-jasmine2'
+import {detox as config} from '../package.json'
+import adapter, {
+	beforeEach as beforeEachDetox,
+	afterAll as afterAllDetox,
+} from 'detox/runners/jest/adapter'
 
 jest.setTimeout(120000)
 
