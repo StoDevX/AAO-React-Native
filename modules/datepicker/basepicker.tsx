77e9d7ced08faf8c0911dafcce97c5bf8df9ebd2
Drew Volz <volz.drew@gmail.com>
2021-10-31 14:27:35 -0700

    Split datetime picker into separate platform-specific initializers

---
+++ modules/datepicker/basepicker.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react'
+import {Button} from 'react-native'
+
+import {useTheme} from '@frogpond/app-theme'
+import type {AppTheme} from '@frogpond/app-theme'
+
+import moment from 'moment-timezone'
+import type {Moment} from 'moment-timezone'
+
+import DateTimePicker from '@react-native-community/datetimepicker'
+import type {Event} from '@react-native-community/datetimepicker'
+
+import {BaseDatetimePickerProps} from './types'
+
+const FORMATS = {
+	date: 'YYYY-MM-DD',
+	datetime: 'YYYY-MM-DD HH:mm',
+	time: 'HH:mm',
+}
+
+export const BaseDateTimePicker = (
+	props: BaseDatetimePickerProps,
+): JSX.Element => {
+	let [date, setDate] = React.useState(props.initialDate)
+	let [timezone] = React.useState(props.initialDate.tz() || '')
+
+	const onChange = (_event: Event, selectedDate?: Date | undefined) => {
+		// Reset Android's modal's presenting state
+		props.onChange?.()
+
+		if (!selectedDate) {
+			return
+		}
+
+		setDate(moment(selectedDate))
+		props.onDateChange(moment.tz(selectedDate, timezone))
+	}
+
+	const formatDate = (date: Moment): string => {
+		const {mode, format = FORMATS[mode]} = props
+		return moment.tz(date, timezone).format(format)
+	}
+
+	const getTimezoneOffsetInMinutes = (): number => {
+		// We need to negate the offset because moment inverts the offset
+		// for POSIX compatability. So, GMT-5 (CST) is shown to be GMT+5.
+		//
+		// We also need to make the types happy when we negate a negative
+		// MomentZone which is possibly null.
+		let dateInUnixMs = date.valueOf()
+		let momentZone = moment.tz.zone(timezone)
+
+		let offset = momentZone ? -momentZone.utcOffset(dateInUnixMs) : 0
+		return offset
+	}
+
+	let theme: AppTheme = useTheme()
+
+	return (
+		<>
+			{props.showPickerButtonAndroid && (
+				<Button
+					color={theme.buttonBackground}
+					onPress={() => props.setShowPickerAndroid?.(true)}
+					title={formatDate(date)}
+				/>
+			)}
+
+			{(props.showPickerAndroid || props.showPickeriOS) && (
+				<DateTimePicker
+					minuteInterval={props.minuteInterval}
+					mode={props.mode}
+					onChange={onChange}
+					style={props.style}
+					testID="buildingHoursTimePicker"
+					timeZoneOffsetInMinutes={getTimezoneOffsetInMinutes()}
+					value={moment.tz(date, timezone).toDate()}
+				/>
+			)}
+		</>
+	)
+}
--- modules/datepicker/types.ts
+++ modules/datepicker/types.ts
@@ -1,1 +1,34 @@
-export type StyleSheetRules = any | number | false | Array<StyleSheetRules>
+import type {Moment} from 'moment-timezone'
+import type {
+	AndroidNativeProps,
+	IOSNativeProps,
+} from '@react-native-community/datetimepicker'
+
+export interface BaseDatetimePickerProps {
+	// dep-specific
+	initialDate: Moment
+	minuteInterval?: 1 | 2 | 3 | 4 | 5 | 6 | 10 | 12 | 15 | 20 | 30
+	mode: IOSNativeProps['mode'] | AndroidNativeProps['mode']
+	format?: string
+	onDateChange: (moment: Moment) => void
+
+	// platform/wrapper components
+	showPickeriOS?: boolean
+	showPickerAndroid?: boolean
+	showPickerButtonAndroid?: boolean
+	style?: any
+	setShowPickerAndroid?: React.Dispatch<React.SetStateAction<boolean>>
+	onChange?: () => void
+}
+
+interface IOSProps {
+	mode: IOSNativeProps['mode']
+}
+
+interface AndroidProps {
+	mode: AndroidNativeProps['mode']
+}
+
+export type IosDatetimePickerProps = BaseDatetimePickerProps & IOSProps
+
+export type AndroidDatetimePickerProps = BaseDatetimePickerProps & AndroidProps
