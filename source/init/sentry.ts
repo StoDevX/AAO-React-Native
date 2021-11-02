1e5b249bf07836e2e05421c5ff7fa38ed5506533
Hawken Rives <hawkrives@fastmail.fm>
2021-11-02 21:53:20 +0000

    add /u flag for regex

--- source/init/sentry.ts
+++ source/init/sentry.ts
@@ -14,7 +14,7 @@ function install() {
 
 		integrations: [
 			new Sentry.ReactNativeTracing({
-				tracingOrigins: ['localhost', 'my-site-url.com', /^\//],
+				tracingOrigins: ['localhost', 'my-site-url.com', /^\//u],
 			}),
 		],
 	})
