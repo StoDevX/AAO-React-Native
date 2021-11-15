95539fb4aa7009528d287ccb4f09108cc439dc6b
Kristofer Rye <kristofer.rye@gmail.com>
2021-11-15 06:07:06 -0600

    m/layout: Fix one type error
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- modules/layout/styled-component.tsx
+++ modules/layout/styled-component.tsx
@@ -3,10 +3,11 @@ import {View} from 'react-native'
 import type {ViewStyle} from 'react-native'
 
 export type PropsType = {
-	children?: React.Node
+	children?: React.ReactNode
 	style?: ViewStyle
 	props: ViewStyle
 }
+
 export const StyledComponent = ({children, style, ...props}: PropsType) => {
 	return <View style={[props, style]}>{children}</View>
 }
