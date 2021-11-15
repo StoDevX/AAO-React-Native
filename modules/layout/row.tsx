8759a428a9766c3f6cf5912c3d320df507ec48f3
Drew Volz <volz.drew@gmail.com>
2021-11-15 18:58:26 +0000

    m/layout: Add return type

--- modules/layout/row.tsx
+++ modules/layout/row.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import {StyledComponent} from './styled-component'
 import type {PropsType} from './styled-component'
 
-export const Row = ({children, ...props}: PropsType) => {
+export const Row = ({children, ...props}: PropsType): JSX.Element => {
 	return (
 		<StyledComponent flexDirection="row" {...props}>
 			{children}
