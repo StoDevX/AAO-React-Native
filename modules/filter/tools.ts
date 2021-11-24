6a966f76b46aeffcaba62fcad9432166614e7aaf
Kristofer Rye <kristofer.rye@gmail.com>
2021-11-23 20:35:51 -0600

    m/filter/tools: Explicitly map Filter types over
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- modules/filter/tools.ts
+++ modules/filter/tools.ts
@@ -1,16 +1,22 @@
 import type {FilterType, ListType, PickerType, ToggleType} from './types'
 
 export function filterListSpecs(specs: Array<FilterType>): Array<ListType> {
-	let retval = specs.filter((f) => f.type === 'list')
+	let retval = specs
+		.filter((f) => f.type === 'list')
+		.map((f: FilterType) => f as ListType)
 	return retval
 }
 
 export function filterPickerSpecs(specs: Array<FilterType>): Array<PickerType> {
-	let retval = specs.filter((f) => f.type === 'picker')
+	let retval = specs
+		.filter((f: FilterType) => f.type === 'picker')
+		.map((f: FilterType) => f as PickerType)
 	return retval
 }
 
 export function filterToggleSpecs(specs: Array<FilterType>): Array<ToggleType> {
-	let retval = specs.filter((f) => f.type === 'toggle')
+	let retval = specs
+		.filter((f) => f.type === 'toggle')
+		.map((f) => f as ToggleType)
 	return retval
 }
