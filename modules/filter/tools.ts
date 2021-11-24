5b211e615412e3ad70dad4a21254d733dd9750e7
Kristofer Rye <kristofer.rye@gmail.com>
2021-11-23 20:37:07 -0600

    m/fetch/tools: Actually, tell TypeScript what the return type is
    TypeScript isn't aware that we are actually correctly filtering down to
    items that correctly match up, so tell TypeScript what we're doing here.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- modules/filter/tools.ts
+++ modules/filter/tools.ts
@@ -1,22 +1,16 @@
 import type {FilterType, ListType, PickerType, ToggleType} from './types'
 
 export function filterListSpecs(specs: Array<FilterType>): Array<ListType> {
-	let retval = specs
-		.filter((f) => f.type === 'list')
-		.map((f: FilterType) => f as ListType)
-	return retval
+	let retval = specs.filter((f) => f.type === 'list')
+	return retval as Array<ListType>
 }
 
 export function filterPickerSpecs(specs: Array<FilterType>): Array<PickerType> {
-	let retval = specs
-		.filter((f: FilterType) => f.type === 'picker')
-		.map((f: FilterType) => f as PickerType)
-	return retval
+	let retval = specs.filter((f: FilterType) => f.type === 'picker')
+	return retval as Array<PickerType>
 }
 
 export function filterToggleSpecs(specs: Array<FilterType>): Array<ToggleType> {
-	let retval = specs
-		.filter((f) => f.type === 'toggle')
-		.map((f) => f as ToggleType)
-	return retval
+	let retval = specs.filter((f) => f.type === 'toggle')
+	return retval as Array<ToggleType>
 }
