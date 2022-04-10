7c62e4030ec6e5b05273d148f5d0159f14f40bfc
Drew Volz <volz.drew@gmail.com>
2022-04-10 10:15:56 -0700

    prettify not recently changed files

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,5 @@ module.exports = {
 		'module:metro-react-native-babel-preset',
 		'@babel/preset-typescript',
 	],
-	plugins: [
-		'@babel/plugin-proposal-export-namespace-from'
-	]
+	plugins: ['@babel/plugin-proposal-export-namespace-from'],
 }
