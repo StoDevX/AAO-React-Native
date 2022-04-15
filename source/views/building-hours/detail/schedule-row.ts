e4d3e929a769761a65194a30947a529a8c891905
Hawken Rives <hawkrives@fastmail.fm>
2022-04-15 00:19:32 -0400

    refactor schedule-table and schedule-row and link-table

---
+++ source/views/building-hours/detail/schedule-row.ts
@@ -0,0 +1,6 @@
+import {Platform} from 'react-native'
+
+import {ScheduleRow as ScheduleRowIos} from './schedule-row-ios'
+import {ScheduleRow as ScheduleRowAndroid} from './schedule-row-android'
+
+export const ScheduleRow = Platform.OS === 'ios' ? ScheduleRowIos : ScheduleRowAndroid;
---
+++ source/views/building-hours/detail/schedule-table.ts
@@ -0,0 +1,6 @@
+import {Platform} from 'react-native'
+
+import {ScheduleTable as ScheduleTableIos} from './schedule-table-ios'
+import {ScheduleTable as ScheduleTableAndroid} from './schedule-table-android'
+
+export const ScheduleTable = Platform.OS === 'ios' ? ScheduleTableIos : ScheduleTableAndroid;
