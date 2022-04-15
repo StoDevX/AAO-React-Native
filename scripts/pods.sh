f994c0aadd441bd3cab5ecc31be54f63608b744b
Hawken Rives <hawkrives@fastmail.fm>
2022-04-15 11:37:04 -0400

    add --repo-update

--- scripts/pods.sh
+++ scripts/pods.sh
@@ -7,7 +7,7 @@ USE_PODS=${FP_PODS:-yes}
 if [[ $USE_PODS = 'yes' ]]; then
 	if [[ $(uname) = 'Darwin' ]]; then
 		bundle install
-		cd ios && bundle exec pod install
+		cd ios && bundle exec pod install --repo-update
 
 		if [[ $? != 0 ]]; then
 			echo 'try running `bundle exec pod repo update`' 1>&2
