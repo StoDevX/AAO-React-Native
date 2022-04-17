488e9258dbe10a08c624c1ec96eff04afa77962c
Hawken Rives <hawkrives@fastmail.fm>
2022-04-17 11:11:28 -0400

    clean up some Theme type issues in Radio

--- source/views/streaming/radio/buttons.tsx
+++ source/views/streaming/radio/buttons.tsx
@@ -1,28 +1,20 @@
 import * as React from 'react'
-import {StyleSheet, Text, View, Platform} from 'react-native'
+import {Platform, StyleSheet, Text, View} from 'react-native'
 import * as c from '@frogpond/colors'
-import {Icon} from '@frogpond/icon'
 import type {Glyphs} from '@frogpond/icon'
+import {Icon} from '@frogpond/icon'
 import {Touchable} from '@frogpond/touchable'
-import {withTheme} from '@frogpond/app-theme'
-import type {PlayerTheme} from './types'
+import {useTheme} from './theme'
 
-type ActionButtonBaseProps = {
+type ActionButtonProps = {
 	icon: Glyphs
 	text: string
 	onPress: () => unknown
 }
 
-type HocProps = {
-	theme: PlayerTheme
-}
-
-type ActionButtonProps = ActionButtonBaseProps & HocProps
-
-const ActionButton: React.FunctionComponent<ActionButtonProps> = (
-	props: ActionButtonProps,
-) => {
-	let {icon, text, onPress, theme} = props
+export function ActionButton(props: ActionButtonProps): JSX.Element {
+	let theme = useTheme()
+	let {icon, text, onPress} = props
 	let bg = {backgroundColor: theme.tintColor}
 	let fg = {color: theme.buttonTextColor}
 	let style = [styles.button, styles.largeButton, bg]
@@ -37,34 +29,39 @@ const ActionButton: React.FunctionComponent<ActionButtonProps> = (
 	)
 }
 
-const ThemedActionButton: React.FunctionComponent<ActionButtonBaseProps> =
-	withTheme(ActionButton)
-
-export {ThemedActionButton as ActionButton}
-
-export const CallButton = ({onPress}: {onPress: () => unknown}) => (
-	<ThemedSmallActionButton
-		icon={Platform.OS === 'ios' ? 'ios-call' : 'md-call'}
-		onPress={onPress}
-	/>
-)
-
-export const ShowCalendarButton = ({onPress}: {onPress: () => unknown}) => (
-	<ThemedSmallActionButton
-		icon={Platform.OS === 'ios' ? 'ios-calendar' : 'md-calendar'}
-		onPress={onPress}
-	/>
-)
-
-type SmallActionButtonBaseProps = {
-	icon: Glyphs
+type CallButtonProps = {
 	onPress: () => unknown
 }
 
-type SmallActionButtonProps = ActionButtonBaseProps & HocProps
+export function CallButton({onPress}: CallButtonProps): JSX.Element {
+	return (
+		<SmallActionButton
+			icon={Platform.OS === 'ios' ? 'ios-call' : 'md-call'}
+			onPress={onPress}
+		/>
+	)
+}
 
-const SmallActionButton = (props: SmallActionButtonProps) => {
-	let {icon, onPress, theme} = props
+type ShowCalendarButtonProps = {
+	onPress: () => unknown
+}
+
+export function ShowCalendarButton({
+	onPress,
+}: ShowCalendarButtonProps): JSX.Element {
+	return (
+		<SmallActionButton
+			icon={Platform.OS === 'ios' ? 'ios-calendar' : 'md-calendar'}
+			onPress={onPress}
+		/>
+	)
+}
+
+type SmallActionButtonProps = Omit<ActionButtonProps, 'text'>
+
+export function SmallActionButton(props: SmallActionButtonProps): JSX.Element {
+	let theme = useTheme()
+	let {icon, onPress} = props
 	let bg = {backgroundColor: theme.tintColor}
 	let fg = {color: theme.buttonTextColor}
 	let style = [styles.button, styles.smallButton, bg]
@@ -75,9 +72,6 @@ const SmallActionButton = (props: SmallActionButtonProps) => {
 		</Touchable>
 	)
 }
-
-const ThemedSmallActionButton: React.FunctionComponent<SmallActionButtonBaseProps> =
-	withTheme(SmallActionButton)
 
 const styles = StyleSheet.create({
 	button: {
