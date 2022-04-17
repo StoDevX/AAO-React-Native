dd1892866a86af5561711f38ee3a34f6b5ce4df0
Hawken Rives <hawkrives@fastmail.fm>
2022-04-17 18:07:50 -0400

    convert CarletonMenus to a function component

--- source/views/menus/carleton-menus.tsx
+++ source/views/menus/carleton-menus.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {TabBarIcon} from '@frogpond/navigation-tabs'
-import {View, Platform, ScrollView, StyleSheet} from 'react-native'
+import {Platform, ScrollView, StyleSheet, View} from 'react-native'
 import type {TopLevelViewPropsType} from '../types'
 import {Row} from '@frogpond/layout'
 import {ListRow, ListSeparator, Title} from '@frogpond/lists'
@@ -66,37 +66,33 @@ CarletonSaylesMenuScreen.navigationOptions = {
 
 type Props = TopLevelViewPropsType
 
-export class CarletonCafeIndex extends React.Component<Props> {
-	render() {
-		let carletonCafes = [
-			{id: 'CarletonBurtonMenuView', title: 'Burton'},
-			{id: 'CarletonLDCMenuView', title: 'LDC'},
-			{id: 'CarletonWeitzMenuView', title: 'Weitz Center'},
-			{id: 'CarletonSaylesMenuView', title: 'Sayles Hill'},
-		]
+export function CarletonCafeIndex(props: Props) {
+	let carletonCafes = [
+		{id: 'CarletonBurtonMenuView', title: 'Burton'},
+		{id: 'CarletonLDCMenuView', title: 'LDC'},
+		{id: 'CarletonWeitzMenuView', title: 'Weitz Center'},
+		{id: 'CarletonSaylesMenuView', title: 'Sayles Hill'},
+	]
 
-		return (
-			<ScrollView style={styles.container}>
-				{carletonCafes.map(
-					(loc: {id: string; title: string}, i, collection) => (
-						<View key={i}>
-							<ListRow
-								arrowPosition="center"
-								onPress={() => this.props.navigation.navigate(loc.id)}
-							>
-								<Row alignItems="center">
-									<Title style={styles.rowText}>{loc.title}</Title>
-								</Row>
-							</ListRow>
-							{i < collection.length - 1 ? (
-								<ListSeparator spacing={{left: 15}} />
-							) : null}
-						</View>
-					),
-				)}
-			</ScrollView>
-		)
-	}
+	return (
+		<ScrollView style={styles.container}>
+			{carletonCafes.map((loc: {id: string; title: string}, i, collection) => (
+				<View key={i}>
+					<ListRow
+						arrowPosition="center"
+						onPress={() => props.navigation.navigate(loc.id)}
+					>
+						<Row alignItems="center">
+							<Title style={styles.rowText}>{loc.title}</Title>
+						</Row>
+					</ListRow>
+					{i < collection.length - 1 ? (
+						<ListSeparator spacing={{left: 15}} />
+					) : null}
+				</View>
+			))}
+		</ScrollView>
+	)
 }
 
 const styles = StyleSheet.create({
