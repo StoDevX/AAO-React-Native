fcfd35bf3c7cbbc54c3a3eade9edf607e4b9513c
Hawken Rives <hawkrives@fastmail.fm>
2022-04-19 17:55:59 -0400

    refactor FancyMenu into a function component, and simplify the data flow

--- modules/food-menu/fancy-menu.tsx
+++ modules/food-menu/fancy-menu.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import {useEffect, useState} from 'react'
 import {SectionList, StyleSheet} from 'react-native'
 import * as c from '@frogpond/colors'
 import type {
@@ -24,6 +24,8 @@ import {chooseMeal} from './lib/choose-meal'
 import {buildFilters} from './lib/build-filters'
 import type {Moment} from 'moment'
 
+type FilterFunc = (filters: Array<FilterType>, item: MenuItemType) => boolean
+
 type ReactProps = {
 	cafeMessage?: string
 	foodItems: MenuItemContainerType
@@ -33,21 +35,14 @@ type ReactProps = {
 	now: Moment
 	onRefresh?: () => void
 	refreshing?: boolean
+	applyFilters: FilterFunc
 }
 
-type FilterFunc = (filters: Array<FilterType>, item: MenuItemType) => boolean
-
 type DefaultProps = {
-	applyFilters: FilterFunc
 	navigation: NavigationScreenProp<NavigationRoute, NavigationAction>
 }
 
 type Props = ReactProps & DefaultProps
-
-type State = {
-	filters: Array<FilterType>
-	cachedFoodItems?: MenuItemContainerType
-}
 
 const styles = StyleSheet.create({
 	inner: {
@@ -64,161 +59,137 @@ const styles = StyleSheet.create({
 const LEFT_MARGIN = 28
 const Separator = () => <ListSeparator spacing={{left: LEFT_MARGIN}} />
 
-export class FancyMenu extends React.Component<Props, State> {
-	static defaultProps = {
-		applyFilters: applyFiltersToItem,
+const areSpecialsFiltered = (filters: Array<FilterType>): boolean =>
+	Boolean(filters.find(isSpecialsFilter))
+
+const isSpecialsFilter = (f: FilterType): boolean =>
+	f.enabled && f.type === 'toggle' && f.spec.label === 'Only Show Specials'
+
+const groupMenuData = (args: {
+	filters: Array<FilterType>
+	stations: Array<StationMenuType>
+	foodItems: MenuItemContainerType
+	applyFilters: FilterFunc
+}): {title: string; data: Array<MenuItemType>}[] => {
+	const {applyFilters, foodItems, stations, filters} = args
+
+	const dereferenceMenuItems = (menu: StationMenuType) =>
+		menu.items
+			// Dereference each menu item
+			.map((id) => foodItems[id])
+			// Ensure that the referenced menu items exist,
+			// and apply the selected filters to the items in the menu
+			.filter((item) => item && applyFilters(filters, item))
+
+	const stationMenusByLabel: [string, MenuItemType[]][] = stations.map(
+		(menu: StationMenuType) => [menu.label, dereferenceMenuItems(menu)],
+	)
+
+	return stationMenusByLabel
+		.filter(([_, items]) => items.length)
+		.map(([title, data]) => ({title, data}))
+}
+
+export function FancyMenu(props: Props): JSX.Element {
+	const {now, meals, cafeMessage, foodItems, menuCorIcons} = props
+	const navigate = props.navigation.navigate
+	const applyFilters = props.applyFilters ?? applyFiltersToItem
+
+	const [filters, setFilters] = useState<FilterType[]>([])
+
+	const meal = chooseMeal(meals, filters, now)
+	const {label: mealName, stations} = meal
+	const stationsByLabel = new Map(
+		stations.map((station) => [station.label, station]),
+	)
+
+	const [groupedMenuData, setGroupedMenuData] = useState<
+		{title: string; data: Array<MenuItemType>}[]
+	>([])
+
+	const anyFiltersEnabled = filters.some((f) => f.enabled)
+	const specialsFilterEnabled = areSpecialsFiltered(filters)
+
+	// reset the filters when the data changes
+	useEffect(() => {
+		let foodItemsArray = Object.values(foodItems)
+		setFilters(buildFilters(foodItemsArray, menuCorIcons, [meal]))
+	}, [foodItems, menuCorIcons, meal])
+
+	// re-group the food when the data changes
+	useEffect(() => {
+		let grouped = groupMenuData({stations, filters, applyFilters, foodItems})
+		setGroupedMenuData(grouped)
+	}, [applyFilters, filters, foodItems, stations])
+
+	let message = 'No items to show.'
+	if (cafeMessage) {
+		message = cafeMessage
+	} else if (specialsFilterEnabled && stations.length === 0) {
+		message =
+			'No items to show. There may be no specials today. Try changing the filters.'
+	} else if (anyFiltersEnabled && !size(groupedMenuData)) {
+		message = 'No items to show. Try changing the filters.'
 	}
 
-	state: State = {
-		filters: [],
-		cachedFoodItems: undefined,
-	}
+	const messageView = <NoticeView style={styles.message} text={message} />
 
-	static getDerivedStateFromProps(
-		props: Props,
-		prevState: State,
-	): State | null {
-		// we only need to do this when the menu has changed; this avoids
-		// us overriding our changes from FilterView.onDismiss
-		if (
-			!prevState.cachedFoodItems ||
-			props.foodItems !== prevState.cachedFoodItems
-		) {
-			const {foodItems, menuCorIcons, meals, now} = props
-			const filters =
-				prevState.filters.length !== 0
-					? prevState.filters
-					: buildFilters(Object.values(foodItems), menuCorIcons, meals, now)
-			return {filters, cachedFoodItems: props.foodItems}
-		}
-		return null
-	}
+	// If the requested menu has no food items, that location is closed
+	const isOpen = Object.keys(foodItems).length !== 0
 
-	areSpecialsFiltered = (filters: Array<FilterType>): boolean =>
-		Boolean(filters.find(this.isSpecialsFilter))
+	const header = (
+		<FilterToolbar
+			date={now}
+			filters={filters}
+			isOpen={isOpen}
+			onPopoverDismiss={(newFilter) => {
+				let edited = filters.map((f) =>
+					f.key === newFilter.key ? newFilter : f,
+				)
+				setFilters(edited)
+			}}
+			title={mealName}
+		/>
+	)
 
-	isSpecialsFilter = (f: FilterType): boolean =>
-		f.enabled && f.type === 'toggle' && f.spec.label === 'Only Show Specials'
+	return (
+		<SectionList
+			ItemSeparatorComponent={Separator}
+			ListEmptyComponent={messageView}
+			ListHeaderComponent={header}
+			contentContainerStyle={styles.contentContainer}
+			extraData={filters}
+			keyExtractor={(item: MenuItemType) => item.id}
+			onRefresh={props.onRefresh}
+			refreshing={props.refreshing}
+			renderItem={({item}) => {
+				return (
+					<FoodItemRow
+						badgeSpecials={!specialsFilterEnabled}
+						corIcons={menuCorIcons}
+						data={item}
+						onPress={() =>
+							navigate('MenuItemDetailView', {item, icons: menuCorIcons})
+						}
+						spacing={{left: LEFT_MARGIN}}
+					/>
+				)
+			}}
+			renderSectionHeader={(info) => {
+				const title = info.section.title
+				const note = stationsByLabel.get(title)?.note ?? ''
 
-	updateFilter = (filter: FilterType): void => {
-		this.setState((state) => {
-			const edited = state.filters.map((f) =>
-				f.key !== filter.key ? f : filter,
-			)
-			return {filters: edited}
-		})
-	}
-
-	groupMenuData = (args: {
-		filters: Array<FilterType>
-		stations: Array<StationMenuType>
-		foodItems: MenuItemContainerType
-		applyFilters: FilterFunc
-	}): {title: string; data: Array<MenuItemType>}[] => {
-		const {applyFilters, foodItems, stations, filters} = args
-
-		const dereferenceMenuItems = (menu: StationMenuType) =>
-			menu.items
-				// Dereference each menu item
-				.map((id) => foodItems[id])
-				// Ensure that the referenced menu items exist,
-				// and apply the selected filters to the items in the menu
-				.filter((item) => item && applyFilters(filters, item))
-
-		const stationMenusByLabel: [string, MenuItemType[]][] = stations.map(
-			(menu: StationMenuType) => [menu.label, dereferenceMenuItems(menu)],
-		)
-
-		return stationMenusByLabel
-			.filter(([_, items]) => items.length)
-			.map(([title, data]) => ({title, data}))
-	}
-
-	render(): JSX.Element {
-		const {now, meals, cafeMessage, applyFilters, foodItems} = this.props
-		const {filters} = this.state
-
-		const {label: mealName, stations} = chooseMeal(meals, filters, now)
-		const anyFiltersEnabled = filters.some((f) => f.enabled)
-		const specialsFilterEnabled = this.areSpecialsFiltered(filters)
-		const groupedMenuData = this.groupMenuData({
-			stations,
-			filters,
-			applyFilters,
-			foodItems,
-		})
-
-		let message = 'No items to show.'
-		if (cafeMessage) {
-			message = cafeMessage
-		} else if (specialsFilterEnabled && stations.length === 0) {
-			message =
-				'No items to show. There may be no specials today. Try changing the filters.'
-		} else if (anyFiltersEnabled && !size(groupedMenuData)) {
-			message = 'No items to show. Try changing the filters.'
-		}
-
-		const messageView = <NoticeView style={styles.message} text={message} />
-
-		// If the requested menu has no food items, that location is closed
-		const isOpen = Object.keys(foodItems).length !== 0
-
-		const header = (
-			<FilterToolbar
-				date={now}
-				filters={filters}
-				isOpen={isOpen}
-				onPopoverDismiss={this.updateFilter}
-				title={mealName}
-			/>
-		)
-
-		const navigate = this.props.navigation.navigate
-
-		return (
-			<SectionList
-				ItemSeparatorComponent={Separator}
-				ListEmptyComponent={messageView}
-				ListHeaderComponent={header}
-				contentContainerStyle={styles.contentContainer}
-				extraData={filters}
-				keyExtractor={(_item, index) => index.toString()}
-				onRefresh={this.props.onRefresh}
-				refreshing={this.props.refreshing}
-				renderItem={({item}) => {
-					const specialsFilterEnabled = this.areSpecialsFiltered(
-						this.state.filters,
-					)
-					const icons = this.props.menuCorIcons
-					return (
-						<FoodItemRow
-							badgeSpecials={!specialsFilterEnabled}
-							corIcons={icons}
-							data={item}
-							onPress={() => navigate('MenuItemDetailView', {item, icons})}
-							spacing={{left: LEFT_MARGIN}}
-						/>
-					)
-				}}
-				renderSectionHeader={(info): JSX.Element => {
-					const title = info.section.title
-					const {now, meals} = this.props
-					const {filters} = this.state
-					const {stations} = chooseMeal(meals, filters, now)
-					const menu = stations.find((m) => m.label === title)
-
-					return (
-						<ListSectionHeader
-							spacing={{left: LEFT_MARGIN}}
-							subtitle={menu ? menu.note : ''}
-							title={title}
-						/>
-					)
-				}}
-				sections={groupedMenuData}
-				style={styles.inner}
-				windowSize={5}
-			/>
-		)
-	}
+				return (
+					<ListSectionHeader
+						spacing={{left: LEFT_MARGIN}}
+						subtitle={note}
+						title={title}
+					/>
+				)
+			}}
+			sections={groupedMenuData}
+			style={styles.inner}
+			windowSize={5}
+		/>
+	)
 }
