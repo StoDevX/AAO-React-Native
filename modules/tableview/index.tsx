9a6e9988325e14577589a9ee550a85d90ac8f5ad
Drew Volz <volz.drew@gmail.com>
2022-04-19 23:29:13 -0700

    adopt ios 14 tableview styles

--- modules/tableview/index.tsx
+++ modules/tableview/index.tsx
@@ -1,17 +1,34 @@
 import * as React from 'react'
+import {Platform, StyleSheet} from 'react-native'
 import {sectionBgColor} from '@frogpond/colors'
 
 import {
-	TableView,
+	TableView as ActualTableView,
 	Section as ActualSection,
 	Cell,
 } from 'react-native-tableview-simple'
-import type {SectionInterface} from 'react-native-tableview-simple/src/components/Section'
-
+import type {SectionInterface} from 'react-native-tableview-simple/lib/typescript/components/Section'
+import type {TableViewInterface} from 'react-native-tableview-simple/lib/typescript/components/TableView'
 export * from './cells'
 
 let Section = (props: SectionInterface): JSX.Element => (
-	<ActualSection sectionTintColor={sectionBgColor} withSafeAreaView={false} {...props} />
+	<ActualSection
+		hideSurroundingSeparators={Platform.OS === 'ios'}
+		roundedCorners={Platform.OS === 'ios'}
+		sectionTintColor={sectionBgColor}
+		withSafeAreaView={false}
+		{...props}
+	/>
 )
+
+let TableView = (props: TableViewInterface): JSX.Element => (
+	<ActualTableView style={styles.tableview} {...props} />
+)
+
+const styles = StyleSheet.create({
+	tableview: {
+		marginHorizontal: Platform.OS === 'ios' ? 15 : 0,
+	},
+})
 
 export {TableView, Section, Cell}
