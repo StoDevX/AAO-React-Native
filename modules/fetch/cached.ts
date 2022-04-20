60eea4e911f6742397cd7541287537ff0ec9f3b9
Hawken Rives <hawkrives@fastmail.fm>
2022-04-20 09:09:01 -0400

    switch to external AsyncStorage module

--- modules/fetch/cached.ts
+++ modules/fetch/cached.ts
@@ -1,4 +1,4 @@
-import {AsyncStorage} from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import CachePolicy from 'http-cache-semantics'
 import fromPairs from 'lodash/fromPairs'
 import type {Dictionary} from 'lodash'
--- modules/storage/index.ts
+++ modules/storage/index.ts
@@ -1,4 +1,4 @@
-import {AsyncStorage} from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 let prefix: string
 
