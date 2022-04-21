31a11e1bf8de4f0a35cb806a31914011ccebfc2a
Hawken Rives <hawkrives@fastmail.fm>
2022-04-21 18:24:29 -0400

    clean up DatepickerIOS

--- modules/datepicker/datepicker-ios.tsx
+++ modules/datepicker/datepicker-ios.tsx
@@ -1,19 +1,15 @@
-import * as React from 'react'
 import {StyleSheet} from 'react-native'
 import {BaseDateTimePicker} from './basepicker'
 import {IosDatetimePickerProps} from './types'
 
 export const DatePicker = (props: IosDatetimePickerProps): JSX.Element => {
-	// Picker + Button needs to always show on iOS
-	let showPickeriOS = true
-
 	return (
 		<BaseDateTimePicker
 			initialDate={props.initialDate}
 			minuteInterval={props.minuteInterval}
 			mode={props.mode}
 			onDateChange={props.onDateChange}
-			showPickerIos={showPickeriOS}
+			showPickerIos={true}
 			style={defaultStyle.datePicker}
 		/>
 	)
