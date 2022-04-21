11866a8b8a193943e851bce56f071855b6870e2f
Hawken Rives <hawkrives@fastmail.fm>
2022-04-21 13:40:18 -0400

    change OtherModes.Row to a function component

--- source/views/transportation/other-modes/row.tsx
+++ source/views/transportation/other-modes/row.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import type {OtherModeType} from '../types'
 import {Detail, ListRow, Title} from '@frogpond/lists'
 import {Column, Row} from '@frogpond/layout'
@@ -8,22 +7,17 @@ type Props = {
 	mode: OtherModeType
 }
 
-export class OtherModesRow extends React.PureComponent<Props> {
-	render(): JSX.Element {
-		let {mode} = this.props
+export function OtherModesRow(props: Props): JSX.Element {
+	let {mode, onPress} = props
 
-		return (
-			<ListRow
-				arrowPosition="top"
-				onPress={() => this.props.onPress(this.props.mode)}
-			>
-				<Row alignItems="center">
-					<Column flex={1}>
-						<Title lines={1}>{mode.name}</Title>
-						<Detail lines={1}>{mode.synopsis}</Detail>
-					</Column>
-				</Row>
-			</ListRow>
-		)
-	}
+	return (
+		<ListRow arrowPosition="top" onPress={() => onPress(mode)}>
+			<Row alignItems="center">
+				<Column flex={1}>
+					<Title lines={1}>{mode.name}</Title>
+					<Detail lines={1}>{mode.synopsis}</Detail>
+				</Column>
+			</Row>
+		</ListRow>
+	)
 }
