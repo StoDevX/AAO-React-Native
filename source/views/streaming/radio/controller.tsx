6c9cdd6c0ac35ed092451bd8f3812dbcb9f6f605
Drew Volz <volz.drew@gmail.com>
2022-05-07 18:36:52 -0700

    prettier, removal of TopLevelViewPropsType, and navigation hook

--- source/views/streaming/radio/controller.tsx
+++ source/views/streaming/radio/controller.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react'
 import {useCallback, useState} from 'react'
-import {Image, ScrollView, StyleSheet, Text, View, useWindowDimensions} from 'react-native'
+import {
+	Image,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
+	useWindowDimensions,
+} from 'react-native'
 import noop from 'lodash/noop'
 import * as c from '@frogpond/colors'
 import {callPhone} from '../../../components/call-phone'
 import {Row} from '@frogpond/layout'
-import type {TopLevelViewPropsType} from '../../types'
 import {StreamPlayer} from './player'
 import type {HtmlAudioError, PlayState} from './types'
 import {useTheme} from './theme'
 import {ActionButton, CallButton, ShowCalendarButton} from './buttons'
 import {openUrl} from '@frogpond/open-url'
+import {useNavigation} from '@react-navigation/native'
 
 // If you want to fix the inline player, switch to `true`
 const ALLOW_INLINE_PLAYER = false
@@ -46,7 +53,7 @@ function PlayButton(props: PlayButtonProps): JSX.Element {
 	}
 }
 
-type Props = TopLevelViewPropsType & {
+type Props = {
 	image: number
 	playerUrl: string
 	stationNumber: string
@@ -67,11 +74,12 @@ export function RadioControllerView(props: Props): JSX.Element {
 		title,
 		stationName,
 		image,
-		navigation,
 		scheduleViewName,
 		stationNumber,
 		playerUrl,
 	} = props
+
+	let navigation = useNavigation()
 
 	let [playState, setPlayState] = useState<PlayState>('paused')
 	let [streamError, setStreamError] = useState<HtmlAudioError | null>(null)
@@ -178,10 +186,7 @@ export function RadioControllerView(props: Props): JSX.Element {
 	let logoBorderColor = {borderColor: theme.imageBorderColor}
 	let logoBg = {backgroundColor: theme.imageBackgroundColor}
 	let logo = [styles.logoBorder, logoSize, logoBorderColor, logoBg]
-	let logoWrapper = [
-		styles.logoWrapper,
-		sideways && landscape.logoWrapper,
-	]
+	let logoWrapper = [styles.logoWrapper, sideways && landscape.logoWrapper]
 
 	return (
 		<ScrollView contentContainerStyle={root}>
