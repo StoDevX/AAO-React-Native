8db79902ae76c7c1d68f0aa4b23d6b8c19dc22e0
Drew Volz <volz.drew@gmail.com>
2022-05-14 13:02:34 -0700

    add event detail navigation options
    * adds navigation options to routes
    * adds title for calendar event detail
    * adds title for radio show event detail

--- modules/event-list/event-detail.tsx
+++ modules/event-list/event-detail.tsx
@@ -1,7 +1,18 @@
 import {Platform} from 'react-native'
 
-import {EventDetail as IosEventDetail} from './event-detail-ios'
-import {EventDetail as AndroidEventDetail} from './event-detail-android'
+import {
+	EventDetail as IosEventDetail,
+	NavigationOptions as IOSDetailNavigationOptions,
+} from './event-detail-ios'
+import {
+	EventDetail as AndroidEventDetail,
+	NavigationOptions as AndroidDetailNavigationOptions,
+} from './event-detail-android'
+
+export const EventDetailNavigationOptions =
+	Platform.OS === 'ios'
+		? IOSDetailNavigationOptions
+		: AndroidDetailNavigationOptions
 
 export const EventDetail =
 	Platform.OS === 'ios' ? IosEventDetail : AndroidEventDetail
