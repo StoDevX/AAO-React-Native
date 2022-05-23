909dfa67375ebd72eba5d9780f6bdefb54e7e2bd
Drew Volz <volz.drew@gmail.com>
2022-05-22 21:27:14 -0700

    add DebugNotice button to navigation buttons
    a button to serve as a visual identifier of views serving up data that
    is mocked

---
+++ modules/navigation-buttons/debug.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import {Alert, StyleSheet} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+import {Touchable} from '@frogpond/touchable'
+import {infoBlue} from '@frogpond/colors'
+import {rightButtonStyles as styles} from './styles'
+
+export const debugStyles = StyleSheet.create({
+	debugButton: {
+		color: infoBlue,
+	},
+})
+
+type Props = {
+	shouldShow: boolean
+}
+
+export const DebugNoticeButton = function (props: Props): JSX.Element {
+	if (!props.shouldShow) {
+		return <></>
+	}
+
+	return (
+		<Touchable
+			highlight={false}
+			onPress={() => {
+				Alert.alert('Debug notice', 'This view is showing mocked data.')
+			}}
+			style={styles.button}
+		>
+			<Icon name="bug" style={[styles.icon, debugStyles.debugButton]} />
+		</Touchable>
+	)
+}
