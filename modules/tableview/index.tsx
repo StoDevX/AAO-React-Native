f29715d13afbcf7bc15d4793802aedcc5e116fa8
Drew Volz <volz.drew@gmail.com>
2022-05-28 11:53:02 -0700

    add CellInterfaceModifiedType to allow a less restrictive onPress from react-native-tableview-simple CellInterface
    Replacing onPress type with a less restricted type of our own otherwise
    we have to deal with issues around passing in functions and promises
    that (i) cannot pass in parameters and (ii) must return void or false.
    
    Otherwise we deal with type issues being flagged for valid callbacks.

--- modules/tableview/index.tsx
+++ modules/tableview/index.tsx
@@ -8,6 +8,19 @@ import type {TableViewInterface} from 'react-native-tableview-simple/lib/typescr
 import {CellInterface} from 'react-native-tableview-simple/lib/typescript/components/Cell'
 export * from './cells'
 
+/*
+ * Replacing onPress type with a less restricted type of our own
+ * otherwise we have to deal with issues around passing in anonymous
+ * functions and promises that (i) cannot pass in parameters and
+ * (ii) must return void or false.
+ *
+ * Otherwise we deal with type issues being flagged for perfectly
+ * valid callbacks.
+ */
+type CellInterfaceModifiedType = Omit<CellInterface, 'onPress'> & {
+	onPress?: () => void
+}
+
 let Section = (props: SectionInterface): JSX.Element => (
 	<RNTableView.Section
 		hideSurroundingSeparators={Platform.OS === 'ios'}
@@ -22,7 +35,7 @@ let TableView = (props: TableViewInterface): JSX.Element => (
 	<RNTableView.TableView style={styles.tableview} {...props} />
 )
 
-let Cell = (props: CellInterface): JSX.Element => (
+let Cell = (props: CellInterfaceModifiedType): JSX.Element => (
 	<RNTableView.Cell {...props} />
 )
 
