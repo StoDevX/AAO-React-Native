195638680df878a7175b1af299e39c8bc6b2b72a
Drew Volz <volz.drew@gmail.com>
2022-05-28 21:31:52 -0700

    replace deprecated Clipboard imports in call-phone and send-email

--- source/components/call-phone.ts
+++ source/components/call-phone.ts
@@ -1,4 +1,5 @@
-import {Alert, Clipboard} from 'react-native'
+import {Alert} from 'react-native'
+import Clipboard from '@react-native-community/clipboard'
 import {phonecall} from 'react-native-communications'
 
 type Options = {
