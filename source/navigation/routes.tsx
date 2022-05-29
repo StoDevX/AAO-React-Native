4876ade51627eb3d04dfa390433b1e8e5434b938
Drew Volz <volz.drew@gmail.com>
2022-05-29 16:41:35 -0700

    route refactoring: home vs settings and configure a modal stack
    * separate home and settings stack navigators
    * move navigation options into shared styledScreenOptions object
    * present the settings screen as a modal

--- source/navigation/routes.tsx
+++ source/navigation/routes.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {
+	createNativeStackNavigator,
+	NativeStackNavigationOptions,
+} from '@react-navigation/native-stack'
 
 import * as home from '../views/home'
 import * as buildingHours from '../views/building-hours'
@@ -32,7 +35,7 @@ import * as othermodes from '../views/transportation/other-modes'
 import * as stoprint from '../views/stoprint'
 import {StyleSheet} from 'react-native'
 import {getTheme} from '@frogpond/app-theme'
-import {RootStackParamList} from './types'
+import {RootStackParamList, SettingsStackParamList} from './types'
 
 const theme = getTheme()
 
@@ -42,242 +45,261 @@ const styles = StyleSheet.create({
 	},
 })
 
-const Stack = createNativeStackNavigator<RootStackParamList>()
-
-export function RootStack(): JSX.Element {
-	return (
-		<Stack.Navigator
-			initialRouteName="Home"
-			screenOptions={{
-				gestureEnabled: true,
-				headerStyle: styles.header,
-				headerTintColor: theme.navigationForeground,
-			}}
-		>
-			<Stack.Screen
-				component={home.View}
-				name="Home"
-				options={home.NavigationOptions}
-			/>
-			<Stack.Group>
-				<Stack.Screen
-					component={calendar.View}
-					name={calendar.NavigationKey}
-					options={calendar.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={eventDetail.EventDetail}
-					name="EventDetail"
-					options={eventDetail.EventDetailNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={settings.SettingsView}
-					name="Settings"
-					options={settings.SettingsNavigationOptions}
-				/>
-				<Stack.Screen
-					component={faqs.View}
-					name="Faq"
-					options={faqs.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={settings.IconSettingsView}
-					name="IconSettings"
-					options={settings.IconNavigationOptions}
-				/>
-				<Stack.Screen component={settings.CreditsView} name="Credits" />
-				<Stack.Screen component={settings.PrivacyView} name="Privacy" />
-				<Stack.Screen component={settings.LegalView} name="Legal" />
-				<Stack.Screen component={settings.APITestView} name="APITest" />
-				<Stack.Screen component={settings.DebugView} name="Debug" />
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={buildingHours.BuildingHoursDetailView}
-					name="BuildingHoursDetail"
-					options={buildingHours.DetailNavigationOptions}
-				/>
-				<Stack.Screen
-					component={buildingHours.BuildingHoursView}
-					name="BuildingHours"
-					options={buildingHours.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={buildingHours.BuildingHoursProblemReportView}
-					name="BuildingHoursProblemReport"
-					options={buildingHours.ReportNavigationOptions}
-				/>
-				<Stack.Screen
-					component={buildingHours.BuildingHoursScheduleEditorView}
-					name="BuildingHoursScheduleEditor"
-					options={buildingHours.EditorNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={contacts.ContactsView}
-					name="Contacts"
-					options={contacts.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={contacts.ContactsDetailView}
-					name="ContactsDetail"
-					options={contacts.DetailNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={dictionary.DictionaryView}
-					name="Dictionary"
-					options={dictionary.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={dictionary.DictionaryDetailView}
-					name="DictionaryDetail"
-					options={dictionary.DetailNavigationOptions}
-				/>
-				<Stack.Screen
-					component={dictionary.DictionaryEditorView}
-					name="DictionaryEditor"
-					options={dictionary.EditorNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={transportation.View}
-					name="Transportation"
-					options={transportation.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={transportation.OtherModesDetailView}
-					name="OtherModesDetail"
-					options={othermodes.OtherModesNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={DevBonAppPickerView}
-					name="BonAppPicker"
-					options={DevBonAppNavigationOptions}
-				/>
-				<Stack.Screen
-					component={menus.View}
-					name="Menus"
-					options={menus.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={menus.CarletonBurtonMenuScreen}
-					name="CarletonBurtonMenu"
-					options={carletonmenus.BurtonNavigationOptions}
-				/>
-				<Stack.Screen
-					component={menus.CarletonLDCMenuScreen}
-					name="CarletonLDCMenu"
-					options={carletonmenus.LDCNavigationOptions}
-				/>
-				<Stack.Screen
-					component={menus.CarletonSaylesMenuScreen}
-					name="CarletonSaylesMenu"
-					options={carletonmenus.SaylesNavigationOptions}
-				/>
-				<Stack.Screen
-					component={menus.CarletonWeitzMenuScreen}
-					name="CarletonWeitzMenu"
-					options={carletonmenus.WeitzNavigationOptions}
-				/>
-				<Stack.Screen
-					component={MenuItemDetailView}
-					name="MenuItemDetail"
-					options={DetailNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={streaming.View}
-					name="Streaming"
-					options={streaming.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={streaming.KSTOScheduleView}
-					name="KSTOSchedule"
-					options={streaming.KSTOScheduleNavigationOptions}
-				/>
-				<Stack.Screen
-					component={streaming.KRLXScheduleView}
-					name="KRLXSchedule"
-					options={streaming.KRLXScheduleNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Screen
-				component={help.HelpView}
-				name="Help"
-				options={help.NavigationOptions}
-			/>
-			<Stack.Group>
-				<Stack.Screen
-					component={orgs.StudentOrgsView}
-					name="StudentOrgs"
-					options={orgs.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={orgs.StudentOrgsDetailView}
-					name="StudentOrgsDetail"
-					options={orgs.DetailNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={news.View}
-					name="News"
-					options={news.NavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={studentwork.View}
-					name="Job"
-					options={studentwork.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={studentworkdetail.View}
-					name="JobDetail"
-					options={studentworkdetail.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={sis.View}
-					name="SIS"
-					options={sis.NavigationOptions}
-				/>
-				<Stack.Screen
-					component={sis.CourseSearchResultsView}
-					name="CourseSearchResults"
-					options={sis.CourseSearchNavigationOptions}
-				/>
-				<Stack.Screen
-					component={sis.CourseDetailView}
-					name="CourseDetail"
-					options={sis.CourseSearchDetailNavigationOptions}
-				/>
-			</Stack.Group>
-			<Stack.Group>
-				<Stack.Screen
-					component={stoprint.PrintJobsView}
-					name="PrintJobs"
-					options={stoprint.PrintJobsNavigationOptions}
-				/>
-				<Stack.Screen
-					component={stoprint.PrinterListView}
-					name="PrinterList"
-					options={stoprint.PrinterListNavigationOptions}
-				/>
-				<Stack.Screen
-					component={stoprint.PrintJobReleaseView}
-					name="PrintJobRelease"
-					options={stoprint.PrintJobReleaseNavigationOptions}
-				/>
-			</Stack.Group>
-		</Stack.Navigator>
-	)
+const styledScreenOptions: NativeStackNavigationOptions = {
+	gestureEnabled: true,
+	headerStyle: styles.header,
+	headerTintColor: theme.navigationForeground,
 }
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+const SettingsStack = createNativeStackNavigator<SettingsStackParamList>()
+
+const HomeStackScreens = () => (
+	<Stack.Navigator screenOptions={styledScreenOptions}>
+		<Stack.Screen
+			component={home.View}
+			name="Home"
+			options={home.NavigationOptions}
+		/>
+		<Stack.Group>
+			<Stack.Screen
+				component={calendar.View}
+				name={calendar.NavigationKey}
+				options={calendar.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={eventDetail.EventDetail}
+				name="EventDetail"
+				options={eventDetail.EventDetailNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={buildingHours.BuildingHoursDetailView}
+				name="BuildingHoursDetail"
+				options={buildingHours.DetailNavigationOptions}
+			/>
+			<Stack.Screen
+				component={buildingHours.BuildingHoursView}
+				name="BuildingHours"
+				options={buildingHours.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={buildingHours.BuildingHoursProblemReportView}
+				name="BuildingHoursProblemReport"
+				options={buildingHours.ReportNavigationOptions}
+			/>
+			<Stack.Screen
+				component={buildingHours.BuildingHoursScheduleEditorView}
+				name="BuildingHoursScheduleEditor"
+				options={buildingHours.EditorNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={contacts.ContactsView}
+				name="Contacts"
+				options={contacts.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={contacts.ContactsDetailView}
+				name="ContactsDetail"
+				options={contacts.DetailNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={dictionary.DictionaryView}
+				name="Dictionary"
+				options={dictionary.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={dictionary.DictionaryDetailView}
+				name="DictionaryDetail"
+				options={dictionary.DetailNavigationOptions}
+			/>
+			<Stack.Screen
+				component={dictionary.DictionaryEditorView}
+				name="DictionaryEditor"
+				options={dictionary.EditorNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={transportation.View}
+				name="Transportation"
+				options={transportation.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={transportation.OtherModesDetailView}
+				name="OtherModesDetail"
+				options={othermodes.OtherModesNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={DevBonAppPickerView}
+				name="BonAppPicker"
+				options={DevBonAppNavigationOptions}
+			/>
+			<Stack.Screen
+				component={menus.View}
+				name="Menus"
+				options={menus.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={menus.CarletonBurtonMenuScreen}
+				name="CarletonBurtonMenu"
+				options={carletonmenus.BurtonNavigationOptions}
+			/>
+			<Stack.Screen
+				component={menus.CarletonLDCMenuScreen}
+				name="CarletonLDCMenu"
+				options={carletonmenus.LDCNavigationOptions}
+			/>
+			<Stack.Screen
+				component={menus.CarletonSaylesMenuScreen}
+				name="CarletonSaylesMenu"
+				options={carletonmenus.SaylesNavigationOptions}
+			/>
+			<Stack.Screen
+				component={menus.CarletonWeitzMenuScreen}
+				name="CarletonWeitzMenu"
+				options={carletonmenus.WeitzNavigationOptions}
+			/>
+			<Stack.Screen
+				component={MenuItemDetailView}
+				name="MenuItemDetail"
+				options={DetailNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={streaming.View}
+				name="Streaming"
+				options={streaming.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={streaming.KSTOScheduleView}
+				name="KSTOSchedule"
+				options={streaming.KSTOScheduleNavigationOptions}
+			/>
+			<Stack.Screen
+				component={streaming.KRLXScheduleView}
+				name="KRLXSchedule"
+				options={streaming.KRLXScheduleNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Screen
+			component={help.HelpView}
+			name="Help"
+			options={help.NavigationOptions}
+		/>
+		<Stack.Group>
+			<Stack.Screen
+				component={orgs.StudentOrgsView}
+				name="StudentOrgs"
+				options={orgs.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={orgs.StudentOrgsDetailView}
+				name="StudentOrgsDetail"
+				options={orgs.DetailNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={news.View}
+				name="News"
+				options={news.NavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={studentwork.View}
+				name="Job"
+				options={studentwork.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={studentworkdetail.View}
+				name="JobDetail"
+				options={studentworkdetail.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={sis.View}
+				name="SIS"
+				options={sis.NavigationOptions}
+			/>
+			<Stack.Screen
+				component={sis.CourseSearchResultsView}
+				name="CourseSearchResults"
+				options={sis.CourseSearchNavigationOptions}
+			/>
+			<Stack.Screen
+				component={sis.CourseDetailView}
+				name="CourseDetail"
+				options={sis.CourseSearchDetailNavigationOptions}
+			/>
+		</Stack.Group>
+		<Stack.Group>
+			<Stack.Screen
+				component={stoprint.PrintJobsView}
+				name="PrintJobs"
+				options={stoprint.PrintJobsNavigationOptions}
+			/>
+			<Stack.Screen
+				component={stoprint.PrinterListView}
+				name="PrinterList"
+				options={stoprint.PrinterListNavigationOptions}
+			/>
+			<Stack.Screen
+				component={stoprint.PrintJobReleaseView}
+				name="PrintJobRelease"
+				options={stoprint.PrintJobReleaseNavigationOptions}
+			/>
+		</Stack.Group>
+	</Stack.Navigator>
+)
+
+const SettingsStackScreens = () => (
+	<SettingsStack.Navigator screenOptions={styledScreenOptions}>
+		<SettingsStack.Screen
+			component={settings.SettingsView}
+			name="Settings"
+			options={settings.SettingsNavigationOptions}
+		/>
+		<SettingsStack.Screen
+			component={faqs.View}
+			name="Faq"
+			options={faqs.NavigationOptions}
+		/>
+		<SettingsStack.Screen
+			component={settings.IconSettingsView}
+			name="IconSettings"
+			options={settings.IconNavigationOptions}
+		/>
+		<SettingsStack.Screen component={settings.CreditsView} name="Credits" />
+		<SettingsStack.Screen component={settings.PrivacyView} name="Privacy" />
+		<SettingsStack.Screen component={settings.LegalView} name="Legal" />
+		<SettingsStack.Screen component={settings.APITestView} name="APITest" />
+		<SettingsStack.Screen component={settings.DebugView} name="Debug" />
+	</SettingsStack.Navigator>
+)
+
+export const RootStack = (): JSX.Element => (
+	<Stack.Navigator
+		initialRouteName="Home"
+		screenOptions={{
+			headerShown: false,
+		}}
+	>
+		<Stack.Screen component={HomeStackScreens} name="Home" />
+		<SettingsStack.Screen
+			component={SettingsStackScreens}
+			name="Settings"
+			options={{
+				presentation: 'modal',
+			}}
+		/>
+	</Stack.Navigator>
+)
