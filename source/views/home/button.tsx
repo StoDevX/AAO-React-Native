de0325f8a66f025d616ed323e1e6e6e89c595481
Hawken Rives <hawkrives@fastmail.fm>
2022-06-27 10:22:15 -0500

    refactor HomeScreenButton and remove gradient support

--- source/views/home/button.tsx
+++ source/views/home/button.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import {View, Text, StyleSheet, Platform} from 'react-native'
 import Icon from 'react-native-vector-icons/Entypo'
 import type {ViewType} from '../views'
-import LinearGradient from 'react-native-linear-gradient'
 import {Touchable} from '@frogpond/touchable'
 import {transparent} from '@frogpond/colors'
 import {homescreenForegroundDark, homescreenForegroundLight} from './colors'
@@ -18,91 +17,19 @@ export function HomeScreenButton({view, onPress}: Props): JSX.Element {
 		view.foreground === 'light' ? styles.lightForeground : styles.darkForeground
 
 	return (
-		<TouchableButton
-			gradient={view.gradient}
-			label={view.title}
+		<Touchable
+			accessibilityLabel={view.title}
+			accessibilityRole="button"
+			accessible={true}
+			highlight={false}
 			onPress={onPress}
-			tint={view.tint}
+			style={[styles.button, {backgroundColor: view.tint}]}
 		>
 			<View style={styles.contents}>
 				<Icon name={view.icon} size={32} style={[foreground, styles.icon]} />
 				<Text style={[foreground, styles.text]}>{view.title}</Text>
 			</View>
-		</TouchableButton>
-	)
-}
-
-type TouchableButtonProps = {
-	onPress: () => void
-	label: string
-	children: React.ReactChildren | JSX.Element
-	tint: string
-	gradient?: [string, string]
-}
-
-function TouchableButton(props: TouchableButtonProps) {
-	let {onPress, label, children, tint, gradient} = props
-
-	if (Platform.OS === 'android') {
-		return (
-			<Tint gradient={gradient} tint={tint}>
-				<TouchableWrapper label={label} onPress={onPress}>
-					{children}
-				</TouchableWrapper>
-			</Tint>
-		)
-	} else {
-		return (
-			<TouchableWrapper label={label} onPress={onPress}>
-				<Tint gradient={gradient} tint={tint}>
-					{children}
-				</Tint>
-			</TouchableWrapper>
-		)
-	}
-}
-
-type TouchableWrapperProps = {
-	onPress: () => void
-	label: string
-	children: React.ReactChildren | JSX.Element
-}
-
-function TouchableWrapper({onPress, children, label}: TouchableWrapperProps) {
-	return (
-		<Touchable
-			accessibilityLabel={label}
-			accessibilityRole="button"
-			accessible={true}
-			highlight={false}
-			onPress={onPress}
-		>
-			{children}
 		</Touchable>
-	)
-}
-
-type TintProps = {
-	children: React.ReactChildren | JSX.Element
-	tint: string
-	gradient?: [string, string]
-}
-
-function Tint({tint = 'black', gradient, children}: TintProps) {
-	if (!gradient) {
-		let bg = {backgroundColor: tint}
-		return <View style={[styles.button, bg]}>{children}</View>
-	}
-
-	return (
-		<LinearGradient
-			colors={gradient}
-			end={{x: 0, y: 0.85}}
-			start={{x: 0, y: 0.05}}
-			style={styles.button}
-		>
-			{children}
-		</LinearGradient>
 	)
 }
 
