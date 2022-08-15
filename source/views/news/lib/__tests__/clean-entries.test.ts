acb4bf5cdb11bb61516713acdea67239fecab177
Drew Volz <volz.drew@gmail.com>
2022-08-14 19:04:23 -0700

    replace toBe with toStrictEqual

--- source/views/news/lib/__tests__/clean-entries.test.ts
+++ source/views/news/lib/__tests__/clean-entries.test.ts
@@ -4,14 +4,14 @@ describe('cleanEntries', () => {
 	let rest = {authors: [], categories: [], title: ''}
 
 	it('should remove entries with empty excerpts', () => {
-		expect(cleanEntries([{excerpt: ' ', content: 'hi bleep', ...rest}])).toBe(
-			[],
-		)
+		expect(
+			cleanEntries([{excerpt: ' ', content: 'hi bleep', ...rest}]),
+		).toStrictEqual([])
 	})
 
 	it('should remove entries with <form in the content', () => {
 		expect(
 			cleanEntries([{excerpt: 'blah', content: 'hi bleep <form', ...rest}]),
-		).toBe([])
+		).toStrictEqual([])
 	})
 })
--- source/views/news/lib/__tests__/trim-categories.test.ts
+++ source/views/news/lib/__tests__/trim-categories.test.ts
@@ -3,14 +3,18 @@ import {trimStoryCateogry} from '../util'
 
 describe('trimStoryCateogry', () => {
 	it('should remove extraneous whitespace from the input', () => {
-		expect(trimStoryCateogry('Blah    Hi\t Bleep')).toBe('Blah Hi Bleep')
+		expect(trimStoryCateogry('Blah    Hi\t Bleep')).toStrictEqual(
+			'Blah Hi Bleep',
+		)
 	})
 
 	it('should title-case the input', () => {
-		expect(trimStoryCateogry('blah hi bleep')).toBe('Blah Hi Bleep')
+		expect(trimStoryCateogry('blah hi bleep')).toStrictEqual('Blah Hi Bleep')
 	})
 
 	it('should normalize any entities', () => {
-		expect(trimStoryCateogry('blah &amp; hi bleep')).toBe('Blah & Hi Bleep')
+		expect(trimStoryCateogry('blah &amp; hi bleep')).toStrictEqual(
+			'Blah & Hi Bleep',
+		)
 	})
 })
