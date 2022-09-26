e932d6ea5d6aa52b1eba715f6624687020f9f0da
Kristofer Rye <kristofer.rye@gmail.com>
2022-09-25 19:01:42 -0500

    Mock react-native-async-storage to make Jest pass
    Fixes GH-6437.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ source/__mocks__/@react-native-async-storage/async-storage.ts
@@ -0,0 +1,5 @@
+// Based on https://react-native-async-storage.github.io/async-storage/docs/advanced/jest/.
+//
+// Installs a mock for react-native-async-storage to prevent an "AsyncStorage is null" error
+// while the tests are running.
+export default from '@react-native-async-storage/async-storage/jest/async-storage-mock'
