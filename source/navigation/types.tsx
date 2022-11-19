25ae59ad81d3516590b66a05ea4809d3aaf6f0d2
Drew Volz <volz.drew@gmail.com>
2022-11-18 19:59:17 -0800

    Show department results natively in directory view (#6571)
    * tapping on a department fetches department data and renders it natively
    * navigating to a department pushes a new view on to the stack
    * navigating between query and department listings works as expected

--- source/navigation/types.tsx
+++ source/navigation/types.tsx
@@ -19,7 +19,7 @@ import type {
 import {Printer, PrintJob} from '../lib/stoprint/types'
 import {JobType} from '../views/sis/student-work/types'
 import {CourseType} from '../lib/course-search/types'
-import {DirectoryItem} from '../views/directory/types'
+import {DirectoryItem, DirectorySearchTypeEnum} from '../views/directory/types'
 
 export type RootStackParamList = {
 	Home: undefined
@@ -38,7 +38,7 @@ export type RootStackParamList = {
 	DictionaryDetail: {item: WordType}
 	Dictionary: undefined
 	DictionaryEditor: {item: WordType}
-	Directory: undefined
+	Directory: {queryType?: DirectorySearchTypeEnum; queryParam?: string}
 	DirectoryDetail: {contact: DirectoryItem}
 	Faq: undefined
 	Help: undefined
