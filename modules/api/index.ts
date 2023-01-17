10f6347ea55d8774f32d900ca73f9f0453016f6b
Hawken Rives <hawkrives@fastmail.fm>
2023-01-17 11:10:21 -0500

    rework api client

--- modules/api/index.ts
+++ modules/api/index.ts
@@ -1,20 +1,7 @@
-import qs from 'query-string'
+import ky from 'ky'
 
-let root: string
+export let client: typeof ky
 
-export function setApiRoot(url: string): void {
-	root = url
-}
-
-export const API = (
-	path: `/${string}`,
-	query?: Record<string, unknown>,
-): string => {
-	let url = root + path
-
-	if (query) {
-		url += `?${qs.stringify(query)}`
-	}
-
-	return url
+export function setApiRoot(url: URL): void {
+	client = ky.create({prefixUrl: url})
 }
