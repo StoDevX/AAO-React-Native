d4ebd10a270460ee493dccac503e82fc934bf947
Drew Volz <volz.drew@gmail.com>
2023-01-16 21:50:05 -0800

    (rn 71): logbox ignore was removed in the upgrade, cannot filter

--- source/root.ts
+++ source/root.ts
@@ -1,12 +1,4 @@
-import {AppRegistry, LogBox} from 'react-native'
+import {AppRegistry} from 'react-native'
 import App from './app'
-
-LogBox.ignoreLogs([
-	'Failed prop type: Invalid prop `containerTagName` of type `object` supplied to `ReactMarkdown`, expected `function`',
-	'Failed prop type: Invalid prop `containerTagName` of type `string` supplied to `ReactMarkdown`, expected `function`.',
-	"MaskedViewIOS has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-community/masked-view' instead of 'react-native'. See https://github.com/react-native-community/react-native-masked-view",
-	'componentWillReceiveProps has been renamed, and is not recommended for use. See https://fb.me/react-async-component-lifecycle-hooks for details.',
-	'componentWillMount has been renamed, and is not recommended for use. See https://fb.me/react-async-component-lifecycle-hooks for details.',
-])
 
 AppRegistry.registerComponent('AllAboutOlaf', () => App)
