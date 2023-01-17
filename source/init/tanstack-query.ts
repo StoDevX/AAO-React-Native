dc3d947b9b3cc612ac04aeebb306f3103502f37f
Hawken Rives <hawkrives@fastmail.fm>
2023-01-17 11:09:05 -0500

    continue setting up tanstack-query

---
+++ source/init/tanstack-query.ts
@@ -0,0 +1,39 @@
+import {AppState, Platform} from 'react-native'
+import NetInfo from '@react-native-community/netinfo'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {createAsyncStoragePersister} from '@tanstack/query-async-storage-persister'
+import {QueryClient, onlineManager, focusManager} from '@tanstack/react-query'
+
+//
+// Set up caching
+//
+
+const oneDayInMs = 1000 * 60 * 60 * 24 // 24 hours
+
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			cacheTime: oneDayInMs,
+		},
+	},
+})
+
+export const persister = createAsyncStoragePersister({storage: AsyncStorage})
+
+//
+// Enable auto-refresh on app switch or network reconnect
+//
+
+// ... on network reconnect
+onlineManager.setEventListener((setOnline) => {
+	return NetInfo.addEventListener((state) => {
+		setOnline(Boolean(state.isConnected))
+	})
+})
+
+// ... on app resume
+AppState.addEventListener('change', (status) => {
+	if (Platform.OS !== 'web') {
+		focusManager.setFocused(status === 'active')
+	}
+})
