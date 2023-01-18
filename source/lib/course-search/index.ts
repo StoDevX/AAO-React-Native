47c71f73d0504793a002fd5496498a320939955e
Hawken Rives <hawkrives@fastmail.fm>
2023-01-18 13:27:47 -0500

    remove unused code from course search

--- source/lib/course-search/index.ts
+++ source/lib/course-search/index.ts
@@ -1,5 +1,2 @@
-export {loadCachedCourses} from './load-cached-courses'
-export {updateStoredCourses, areAnyTermsCached} from './update-course-storage'
 export type {CourseType, TermType} from './types'
 export {parseTerm} from './parse-term'
-export {loadAllCourseFilterOptions} from './load-filter-options'
