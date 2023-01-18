32744aaf17bb8fb6d9fc8892874853a31aac1acb
Hawken Rives <hawkrives@fastmail.fm>
2023-01-17 19:49:25 -0500

    port transit/other-modes to tanstack-query

--- source/views/transportation/other-modes/list.tsx
+++ source/views/transportation/other-modes/list.tsx
@@ -1,22 +1,11 @@
 import * as React from 'react'
 import {OtherModesRow} from './row'
-import * as defaultData from '../../../../docs/transportation.json'
 import * as c from '@frogpond/colors'
 import {SectionList, StyleSheet} from 'react-native'
 import {ListEmpty, ListSectionHeader, ListSeparator} from '@frogpond/lists'
-import groupBy from 'lodash/groupBy'
-import toPairs from 'lodash/toPairs'
-import type {OtherModeType} from '../types'
-import {API} from '@frogpond/api'
-import {fetch} from '@frogpond/fetch'
 import {openUrl} from '@frogpond/open-url'
-
-const transportationUrl = API('/transit/modes')
-
-const groupModes = (modes: OtherModeType[]) => {
-	let grouped = groupBy(modes, (m) => m.category)
-	return toPairs(grouped).map(([key, value]) => ({title: key, data: value}))
-}
+import {useOtherModesGrouped} from './query'
+import {LoadingView, NoticeView} from '@frogpond/notice'
 
 const styles = StyleSheet.create({
 	listContainer: {
@@ -28,45 +17,42 @@ const styles = StyleSheet.create({
 })
 
 let OtherModesView = (): JSX.Element => {
-	let [modes, setModes] = React.useState<OtherModeType[]>(defaultData.data)
-	let [refreshing, setRefreshing] = React.useState(false)
+	let {
+		data = [],
+		error,
+		refetch,
+		isRefetching,
+		isLoading,
+		isError,
+	} = useOtherModesGrouped()
 
-	let fetchData = React.useCallback(async (reload?: boolean): Promise<void> => {
-		let {data: modes}: {data: Array<OtherModeType>} = await fetch(
-			transportationUrl,
-			{delay: reload ? 500 : 0},
-		).json()
-
-		setModes(modes)
-	}, [])
-
-	React.useEffect(() => {
-		fetchData()
-	}, [fetchData])
-
-	let refresh = async (): Promise<void> => {
-		setRefreshing(true)
-		await fetchData(true)
-		setRefreshing(false)
+	if (isError && error instanceof Error) {
+		return (
+			<NoticeView
+				buttonText="Try Again"
+				onPress={refetch}
+				text={`A problem occured while loading the transit modes. ${error.message}`}
+			/>
+		)
 	}
-
-	let groupedData = groupModes(modes)
 
 	return (
 		<SectionList
 			ItemSeparatorComponent={ListSeparator}
-			ListEmptyComponent={<ListEmpty mode="bug" />}
+			ListEmptyComponent={
+				isLoading ? <LoadingView /> : <ListEmpty mode="bug" />
+			}
 			contentContainerStyle={styles.contentContainer}
 			keyExtractor={(item) => item.name}
-			onRefresh={refresh}
-			refreshing={refreshing}
+			onRefresh={refetch}
+			refreshing={isRefetching}
 			renderItem={({item}) => (
 				<OtherModesRow mode={item} onPress={(mode) => openUrl(mode.url)} />
 			)}
 			renderSectionHeader={({section: {title}}) => (
 				<ListSectionHeader title={title} />
 			)}
-			sections={groupedData}
+			sections={data}
 			style={styles.listContainer}
 		/>
 	)
---
+++ source/views/transportation/other-modes/query.ts
@@ -0,0 +1,32 @@
+import {client} from '@frogpond/api'
+import {useQuery, UseQueryResult} from '@tanstack/react-query'
+import { groupBy, toPairs } from 'lodash'
+import {OtherModeType} from '../types'
+
+export const keys = {
+	all: ['transit-modes'] as const,
+}
+
+export function useOtherModes(): UseQueryResult<OtherModeType[], unknown> {
+	return useQuery({
+		queryKey: keys.all,
+		queryFn: async ({signal}) => {
+			let response = await client.get('/transit/modes', {signal}).json()
+			return response as OtherModeType[]
+		},
+	})
+}
+
+export function useOtherModesGrouped(): UseQueryResult<{title: string, data: OtherModeType[]}[], unknown> {
+	return useQuery({
+		queryKey: keys.all,
+		queryFn: async ({signal}) => {
+			let response = await client.get('/transit/modes', {signal}).json()
+			return response as OtherModeType[]
+		},
+        select: (modes) => {
+            let grouped = groupBy(modes, (m) => m.category)
+            return toPairs(grouped).map(([key, value]) => ({title: key, data: value}))
+        }
+	})
+}
