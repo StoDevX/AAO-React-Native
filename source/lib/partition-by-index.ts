c4015081311495d0ee5d3be7ffc706d1569dbcc9
Hawken Rives <hawkrives@fastmail.fm>
2023-01-17 21:17:51 -0500

    fix type error in partition-by-index

--- source/lib/partition-by-index.ts
+++ source/lib/partition-by-index.ts
@@ -1,10 +1,20 @@
+function* enumerate<T>(iter: Array<T>): Generator<[number, T]> {
+	let i = 0;
+	for (let item of iter) {
+		yield [i, item]
+		i += 1
+	}
+}
+
 export function partitionByIndex<T>(arr: T[]): [T[], T[]] {
-	return arr.reduce(
-		(acc, val, idx) => {
-			return idx % 2 === 0
-				? [acc[0].concat(val), acc[1]]
-				: [acc[0], acc[1].concat(val)]
-		},
-		[[], []],
-	)
+	let evens = [];
+	let odds = [];
+	for (let [i, item] of enumerate(arr)) {
+		if (i % 2 === 0) {
+			evens.push(item)
+		} else {
+			odds.push(item)
+		}
+	}
+	return [evens, odds]
 }
