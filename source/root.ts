b4058765c1cf43cb548a922b58ee5fe64a3ebcc4
Hawken Rives <hawkrives@fastmail.fm>
2023-01-19 09:51:00 -0500

    add typings for keyword-search

--- source/root.ts
+++ source/root.ts
@@ -1,3 +1,6 @@
+// This declaration must be inside the app code, so that Typescript merges
+// it with the default react typings. If it is placed into a separate .d.ts
+// file, Typescript will instead replace the default typings.
 declare module 'react' {
 	// source: https://fettblog.eu/typescript-react-generic-forward-refs/
 	function forwardRef<T, P = object>(
