90f13801971cbe7e9221e7fab9f33f477c8c392c
Hawken Rives <hawkrives@fastmail.fm>
2023-01-20 14:53:43 -0500

    fix detox jest config one more time

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
 	preset: 'ts-jest',
 	maxWorkers: 1,
-	setupFilesAfterEnv: ['./init.js'],
+	rootDir: '..',
+	testMatch: ['<rootDir>/e2e/**/*.(test|spec).ts'],
+	setupFilesAfterEnv: ['<rootDir>/e2e/init.js'],
 	testTimeout: 120000,
 	verbose: true,
 	reporters: ['detox/runners/jest/reporter'],
 	globalSetup: 'detox/runners/jest/globalSetup',
 	globalTeardown: 'detox/runners/jest/globalTeardown',
 	testEnvironment: 'detox/runners/jest/testEnvironment',
-	rootDir: '..',
-	testMatch: ['<rootDir>/e2e/**/*.(test|spec).tsx?'],
 }
