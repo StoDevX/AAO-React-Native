e7f48ab45ff11878d13edc6eaaf902975b6e1fdb
Drew Volz <volz.drew@gmail.com>
2023-01-21 00:19:59 -0800

    remove datetimepicker manual linking, fixed by hoisting

--- android/app/src/main/java/com/allaboutolaf/MainApplication.java
+++ android/app/src/main/java/com/allaboutolaf/MainApplication.java
@@ -11,7 +11,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,7 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
             List<ReactPackage> packages = new PackageList(this).getPackages();
             // Packages that cannot be autolinked yet can be added manually here, for example:
             // packages.add(new MyReactNativePackage());
-            packages.add(new RNDateTimePickerPackage());
             return packages;
         }
 
