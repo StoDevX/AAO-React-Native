bc87a4bbc992b9db8e81e093ee03fba334176405
Hawken Rives <hawkrives@fastmail.fm>
2023-01-21 07:49:24 -0500

    update tests for formatEmailParts

--- source/components/__tests__/email.test.ts
+++ source/components/__tests__/email.test.ts
@@ -3,46 +3,50 @@ import {formatEmailParts} from '../send-email'
 
 describe('formatEmailParts', () => {
 	it('should format empty inputs', () => {
-		expect(formatEmailParts({to: ['']})).toBe('mailto:?')
+		expect(formatEmailParts({to: ['']})).toMatchInlineSnapshot('"mailto:"')
 	})
 
 	it('should encode the "to" addressees', () => {
-		expect(formatEmailParts({to: ['test@domain.com']})).toBe(
-			'mailto:test%40domain.com?',
+		expect(formatEmailParts({to: ['test@domain.com']})).toMatchInlineSnapshot(
+			'"mailto:test@domain.com"',
 		)
-		expect(formatEmailParts({to: ['test@domain.com', 'help@domain.com']})).toBe(
-			'mailto:test%40domain.com%2Chelp%40domain.com?',
-		)
+		expect(
+			formatEmailParts({to: ['test@domain.com', 'help@domain.com']}),
+		).toMatchInlineSnapshot('"mailto:test@domain.com,help@domain.com"')
 	})
 
 	it('should encode the "cc" addressees', () => {
-		expect(formatEmailParts({cc: ['test@domain.com']})).toBe(
-			'mailto:?cc=test%40domain.com',
+		expect(formatEmailParts({cc: ['test@domain.com']})).toMatchInlineSnapshot(
+			'"mailto:?cc=test%40domain.com"',
 		)
-		expect(formatEmailParts({cc: ['test@domain.com', 'help@domain.com']})).toBe(
-			'mailto:?cc=test%40domain.com%2Chelp%40domain.com',
+		expect(
+			formatEmailParts({cc: ['test@domain.com', 'help@domain.com']}),
+		).toMatchInlineSnapshot(
+			'"mailto:?cc=test%40domain.com%2Chelp%40domain.com"',
 		)
 	})
 
 	it('should encode the "bcc" addressees', () => {
-		expect(formatEmailParts({bcc: ['test@domain.com']})).toBe(
-			'mailto:?bcc=test%40domain.com',
+		expect(formatEmailParts({bcc: ['test@domain.com']})).toMatchInlineSnapshot(
+			'"mailto:?bcc=test%40domain.com"',
 		)
 		expect(
 			formatEmailParts({bcc: ['test@domain.com', 'help@domain.com']}),
-		).toBe('mailto:?bcc=test%40domain.com%2Chelp%40domain.com')
+		).toMatchInlineSnapshot(
+			'"mailto:?bcc=test%40domain.com%2Chelp%40domain.com"',
+		)
 	})
 
 	it('should encode the subject', () => {
-		expect(formatEmailParts({subject: 'a thing'})).toBe(
-			'mailto:?subject=a%20thing',
+		expect(formatEmailParts({subject: 'a thing'})).toMatchInlineSnapshot(
+			'"mailto:?subject=a+thing"',
 		)
 	})
 
 	it('should encode to and subject', () => {
 		expect(
 			formatEmailParts({to: ['test@domain.com'], subject: 'a thing'}),
-		).toBe('mailto:test%40domain.com?subject=a%20thing')
+		).toMatchInlineSnapshot('"mailto:test@domain.com?subject=a+thing"')
 	})
 
 	it('should encode to, subject, and body', () => {
@@ -52,15 +56,17 @@ describe('formatEmailParts', () => {
 				subject: 'a thing',
 				body: 'hey there',
 			}),
-		).toBe('mailto:test%40domain.com?body=hey%20there&subject=a%20thing')
+		).toMatchInlineSnapshot(
+			'"mailto:test@domain.com?subject=a+thing&body=hey+there"',
+		)
 		expect(
 			formatEmailParts({
 				to: ['test@domain.com', 'test2@domain.com'],
 				subject: 'a thing',
 				body: 'hey there',
 			}),
-		).toBe(
-			'mailto:test%40domain.com%2Ctest2%40domain.com?body=hey%20there&subject=a%20thing',
+		).toMatchInlineSnapshot(
+			'"mailto:test@domain.com,test2@domain.com?subject=a+thing&body=hey+there"',
 		)
 	})
 
@@ -73,8 +79,8 @@ describe('formatEmailParts', () => {
 				subject: 'a thing',
 				body: 'hey there',
 			}),
-		).toBe(
-			'mailto:test%40domain.com?bcc=test3%40domain.com&body=hey%20there&cc=test2%40domain.com&subject=a%20thing',
+		).toMatchInlineSnapshot(
+			'"mailto:test@domain.com?cc=test2%40domain.com&bcc=test3%40domain.com&subject=a+thing&body=hey+there"',
 		)
 	})
 })
