cc40752bd8c4a5e20f24c0e20ff6f41796095b76
Drew Volz <volz.drew@gmail.com>
2023-01-23 00:48:21 -0800

    add demo user constants and hook to check if logged-in as this user

--- source/init/constants.ts
+++ source/init/constants.ts
@@ -1,5 +1,10 @@
 import {setVersionInfo, setAppName, setTimezone} from '@frogpond/constants'
 
+export const DEMO_USER = {
+	username: 'demo@frogpond.tech',
+	password: 'demo',
+}
+
 export const SENTRY_DSN =
 	'https://7f68e814c5c24c32a582f2ddc3d42b4c@o524787.ingest.sentry.io/5637838'
 
--- source/lib/stoprint/debug.ts
+++ source/lib/stoprint/debug.ts
@@ -1,1 +1,8 @@
-export const isStoprintMocked = false
+import {DEMO_USER} from '../../init/constants'
+import {useUsername} from '../login'
+
+export function useDemoAccount(): boolean {
+	let {data} = useUsername()
+	let username = data ? data.username : ''
+	return username === DEMO_USER.username
+}
