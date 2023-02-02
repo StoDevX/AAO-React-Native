7f27e9de052966e9b19785c6fafd01b7ef390465
Hawken Rives <hawkrives@fastmail.fm>
2023-02-02 08:17:40 -0500

    remove unused stringifyFilters module

--- modules/filter/index.ts
+++ modules/filter/index.ts
@@ -1,6 +1,5 @@
 export type {Filter, ListFilter, ToggleFilter, PickerFilter} from './types'
 export {applyFiltersToItem} from './apply-filters'
-export {stringifyFilters} from './stringify-filters'
 export {filterListSpecs, filterPickerSpecs, filterToggleSpecs} from './tools'
 export {FilterToolbar} from './filter-toolbar'
 export {FilterPopover} from './filter-popover'
