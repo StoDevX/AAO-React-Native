93f5ff65ab27567e934aa9829a5895b38d82b7d6
Drew Volz <volz.drew@gmail.com>
2023-02-04 01:16:11 -0800

    remove touchable from open-settings navigation button

--- modules/navigation-buttons/open-settings.tsx
+++ modules/navigation-buttons/open-settings.tsx
@@ -1,29 +1,15 @@
 import * as React from 'react'
-import {Touchable} from '@frogpond/touchable'
 import {Icon, platformPrefixIconName} from '@frogpond/icon'
 import * as c from '@frogpond/colors'
 import {commonStyles, rightButtonStyles} from './styles'
-import {useNavigation} from '@react-navigation/native'
 import {HeaderBackButtonProps} from '@react-navigation/native-stack/lib/typescript/src/types'
 
 export function OpenSettingsButton(_props: HeaderBackButtonProps): JSX.Element {
-	let navigation = useNavigation()
-
 	return (
-		<Touchable
-			accessibilityLabel="Open Settings"
-			accessibilityRole="button"
-			accessible={true}
-			borderless={true}
-			highlight={false}
-			onPress={() => navigation.navigate('Settings')}
-			style={commonStyles.button}
+		<Icon
+			name={platformPrefixIconName('ellipsis-horizontal-circle')}
+			style={[commonStyles.button, rightButtonStyles.icon, {color: c.label}]}
 			testID="button-open-settings"
-		>
-			<Icon
-				name={platformPrefixIconName('settings')}
-				style={[rightButtonStyles.icon, {color: c.label}]}
-			/>
-		</Touchable>
+		/>
 	)
 }
