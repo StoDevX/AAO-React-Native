bc5be216c206db095c1c0bf66a29dd7e414ed707
Drew Volz <volz.drew@gmail.com>
2023-08-21 22:58:53 -0700

    add paper provider

--- source/app.tsx
+++ source/app.tsx
@@ -13,7 +13,7 @@ import {queryClient, persister} from './init/tanstack-query'
 import * as React from 'react'
 import {PersistGate} from 'redux-persist/integration/react'
 import {Provider as ReduxProvider} from 'react-redux'
-// import {Provider as PaperProvider} from 'react-native-paper'
+import {Provider as PaperProvider} from 'react-native-paper'
 import {PersistQueryClientProvider} from '@tanstack/react-query-persist-client'
 import {store, persistor} from './redux'
 import {CombinedLightTheme, CombinedDarkTheme} from '@frogpond/app-theme'
@@ -39,22 +39,28 @@ export default function App(): JSX.Element {
 					client={queryClient}
 					persistOptions={{persister}}
 				>
-					<ActionSheetProvider>
-						<NavigationContainer
-							onReady={() => {
-								// Register the navigation container with the instrumentation
-								sentryInit.routingInstrumentation.registerNavigationContainer(
-									navigationRef,
-								)
-							}}
-							theme={scheme === 'dark' ? CombinedDarkTheme : CombinedLightTheme}
-						>
-							<StatusBar
-								barStyle={scheme === 'dark' ? 'light-content' : 'dark-content'}
-							/>
-							<RootStack />
-						</NavigationContainer>
-					</ActionSheetProvider>
+					<PaperProvider>
+						<ActionSheetProvider>
+							<NavigationContainer
+								onReady={() => {
+									// Register the navigation container with the instrumentation
+									sentryInit.routingInstrumentation.registerNavigationContainer(
+										navigationRef,
+									)
+								}}
+								theme={
+									scheme === 'dark' ? CombinedDarkTheme : CombinedLightTheme
+								}
+							>
+								<StatusBar
+									barStyle={
+										scheme === 'dark' ? 'light-content' : 'dark-content'
+									}
+								/>
+								<RootStack />
+							</NavigationContainer>
+						</ActionSheetProvider>
+					</PaperProvider>
 				</PersistQueryClientProvider>
 			</PersistGate>
 		</ReduxProvider>
