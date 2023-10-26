da68a1854f695e49d397aa73432ac7307c1b1ecf
Drew Volz <volz.drew@gmail.com>
2023-10-26 13:24:28 -0600

    add tests around redux favorite building hours

--- source/redux/parts/buildings.ts
+++ source/redux/parts/buildings.ts
@@ -2,7 +2,7 @@ import {createSlice} from '@reduxjs/toolkit'
 import type {PayloadAction} from '@reduxjs/toolkit'
 import type {RootState} from '../store'
 
-type State = {
+export type State = {
 	favorites: Array<string>
 }
 
