c16f649cfd59f7af185f43d472a2358a82965060
Drew Volz <volz.drew@gmail.com>
2024-10-16 16:30:07 -0700

    remove nonexistent themes from expo-router

--- source/modules/app-theme/paper.ts
+++ source/modules/app-theme/paper.ts
@@ -1,15 +1,8 @@
-import merge from 'deepmerge'
-
-import {
-	DarkTheme as NavigationDarkTheme,
-	DefaultTheme as NavigationLightTheme,
-} from 'expo-router'
-
 import {
 	MD3DarkTheme as PaperDarkTheme,
 	MD3LightTheme as PaperLightTheme,
 } from 'react-native-paper'
 
-export const CombinedLightTheme = merge(PaperLightTheme, NavigationLightTheme)
+export const CombinedLightTheme = PaperLightTheme
 
-export const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme)
+export const CombinedDarkTheme = PaperDarkTheme
