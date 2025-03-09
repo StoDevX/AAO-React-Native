2e3f601277795760314349e6ff4f8be1552a4e50
Drew Volz <volz.drew@gmail.com>
2025-03-08 20:56:18 -0800

    remove typo

--- modules/lists/large-sectionlist-props.ts
+++ modules/lists/large-sectionlist-props.ts
@@ -1,4 +1,4 @@
-ackimport {SectionList} from 'react-native'
+import {SectionList} from 'react-native'
 
 // Opting out of RN's defaults (because we are dealing with a large list)
 // and this seems to fix the headers from jumping all over when scrolling.
