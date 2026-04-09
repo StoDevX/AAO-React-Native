fad576cb4e588db5c59f1e8aa31f2a77354a568e
Hawken Rives <hawkrives@fastmail.fm>
2026-04-09 20:30:35 +0000

    initial work on cocoapods support on linux

--- scripts/pods.sh
+++ scripts/pods.sh
@@ -4,17 +4,27 @@ set -e -o pipefail
 
 USE_PODS=${FP_PODS:-yes}
 
-if [[ $USE_PODS = 'yes' ]]; then
-	if [[ $(uname) = 'Darwin' ]]; then
-		bundle install
-		cd ios || exit 1
-
-		if ! bundle exec pod install --deployment; then
-			echo 'try running "bundle exec pod install --repo-update"' 1>&2
-		fi
-	else
-		echo 'not on macos; not installing cocoapods'
-	fi
-else
+if test "$USE_PODS" != "yes"; then
 	echo 'FP_PODS=no; not installing cocoapods'
+	exit 0
+fi
+
+if test "$(uname)" != "Darwin"; then
+	export PATH="$PWD/scripts:$PATH"
+	echo 'not on Darwin; using scripts/xcrun and scripts/xcodebuild shims so the react-native pod specs can run on Linux'
+fi
+
+bundle install
+cd ios || exit 1
+
+# if this script is invoked with a `--` argument, run the following command instead of `pod install`:
+if test "$1" = "--"; then
+	shift
+	exec "$@"
+fi
+
+if ! bundle exec pod install --deployment; then
+	STATUS=$?
+	echo 'try running "bundle exec pod install --repo-update"' 1>&2
+	exit $STATUS
 fi
