fbe8ef1a12e3d5bbdef1710581e50f786ae8a87f
Drew Volz <volz.drew@gmail.com>
2026-04-25 19:47:54 -0600

    feat(athletics): fetch scores from ccc-server proxy instead of upstream URL directly

--- source/views/athletics/query.ts
+++ source/views/athletics/query.ts
@@ -1,21 +1,14 @@
-import ky from 'ky'
+import {client} from '@frogpond/api'
 import {queryOptions} from '@tanstack/react-query'
-import {AthleticsResponse, Score} from './types'
+import {Score} from './types'
 
 export const keys = {
 	all: ['athletics', 'scores'] as const,
 }
 
-// TODO: move this behind the AAO proxy server
-const ATHLETICS_URL =
-	'https://athletics.stolaf.edu/services/scores_chris.aspx?format=json'
-
 export const athleticsOptions = queryOptions({
 	queryKey: keys.all,
 	queryFn: async ({signal}): Promise<Score[]> => {
-		const response = await ky
-			.get(ATHLETICS_URL, {signal})
-			.json<AthleticsResponse>()
-		return response.scores
+		return client.get('athletics/scores', {signal}).json<Score[]>()
 	},
 })
