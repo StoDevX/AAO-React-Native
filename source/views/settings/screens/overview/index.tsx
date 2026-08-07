0844e5514ba804c21fbcd42978bbb3e43d07f7c9
Hawken Rives <hawken.rives@techcyte.com>
2026-08-07 08:04:22 -0400

    Rewire Settings root screen onto Host/Form

--- source/views/settings/screens/overview/index.tsx
+++ source/views/settings/screens/overview/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import {StyleSheet, ScrollView, Platform} from 'react-native'
-import {TableView} from '@frogpond/tableview'
+import {Platform, StyleSheet} from 'react-native'
+import {Form, Host, RNHostView} from '@expo/ui/swift-ui'
 import {CloseScreenButton} from '@frogpond/navigation-buttons'
 import {NativeStackNavigationOptions} from '@react-navigation/native-stack'
 
@@ -12,44 +12,48 @@ import {CredentialsLoginSection} from './login-credentials'
 import {MiscellanySection} from './miscellany'
 import {SupportSection} from './support'
 import {DeveloperSection} from './developer'
-import {AppIconSection} from './app-icon'
+import {IconSettingsView} from '../change-icon'
 
 const styles = StyleSheet.create({
-	container: {
-		paddingVertical: 20,
+	host: {
+		flex: 1,
 	},
 	banner: {
 		marginHorizontal: 20,
-		marginBottom: 20,
+		marginTop: 20,
+		marginBottom: 10,
 	},
 })
 
 const SettingsView = (): React.ReactNode => {
 	const isDev = useIsDevMode()
-	return (
-		<ScrollView
-			contentContainerStyle={styles.container}
-			contentInsetAdjustmentBehavior="automatic"
-			keyboardDismissMode="on-drag"
-			keyboardShouldPersistTaps="always"
-		>
-			<FaqBannerGroup
-				style={styles.banner}
-				target={FAQ_TARGETS.SETTINGS_ROOT}
-			/>
 
-			<TableView>
+	// SwiftUI's Form owns the scrolling. Wrapping this in a React Native
+	// ScrollView instead puts that scroll view between the touch and the
+	// SwiftUI rows, the way it does on the home screen.
+	return (
+		<Host style={styles.host}>
+			<Form>
+				{/* The banner is still React Native, so it has to be hosted back
+				    into SwiftUI to scroll with the rest of the content. */}
+				<RNHostView matchContents={true}>
+					<FaqBannerGroup
+						style={styles.banner}
+						target={FAQ_TARGETS.SETTINGS_ROOT}
+					/>
+				</RNHostView>
+
 				<CredentialsLoginSection />
 
 				<SupportSection />
 
-				<AppIconSection />
+				<IconSettingsView />
 
 				<MiscellanySection />
 
 				{isDev && <DeveloperSection />}
-			</TableView>
-		</ScrollView>
+			</Form>
+		</Host>
 	)
 }
 
