63f629d5f1747f923db3d297c177265f7f2b2ee2
Hawken Rives <hawken.rives@techcyte.com>
2026-09-06 18:58:52 -0400

    Read the calendar's clock from the device

--- app/(home)/Calendar.tsx
+++ app/(home)/Calendar.tsx
@@ -8,7 +8,6 @@ import {
 	useCalendarSources,
 	useMergedEvents,
 } from '@frogpond/ccc-calendar'
-import {timezone} from '@frogpond/constants'
 import {EventList} from '@frogpond/event-list'
 import {useMomentTimer} from '@frogpond/timer'
 
@@ -17,7 +16,7 @@ import {useCalendarFilterStore} from '../../source/features/calendar/store'
 
 export default function CalendarPage(): React.ReactNode {
 	let router = useRouter()
-	let {now} = useMomentTimer({intervalMs: 60000, timezone: timezone()})
+	let {now} = useMomentTimer({intervalMs: 60000})
 	let {enabled} = useCalendarSources()
 	let {events, failed, isLoading, isRefetching, refetchAll} = useMergedEvents(enabled)
 	let eventListRef = React.useRef<EventList.EventListHandle>(null)
